a7d99a2ec2cf05b85aa5cf8179eb86771cca159b
roca <romelcampbell@gmail.com>
2013-05-30 10:53:49 -0400

    first commit

---
+++ src/github.com/nf/string/string_test.go
@@ -0,0 +1,23 @@
+package string
+
+
+import "testing"
+
+func Test(t *testing.T) {
+       var tests = []struct {
+                 s,want string
+       }{
+              {"Backward", "drawkcaB"},
+              {"Hello, 1234","4321 ,olleH"},
+              {"Hello, 世界","界世 ,olleH"},
+              {"",""},
+       }
+      for _, c := range tests {
+              got := Reverse(c.s)
+              if got != c.want {
+                    t.Errorf("Reverse(%q) == %q, want %q", c.s,got, c.want)
+              }
+
+      }
+
+}
