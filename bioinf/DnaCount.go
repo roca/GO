b57df581896f42a9526448f1c0f07a266cacffce
roca <romelcampbell@gmail.com>
2013-06-05 11:38:31 -0400

    Trying to convert []byte to string

--- bioinf/DnaCount.go
+++ bioinf/DnaCount.go
@@ -3,10 +3,37 @@ package main
 import (
 	"../nf/string"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
+func CToGoString(c []byte) string {
+	n := -1
+	for i, b := range c {
+		if b == 0 {
+			break
+		}
+		n = i
+	}
+	return string(c[:n+1])
+}
+
 func main() {
 	args := os.Args
-	fmt.Println(string.Reverse(args[1]))
+
+	// read whole the file
+	b, err := ioutil.ReadFile(args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	// write whole the body
+	err = ioutil.WriteFile("output.txt", b, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	sequence := CToGoString(b[:])
+
+	fmt.Println(string.Reverse(sequence))
 }
