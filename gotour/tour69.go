fad4c5426f9f05b8feb1b5f09090c8f2b9e0f6c9
roca <RomelCampbell@gmail.com>
2013-07-10 03:39:54 -0400

    gotour 69

---
+++ gotour/tour69.go
@@ -0,0 +1,26 @@
+package main
+
+import ("fmt"
+        "tour/tree")
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int){
+    
+    
+        ch <- 1
+    
+        close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool
+
+func main() {
+    ch := make(chan int)
+    go Walk(tree.New(1), ch)
+    for i := range ch {
+        fmt.Println(i)
+    }
+}
