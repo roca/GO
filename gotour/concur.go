342a5e3ddc37cf5f43077455521566d433359a72
roca <RomelCampbell@gmail.com>
2013-07-18 05:48:23 -0400

    Search exmaple 30:00

--- gotour/concur.go
+++ gotour/concur.go
@@ -13,7 +13,12 @@ type Message struct {
 
 func main() {
 
-	whisper()
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
 }
 
 func concur() {
