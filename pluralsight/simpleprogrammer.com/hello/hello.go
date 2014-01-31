db42bc6a277842ca6dfb6456b9fd6c77da00bec7
roca <RomelCampbell@gmail.com>
2014-01-31 06:12:02 -0500

    Mod 3 'Basic Declaration

--- pluralsight/simpleprogrammer.com/hello/hello.go
+++ pluralsight/simpleprogrammer.com/hello/hello.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type Salutation struct {
+	name     string
+	greeting *string
+}
+
 func main() {
-	fmt.Println("Hello,GO")
+
+	var message Salutation
+	message.name = "Hello Go World"
+	message.greeting = &message.name
+	*message.greeting = "hi"
+
+	fmt.Println(message.name, *message.greeting)
 }
