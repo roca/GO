a1c4a817dee08f69550accc9726c075630142533
roca <RomelCampbell@gmail.com>
2014-02-02 05:35:59 -0500

    Finished module on functions

--- pluralsight/simpleprogrammer.com/hello/hello.go
+++ pluralsight/simpleprogrammer.com/hello/hello.go
@@ -7,23 +7,41 @@ type Salutation struct {
 	greeting string
 }
 
-func Greet(salutation Salutation) {
+type Printer func(string)
+
+func Greet(salutation Salutation, do Printer) {
 	message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
 
-	fmt.Println(message)
-	fmt.Println(alternate)
+	do(message)
+	do(alternate)
 
 }
 
 func CreateMessage(name string, greeting ...string) (message string, alternate string) {
-	fmt.Println(len(greeting))
+	//fmt.Println(len(greeting))
 	message, alternate = greeting[1]+" "+name, "HEY! "+name
 	return
 }
 
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func PrintLine(s string) {
+	fmt.Println(s)
+}
+
+func PrintCustom(s string, custom string) {
+	fmt.Println(s + custom)
+}
+
+func CreatePrintFunction(custom string) Printer {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
 func main() {
 
 	var s = Salutation{"Bob", "Hello"}
-
-	Greet(s)
+	Greet(s, CreatePrintFunction("!!!"))
 }
