c411afd3197f77e669c08260074830e65e308b66
roca <RomelCampbell@gmail.com>
2014-02-13 04:29:00 -0500

    Start module on channels

--- pluralsight/simpleprogrammer.com/hello/hello.go
+++ pluralsight/simpleprogrammer.com/hello/hello.go
@@ -17,7 +17,17 @@ func main() {
 		{"Mary", "What is up?"},
 	}
 
-	fmt.Fprintf(&salutaions[0],"The count is %d",10)
+	fmt.Fprintf(&salutaions[0], "The count is %d", 10)
 
+	done := make(chan bool, 2)
+
+	go func() {
+		salutaions.Greet(greeting.CreatePrintFunction("<C>"), true, 6)
+		done <- true
+		done <- true
+		fmt.Println("Done")
+	}()
 	salutaions.Greet(greeting.CreatePrintFunction("?"), true, 6)
+	<-done
+
 }
