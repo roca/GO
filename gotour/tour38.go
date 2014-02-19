6d30467abb7a3179eac732e8e0c2b9d7c71035aa
roca <romelcampbell@gmail.com>
2014-02-19 09:24:55 -0500

    working on tour 43

--- gotour/tour38.go
+++ gotour/tour38.go
@@ -1,8 +1,24 @@
 package main
 
-import "code.google.com/p/go-tour/pic"
+import (
+	"code.google.com/p/go-tour/pic"
+	//"math"
+)
 
-func Pic(dx, dy int) [][]uint8 {
+func Pic(dx, dy int) (pic [][]uint8) {
+
+	pic = make([][]uint8, dx)
+	for i := range pic {
+		pic[i] = make([]uint8, dy)
+	}
+
+	for x := 0; x < dx; x++ {
+		for y := 0; y < dy; y++ {
+			pic[x][y] = uint8(x * y)
+		}
+	}
+	return
+
 }
 
 func main() {
