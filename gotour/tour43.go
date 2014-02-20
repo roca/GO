9e38185bfbef718693b5b7c5b81479f3f4f4783a
roca <romelcampbell@gmail.com>
2014-02-20 09:20:25 -0500

    Working on #43

--- gotour/tour43.go
+++ gotour/tour43.go
@@ -3,10 +3,26 @@ package main
 
 import (
 	"code.google.com/p/go-tour/wc"
+	"strings"
 )
 
-func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+func WordCount(s string) (word_map map[string]int) {
+
+	words := strings.Split(s, " ")
+	word_map = make(map[string]int)
+
+	for _, value := range words {
+
+		_, ok := word_map[value]
+
+		if ok {
+			word_map[value]++
+		} else {
+			word_map[value] = 1
+		}
+	}
+
+	return
 }
 
 func main() {
