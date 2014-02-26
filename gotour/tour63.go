708c4190bfceb4dc81074da52fb39869a7ba691d
roca <romelcampbell@gmail.com>
2014-02-26 17:08:10 -0500

    Tour 63

--- gotour/tour63.go
+++ gotour/tour63.go
@@ -7,12 +7,29 @@ import (
 	"strings"
 )
 
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
 type rot13Reader struct {
 	r io.Reader
 }
 
-func (reader rot13Reader) Read(p []byte) (n int, err error) {
-
+func (r rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	for i := 0; i < n; i++ {
+		p[i] = rot13(p[i])
+	}
+	return
 }
 
 func main() {
