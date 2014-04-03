005836d74c2a7636b7138bb7b5232092e6b8f1f4
roca <romelcampbell@gmail.com>
2014-04-03 13:00:37 -0400

    Add validation

--- revel_demo_01/app/controllers/app.go
+++ revel_demo_01/app/controllers/app.go
@@ -10,3 +10,16 @@ func (c App) Index() revel.Result {
 	greeting := "Aloha World"
 	return c.Render(greeting)
 }
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("Your name is required!")
+	c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	return c.Render(myName)
+}
