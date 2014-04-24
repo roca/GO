5f15dbde4aef3c4274703e3fda211bb034baae2f
roca <romelcampbell@gmail.com>
2014-04-24 19:16:00 -0400

    Added sleep

--- system/command_runner/command_runner.go
+++ system/command_runner/command_runner.go
@@ -1,17 +1,36 @@
 // command_runner
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
 type Command string
 
 type Executable interface {
-	execute()
+	execute(done chan bool)
 }
 
-import (
-	"fmt"
-)
+func (command *Command) execute(done chan bool) {
+	fmt.Println("Working on command: ", *command)
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("Done with command!", *command)
+	done <- true
+}
 
 func main() {
+
+	var a_command Command
+	a_command = "test"
 	fmt.Println("Hello World!")
+	done := make(chan bool)
+	go a_command.execute(done)
+
+	for {
+		select {
+		case <-done:
+			break
+		}
+	}
 }
