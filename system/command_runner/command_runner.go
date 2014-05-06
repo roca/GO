382ef469d5b7d7a69caff5a7cf06f2bd817c02df
roca <romelcampbell@gmail.com>
2014-05-06 14:03:44 -0400

    Made groups

--- system/command_runner/command_runner.go
+++ system/command_runner/command_runner.go
@@ -11,13 +11,19 @@ import (
 	//"io/ioutil"
 	"log"
 	"os/exec"
+	"strings"
 	"time"
 )
 
+type Group struct {
+	Id int
+}
+
 type Command struct {
-	Id   int
-	Path string
-	Dir  string
+	Id    int
+	Path  string
+	Dir   string
+	Async int
 }
 
 type Runner interface {
@@ -27,7 +33,9 @@ type Runner interface {
 func (command *Command) run(done chan bool) {
 	//fmt.Println("--------------------------------Working on command: ", *command)
 
-	cmd := exec.Command(command.Path)
+	command_arguments := strings.Split(command.Path, " ")
+
+	cmd := exec.Command(command_arguments[0], command_arguments[1:]...)
 	cmd.Dir = command.Dir
 
 	out, err := cmd.Output()
@@ -37,13 +45,54 @@ func (command *Command) run(done chan bool) {
 
 	fmt.Printf("Output from '%s' is\n%s\n", command.Path, out)
 
-	time.Sleep(5000 * time.Millisecond)
+	//time.Sleep(5000 * time.Millisecond)
 	//fmt.Println("--------------------------------Done with ", *command)
 	done <- true
 }
 
 func main() {
 
+	db := getDatabaseConnection("commander", "cody")
+	groups := queryGroups(db)
+	for _, group := range groups {
+		commands := queryCommands(db, group)
+		runCommands(commands)
+	}
+
+	fmt.Println("all done !")
+}
+
+func runCommands(commands []Command) {
+	done := make(chan bool)
+	defer func() { close(done) }()
+
+	for i := range commands {
+		if commands[i].Async == 0 {
+			commands[i].run(done)
+		} else {
+			go commands[i].run(done)
+		}
+	}
+
+	all_done := len(commands)
+	doneCount := 0
+	timeout := time.After(3600 * time.Second)
+
+	for doneCount != all_done {
+
+		select {
+		case <-done:
+			doneCount++
+		case <-timeout:
+			fmt.Println("timed out!")
+			doneCount = all_done
+		}
+
+	}
+
+}
+
+func getDatabaseConnection(username, password string) *sql.DB {
 	//rootCertPool := x509.NewCertPool()
 	//pem, err := ioutil.ReadFile("/etc/ssl/tarritdb01p/regeneronCA.cert")
 	//if err != nil {
@@ -66,7 +115,7 @@ func main() {
 
 	//db, err := sql.Open("mysql", "commander:cody@tcp(tarritdb01t:3306)/commandlogs_dev?charset=utf8&tls=custom")
 
-	db, err := sql.Open("mysql", "commander:cody@tcp(localhost:3306)/commandlogs_dev?charset=utf8")
+	db, err := sql.Open("mysql", username+":"+password+"@tcp(tarritdb01t:3306)/commandlogs_dev?charset=utf8")
 	if err != nil {
 		fmt.Println("Error")
 		panic(err)
@@ -76,54 +125,44 @@ func main() {
 		panic(conn_err)
 	}
 
-	commands := queryCommands(db)
+	return db
 
-	//commands := make([]Command, 10000)
-
-	//for i := range commands {
-	//	commands[i] = Command(fmt.Sprintf("command %d", i))
-	//}
-
-	done := make(chan bool)
-	defer func() { close(done) }()
-
-	for i := range commands {
-		go commands[i].run(done)
-	}
-
-	all_done := len(commands)
-	doneCount := 0
-	timeout := time.After(6 * time.Second)
-
-	for doneCount != all_done {
-
-		select {
-		case <-done:
-			doneCount++
-		case <-timeout:
-			fmt.Println("timed out!")
-			doneCount = all_done
-		}
-
-	}
-
-	fmt.Println("all done !")
 }
 
-func queryCommands(db *sql.DB) []Command {
-	commands := []Command{}
-	rows, err := db.Query("SELECT id,path,dir FROM commands")
+func queryGroups(db *sql.DB) []Group {
+	groups := []Group{}
+	rows, err := db.Query("SELECT distinct(group_id) FROM commands")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for rows.Next() {
 		var id int
-		var path, dir string
-		if err := rows.Scan(&id, &path, &dir); err != nil {
+		if err := rows.Scan(&id); err != nil {
 			log.Fatal(err)
 		}
-		commands = append(commands, Command{id, path, dir})
-		//fmt.Printf("%s\n", path)
+		groups = append(groups, Group{id})
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return groups
+
+}
+func queryCommands(db *sql.DB, group Group) []Command {
+	commands := []Command{}
+	sql := fmt.Sprintf("SELECT id,path,dir,async FROM commands where group_id = %d", group.Id)
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var id, async int
+		var path, dir string
+		if err := rows.Scan(&id, &path, &dir, &async); err != nil {
+			log.Fatal(err)
+		}
+		commands = append(commands, Command{id, path, dir, async})
 
 	}
 	if err := rows.Err(); err != nil {
