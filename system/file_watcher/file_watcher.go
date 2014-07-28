739c9331e58057fcb3f226a1740db596347c80b7
roca <romelcampbell@gmail.com>
2014-07-28 15:14:18 -0400

    More refacttoring

--- system/file_watcher/file_watcher.go
+++ system/file_watcher/file_watcher.go
@@ -49,6 +49,7 @@ func file_watcher(path string, info os.FileInfo, err error) error {
 			//go func() {
 			fmt.Printf("%s", exec_command(fmt.Sprintf("ls -l %s/RTAComplete.txt", path)))
 			fmt.Println(flowcell_command, "\n")
+			//exec_command(flowcell_command)
 			//}()
 		}
 		return filepath.SkipDir
@@ -63,7 +64,7 @@ func exec_command(command string) []byte {
 	cmd := exec.Command(command_arguments[0], command_arguments[1:]...)
 	out, err := cmd.Output()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("ERROR: ", err)
 	}
 	return out
 
