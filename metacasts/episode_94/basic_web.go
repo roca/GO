90ca73e1611f38d9afb1d01d20659332594a1ccf
Romel.Campbell <Romel.Campbell@regeneron.com>
2014-09-12 14:57:19 -0400

    Go App with external template file

---
+++ metacasts/episode_94/basic_web.go
@@ -0,0 +1,39 @@
+// basic_web
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	renderTemplate("templates/index.html", w)
+}
+
+func renderTemplate(tmplName string, w http.ResponseWriter) {
+	data, _ := ioutil.ReadFile(tmplName)
+	tmpl, err := template.New("Index").Parse(string(data))
+	if err != nil {
+
+		log.Panic(err)
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &HomePageHandler{})
+
+	barHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Bar")
+	}
+
+	http.HandleFunc("/bar", barHandler)
+
+	http.ListenAndServe(":3000", nil)
+}
