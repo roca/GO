d0f0553ce5b1219bf102af9c1c1098bffbbbfd66
Romel Campbell <romel.campbell@regeneron.com>
2015-04-13 13:35:32 -0400

    Tempalte example on page 14

--- goblueprints/chat/main.go
+++ goblueprints/chat/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
-	w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's char!
-				</body>
-			</html>
-		`))
-
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	//start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
