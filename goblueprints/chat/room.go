40dbaf954aaf2d2590180318329e12168839754d
Romel Campbell <romel.campbell@regeneron.com>
2015-04-15 09:28:17 -0400

    Added more code to room

--- goblueprints/chat/room.go
+++ goblueprints/chat/room.go
@@ -4,4 +4,38 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients
 	forward chan []byte
+	// join is a channel for clients wishing to join the room.
+	join chan *client
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *client
+	// clients hoilds all current clients in this room.
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+
+	for {
+		select {
+		case client := <-r.join:
+			//joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			//leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					//send the message
+				default:
+					//failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+
 }
