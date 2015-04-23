5aaa1e8bc5eccd972917b63d527cb634e7e9e1b6
Romel Campbell <romel.campbell@regeneron.com>
2015-04-23 09:22:41 -0400

    Page 28

--- goblueprints/chat/main.go
+++ goblueprints/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,10 +19,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse() //parse the flags
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -29,7 +32,8 @@ func main() {
 	go r.run()
 
 	//start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Print("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
