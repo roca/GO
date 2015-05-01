ae4ce3e738cef3c3683a3253e61153c2b867c424
Romel Campbell <romel.campbell@regeneron.com>
2015-05-01 09:33:53 -0400

    Page 48

--- goblueprints/chat/auth.go
+++ goblueprints/chat/auth.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type authHandler struct {
@@ -20,6 +23,20 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// success - call the next handler
 		h.next.ServeHTTP(w, r)
+	}
+
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
 	}
 
 }
