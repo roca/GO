f0f7c81ba6a8f83cf421f4d86dabb7a289b2866a
Romel Campbell <romel.campbell@regeneron.com>
2015-08-19 16:00:20 -0400

    code takes a file argument

--- xlsx/xlsx.go
+++ xlsx/xlsx.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/tealeg/xlsx"
 )
 
+func usage(arg string) string {
+	return fmt.Sprintf("usage: %s <path to excel File>\n", arg)
+}
+
 func main() {
-	excelFileName := "./foo.xlsx"
+
+	if len(os.Args) == 1 {
+		fmt.Printf(usage(filepath.Base(os.Args[0])))
+		os.Exit(1)
+	}
+	excelFileName := os.Args[len(os.Args)-1]
+
 	xlFile, err := xlsx.OpenFile(excelFileName)
 	if err != nil {
 		fmt.Println("Error")
