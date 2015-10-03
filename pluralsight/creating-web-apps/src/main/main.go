8049fbfd22325d27f218470da80ab21f7d2101b8
Romel Campbell <romel.campbell@regeneron.com>
2015-10-03 05:04:54 -0400

    WIP

--- pluralsight/creating-web-apps/src/main/main.go
+++ pluralsight/creating-web-apps/src/main/main.go
@@ -4,25 +4,24 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content Type", "text/html")
-		tmpl, err := template.New("test").Parse(doc)
-		if err == nil {
-			tmpl.Execute(w, nil)
-
-		}
-	})
-	http.ListenAndServe(":8000", nil)
-}
-
 // func main() {
-// 	http.Handle("/", new(MyHandler))
+// 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+// 		w.Header().Add("Content Type", "text/html")
+// 		tmpl, err := template.New("test").Parse(doc)
+// 		if err == nil {
+// 			tmpl.Execute(w, nil)
+//
+// 		}
+// 	})
 // 	http.ListenAndServe(":8000", nil)
 // }
+
+func main() {
+	http.Handle("/", new(MyHandler))
+	http.ListenAndServe(":8000", nil)
+}
 
 type MyHandler struct {
 	http.Handler
