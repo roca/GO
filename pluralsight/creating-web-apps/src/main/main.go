8a1d42b3fa8181e8dd05e80ae1bc32b29c40bbe1
roca <RomelCampbell@gmail.com>
2015-10-07 20:49:26 -0400

    Adds Demo: Delivering Static Assets

--- pluralsight/creating-web-apps/src/main/main.go
+++ pluralsight/creating-web-apps/src/main/main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+	"bufio"
+	"strings"
 )
 
 func main() {
@@ -20,7 +22,33 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/img/",serveResource)
+	http.HandleFunc("/css/",serveResource)
+
 	http.ListenAndServe(":8000", nil)
+}
+func serveResource(w http.ResponseWriter,req *http.Request) {
+	path := "public" + req.URL.Path
+	var contentType string
+	if strings.HasSuffix(path,".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path,".png") {
+		contentType = "image.png"
+	} else   {
+		contentType = "text/plain"
+	}
+
+	f,err := os.Open(path)
+
+	if err == nil {
+		defer f.Close()
+		w.Header().Add("Content Type",contentType)
+		br := bufio.NewReader(f)
+		br.WriteTo(w)
+
+	} else {
+		w.WriteHeader(404)
+	}
 }
 
 func populateTemplates() *template.Template {
