4ee0838175ea61bfbb0ff7068212cea2d8f183af
Romel Campbell <romel.campbell@regeneron.com>
2016-02-15 14:02:18 -0500

    Pulished to local RabbitMQ

---
+++ pluralsight/distributed-go-apps/app1/src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	server()
+}
+
+func server() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msg := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello RabbitMQ"),
+	}
+	for {
+		ch.Publish("", q.Name, false, false, msg)
+	}
+}
+
+func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
+
+	conn, err := amqp.Dial("amqp://guest@localhost:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	q, err := ch.QueueDeclare("hello",
+		false, //durable bool,
+		false, //autoDelete bool,
+		false, //exclusive bool,
+		false, //noWait bool,
+		nil)   //args amqp.Table)
+	failOnError(err, "Failed to declare a queue")
+
+	return conn, ch, &q
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
