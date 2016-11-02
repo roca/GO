614504a41901105cfe53f207019698d7a54b6119
Romel Campbell <romel.campbell@regeneron.com>
2016-11-02 13:38:07 -0400

    WIP

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -1,7 +1,29 @@
 package hamming
 
+import (
+	"fmt"
+	"strings"
+)
+
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+	// your code here
+	aa := strings.Split(a, "")
+	bb := strings.Split(b, "")
+
+	score := 0
+
+	for i, a_base := range aa {
+		if a_base != bb[i] {
+			score++
+		}
+
+	}
+
+	//score = 0
+
+	fmt.Printf("score %d\n", score)
+	return score, nil
+
 }
