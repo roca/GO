aec6cef1c3bd97d5c2b4bdb68b8bfe071f0eb6de
Romel Campbell <romel.campbell@regeneron.com>
2016-11-07 17:07:43 -0500

    Starting raindrops

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -2,6 +2,16 @@ package raindrops
 
 const testVersion = 2
 
-func Convert(int) string
+// Convert tranforms int to custom message
+func Convert(n int) string {
+	factors := [3]int{3, 5, 7}
+	phrase := ""
+	for _, factor := range factors {
+		if n%factor == 0 && factor == 3 {
+			phrase = phrase + "Pling"
+		}
+	}
+	return phrase
+}
 
 // The test program has a benchmark too.  How fast does your Convert convert?
