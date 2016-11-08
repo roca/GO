b5de74b63db66adaa775b3d99fed598c05bc2e3f
Romel Campbell <romel.campbell@regeneron.com>
2016-11-08 09:42:31 -0500

    WIP

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,15 +1,30 @@
 package raindrops
 
+import (
+	"fmt"
+	"strings"
+)
+
 const testVersion = 2
 
 // Convert tranforms int to custom message
 func Convert(n int) string {
 	factors := [3]int{3, 5, 7}
-	phrase := ""
+	phrase := fmt.Sprintf("%d", n)
 	for _, factor := range factors {
 		if n%factor == 0 && factor == 3 {
 			phrase = phrase + "Pling"
+		} else if n%factor == 0 && factor == 5 {
+			phrase = phrase + "Plang"
+		} else if n%factor == 0 && factor == 5 {
+			phrase = phrase + "Plong"
 		}
+	}
+	aPhrase := strings.Split(phrase, "P")
+	if len(aPhrase) == 1 {
+		phrase = aPhrase[0]
+	} else {
+		phrase = strings.Join(aPhrase[1:], "P")
 	}
 	return phrase
 }
