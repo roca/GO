1958e72bc741a4362d86adb00523fb8ad3fdd3de
Romel Campbell <romel.campbell@regeneron.com>
2016-11-08 13:56:11 -0500

    Some refactoring

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -7,19 +7,19 @@ import (
 
 const testVersion = 2
 
-// Convert tranforms int to custom message
+// Convert tranforms int to custom phrase
+// in keeping with rules in README
 func Convert(n int) string {
 	factors := [3]int{3, 5, 7}
+	labels := [3]string{"Pling", "Plang", "Plong"}
+
 	phrase := fmt.Sprintf("%d", n)
-	for _, factor := range factors {
-		if n%factor == 0 && factor == 3 {
-			phrase = phrase + "Pling"
-		} else if n%factor == 0 && factor == 5 {
-			phrase = phrase + "Plang"
-		} else if n%factor == 0 && factor == 7 {
-			phrase = phrase + "Plong"
+	for i, factor := range factors {
+		if n%factor == 0 {
+			phrase += labels[i]
 		}
 	}
+
 	aPhrase := strings.Split(phrase, "P")
 	if len(aPhrase) == 1 {
 		phrase = aPhrase[0]
