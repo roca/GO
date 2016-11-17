1484723589aa794481d21553c203dc532dea4cc7
Romel Campbell <romel.campbell@regeneron.com>
2016-11-17 16:00:15 -0500

    Test passing

--- exercism/go/acronym/acronym.go
+++ exercism/go/acronym/acronym.go
@@ -3,16 +3,26 @@ package acronym
 import (
 	"regexp"
 	"strings"
+
+	"github.com/fatih/camelcase"
 )
 
 const testVersion = 1
 
 func abbreviate(input string) string {
-	a := regexp.MustCompile(" +|-")
+	a := regexp.MustCompile(" +|-|:|,")
 	inputs := a.Split(input, -1)
 	abbreviation := ""
 	for _, word := range inputs {
-		abbreviation += strings.Split(strings.ToUpper(word), "")[0]
+		camelCaseWords := camelcase.Split(word)
+
+		if len(camelCaseWords) == 1 {
+			abbreviation += strings.Split(strings.ToUpper(word), "")[0]
+		} else {
+			for _, word := range camelCaseWords {
+				abbreviation += strings.Split(strings.ToUpper(word), "")[0]
+			}
+		}
 	}
 	return abbreviation
 
