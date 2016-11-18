c281012be06f9d56ed684a798687ed358959f65c
Romel Campbell <romel.campbell@regeneron.com>
2016-11-18 16:43:15 -0500

    WIP

--- exercism/go/bob/bob.go
+++ exercism/go/bob/bob.go
@@ -1,31 +1,29 @@
 package bob // package name must match the package name in bob_test.go
-import (
-	"fmt"
-	"regexp"
-)
+import "regexp"
 
 const testVersion = 2 // same as targetTestVersion
 
+// Responses struct for response types
+type Responses struct {
+	matcher *regexp.Regexp
+	answer  string
+}
+
 //Hey returns a response based on input
 func Hey(input string) string {
-	matchers := []*regexp.Regexp{
-		regexp.MustCompile(`\?$`),
-		regexp.MustCompile(`^[^[a-z]]*$`),
+
+	responses := []Responses{
+		{matcher: regexp.MustCompile(`\?$`), answer: "Sure."},
+		{matcher: regexp.MustCompile(`^[^[a-z]]*$`), answer: "Whoa, chill out!"},
+		{matcher: regexp.MustCompile(`^[^[a-z]]*$`), answer: "Fine. Be that way!"},
+		{matcher: regexp.MustCompile(`^[^[a-z]]*$`), answer: "Whatever."},
 	}
 
-	responses := []string{
-		"Sure.",
-		"Whoa, chill out!",
-		"Fine. Be that way!",
-		"Whatever.",
-	}
-
-	for idx, matcher := range matchers {
-		if matcher.MatchString(input) {
-			fmt.Println(idx)
-			return responses[idx]
+	for _, response := range responses {
+		if response.matcher.MatchString(input) {
+			return response.answer
 		}
 	}
 
-	return responses[3]
+	return responses[3].answer
 }
