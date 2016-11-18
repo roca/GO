93902c8b5daffe027ff192077f389313e42a2564
Romel Campbell <romel.campbell@regeneron.com>
2016-11-18 16:30:53 -0500

    WIP

--- exercism/go/bob/bob.go
+++ exercism/go/bob/bob.go
@@ -1,7 +1,31 @@
 package bob // package name must match the package name in bob_test.go
+import (
+	"fmt"
+	"regexp"
+)
 
 const testVersion = 2 // same as targetTestVersion
 
+//Hey returns a response based on input
 func Hey(input string) string {
-	return ""
+	matchers := []*regexp.Regexp{
+		regexp.MustCompile(`\?$`),
+		regexp.MustCompile(`^[^[a-z]]*$`),
+	}
+
+	responses := []string{
+		"Sure.",
+		"Whoa, chill out!",
+		"Fine. Be that way!",
+		"Whatever.",
+	}
+
+	for idx, matcher := range matchers {
+		if matcher.MatchString(input) {
+			fmt.Println(idx)
+			return responses[idx]
+		}
+	}
+
+	return responses[3]
 }
