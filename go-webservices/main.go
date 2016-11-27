6e3b0cc929965a57f9f9c60185d19b173d589959
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-11-27 05:58:15 -0500

    WIP: connecting to db from lacal machine

--- go-webservices/main.go
+++ go-webservices/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"os"
 
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -32,10 +35,63 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type User struct {
+	ID    int    "json:id"
+	Name  string "json:username"
+	Email string "json:email"
+	First string "json:first"
+	Last  string "json:last"
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	password := os.Getenv("MYSQL_ROOT_PASSWORD")
+
+	db, err := sql.Open("mysql", "root:"+password+"@tcp(go-network:3306)/social_network")
+	if err != nil {
+		fmt.Println("Could not connect to the database")
+	}
+
+	NewUser := User{}
+	NewUser.Name = r.FormValue("user")
+	NewUser.Email = r.FormValue("email")
+	NewUser.First = r.FormValue("first")
+	NewUser.Last = r.FormValue("last")
+
+	output, err := json.Marshal(NewUser)
+	fmt.Fprintf(w, string(output))
+
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+
+	sql := "INSERT INTO users set " +
+		"   user_nickname='" + NewUser.Name +
+		"', user_first='" + NewUser.First +
+		"', user_last='" + NewUser.Last +
+		"', user_email='" + NewUser.Email + "'"
+
+	stmtIns, err := db.Prepare(sql) // ? = placeholder
+	if err != nil {
+		fmt.Println("Could not prepare sql statement")
+	}
+
+	q, err := stmtIns.Exec()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(q)
+}
+
+//http://localhost:8080/api/user/create?user=nkozyra&first=Nathan&last=Kozyra&email=nathan@nathankozyra.com
+
 func main() {
 	gorillaRoute := mux.NewRouter()
 	gorillaRoute.HandleFunc("/api/{user:[0-9]+}", Hello)
-	http.Handle("/", gorillaRoute)
+	routes := mux.NewRouter()
+	routes.HandleFunc("/api/user/create", CreateUser).Methods("GET")
+
+	http.Handle("/", routes)
 	port := os.Getenv("PORT")
 
 	http.ListenAndServe(":"+port, nil)
