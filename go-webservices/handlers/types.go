ba4303cfa8f7a5d2443df60c1cfed3f9e0e14938
Romel Campbell <romel.campbell@regeneron.com>
2016-11-30 17:03:27 -0500

    Adds SQL table for user relationships

--- go-webservices/handlers/types.go
+++ go-webservices/handlers/types.go
@@ -7,9 +7,9 @@ type API struct {
 
 // User struct
 type User struct {
-	ID    int    "json:id"
-	Name  string "json:username"
-	Email string "json:email"
-	First string "json:first"
-	Last  string "json:last"
+	ID    int    `json:id`
+	Name  string `json:username`
+	Email string `json:email`
+	First string `json:first`
+	Last  string `json:last`
 }
--- go-webservices/scripts/mysql.sql
+++ go-webservices/scripts/mysql.sql
@@ -8,3 +8,20 @@ CREATE TABLE users (
     PRIMARY KEY (user_id),
     UNIQUE INDEX user_nickname (user_nickname)
 );
+
+ALTER TABLE users ADD UNIQUE INDEX user_email (user_email);
+
+CREATE TABLE user_relationships (
+    user_relationship_id INT(13) NOT NULL,
+    from_user_id INT(10) NOT NULL,
+    to_user_id INT(10) NOT NULL,
+    user_relationship_type VARCHAR(10) NOT NULL,
+    user_relationship_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_relationship_id),
+    INDEX from_user_id (from_user_id),
+    INDEX to_user_id (to_user_id),
+    INDEX from_user_id_to_user_id (from_user_id, to_user_id),
+    INDEX from_user_id_to_user_id_user_relationship_type (from_user_id, to_user_id, user_relationship_type)
+    
+    
+)
