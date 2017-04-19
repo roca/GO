3d3c453e164a162247270dd1356f95dc239fc711
Romel Campbell <romel.campbell@regeneron.com>
2017-04-19 09:33:42 -0400

    WIP

--- graphQL/data/quotesLibrary.go
+++ graphQL/data/quotesLibrary.go
@@ -3,16 +3,19 @@ package data
 import (
 	"fmt"
 
+	"gopkg.in/mgo.v2/bson"
+
 	"golang.org/x/net/context"
 
 	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/relay"
 )
 
 var quoteIDFetcher = func(obj interface{}, info graphql.ResolveInfo, ctx context.Context) (string, error) {
 	switch obj := obj.(type) {
-	case *Quote:
-		fmt.Println(obj.ID)
-		return fmt.Sprintf("%v", obj.ID), nil
+	case *bson.ObjectId:
+		fmt.Println(obj.String)
+		return fmt.Sprintf("%v", obj.String), nil
 	}
 	return "", nil
 }
@@ -21,14 +24,7 @@ var QuotesLibraryType = graphql.NewObject(graphql.ObjectConfig{
 	Name:        "QuotesLibrary",
 	Description: "List of all quotes in the Library",
 	Fields: graphql.Fields{
-		"id": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				//ID := p.Source.(*Quote).ID
-				//fmt.Println(ID)
-				return "a", nil
-			},
-		},
+		"id": relay.GlobalIDField("QuotesLibrary", quoteIDFetcher),
 		"text": &graphql.Field{
 			Type: graphql.String,
 		},
