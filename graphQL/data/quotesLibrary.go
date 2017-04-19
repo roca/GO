f2a6a000e5d36117156574d8eda2587145f1dd3a
Romel Campbell <romel.campbell@regeneron.com>
2017-04-19 13:50:28 -0400

    Mongo ID field is extracted out

--- graphQL/data/quotesLibrary.go
+++ graphQL/data/quotesLibrary.go
@@ -13,8 +13,8 @@ import (
 var quoteIDFetcher = func(obj interface{}, info graphql.ResolveInfo, ctx context.Context) (string, error) {
 	switch obj := obj.(type) {
 	case (Quote):
-		fmt.Printf("ID: %v\n", obj.ID.Hex)
-		return fmt.Sprintf("%v", obj.ID.Hex), nil
+		fmt.Printf("ID: %v\n", obj.ID.Hex())
+		return fmt.Sprintf("%v", obj.ID.Hex()), nil
 	}
 	return "", errors.New("Not a Quote")
 }
@@ -24,6 +24,12 @@ var QuotesLibraryType = graphql.NewObject(graphql.ObjectConfig{
 	Description: "List of all quotes in the Library",
 	Fields: graphql.Fields{
 		"id": relay.GlobalIDField("QuotesLibrary", quoteIDFetcher),
+		// "id": &graphql.Field{
+		// 	Type: graphql.String,
+		// 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		// 		return p.Source.(Quote).ID.Hex(), nil
+		// 	},
+		// },
 		"text": &graphql.Field{
 			Type: graphql.String,
 		},
