b531b5da323bc3c4b393f37522bd4a2cdf18b940
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-05-14 05:52:30 -0400

    Learn how to use timeout flag

--- pluralsight/go-testing/code/src/pack/math.go
+++ pluralsight/go-testing/code/src/pack/math.go
@@ -1,18 +1,20 @@
 package pack
 
 func Add(nums ...int) int {
-
 	var result int
-
 	if len(nums) == 0 {
 		println("No arguments provided")
 		return 0
 	}
-
 	for _, i := range nums {
 		result += i
 	}
-
 	return result
+}
 
+func Subtract(initial int, nums ...int) int {
+	for _, i := range nums {
+		initial -= i
+	}
+	return initial
 }
--- pluralsight/go-testing/code/src/pack/math_test.go
+++ pluralsight/go-testing/code/src/pack/math_test.go
@@ -1,10 +1,11 @@
 package pack
 
 import "testing"
+import "time"
 
 func TestCanAddNumbers(t *testing.T) {
-
 	result := Add(1, 2)
+	time.Sleep(3 * time.Second)
 	if result != 3 {
 		t.Log("Failed to add one and two")
 		t.Fail()
@@ -13,5 +14,13 @@ func TestCanAddNumbers(t *testing.T) {
 	result = Add(1, 2, 3, 4)
 	if result != 10 {
 		t.Error("Failed to add more then two numbers")
+	}
+}
+
+func TestCanSubtractNumber(t *testing.T) {
+	result := Subtract(10, 5)
+
+	if result != 5 {
+		t.Error("Failed to substract two numbers properly")
 	}
 }
