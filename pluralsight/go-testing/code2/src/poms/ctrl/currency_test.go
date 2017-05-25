7e81f71ed1fe892607d6bb0a93ae31210c82fb6e
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-05-25 03:59:29 -0400

    Started controller test with mock response writer

---
+++ pluralsight/go-testing/code2/src/poms/ctrl/currency_test.go
@@ -0,0 +1,30 @@
+package ctrl
+
+import "testing"
+import "net/http"
+
+func TestCurrencyController(t *testing.T) {
+	// arrange
+
+	//act
+
+	//assert
+
+	// sets content-type Header
+
+	//writes correct data to client
+}
+
+type mockResponseWriter struct {
+	header       http.Header
+	capturedData []byte
+}
+
+func (mrw *mockResponseWriter) Header() http.Header {
+	return mrw.header
+}
+
+func (mrw *mockResponseWriter) Write(data []byte) (int, error) {
+	mrw.capturedData = data
+	return len(data), nil
+}
