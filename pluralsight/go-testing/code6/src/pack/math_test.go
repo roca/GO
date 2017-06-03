55d6a2d19984fd2dd97f5457349e843c6c1855e8
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-06-03 06:07:49 -0400

    Done with module on Example tests

--- pluralsight/go-testing/code6/src/pack/math_test.go
+++ pluralsight/go-testing/code6/src/pack/math_test.go
@@ -1,13 +1,24 @@
 package pack
 
-import "testing"
+import "fmt"
 
-func TestIntegrateConst(t *testing.T) {
+func ExamplePolyIntegrator_Integrate() {
 	pi := PolyIntegrator{}
 
 	result := pi.Integrate(0, 10, 3)
 
-	if result != 30 {
-		t.Error("Failed to integrate constant polynomial")
-	}
+	fmt.Println(result)
+
+	// Output:
+	// 30
+}
+func ExamplePolyIntegrator_Integrate_line() {
+	pi := PolyIntegrator{}
+
+	result := pi.Integrate(0, 10, 1, 0)
+
+	fmt.Println(result)
+
+	// Output:
+	// 50
 }
