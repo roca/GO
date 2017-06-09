1e9a3de2de6c1f1cf3655e65d28b9825991c3ef6
Romel Campbell <romel.campbell@regeneron.com>
2017-06-09 14:19:03 -0400

    Adding example app to repo

---
+++ buffalo/coke/actions/app.go
@@ -0,0 +1,59 @@
+package actions
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/gobuffalo/buffalo/middleware/i18n"
+	"log"
+
+	"github.com/GOCODE/buffalo/coke/models"
+
+	"github.com/gobuffalo/envy"
+	"github.com/gobuffalo/packr"
+
+	"github.com/markbates/goth/gothic"
+)
+
+// ENV is used to help switch settings based on where the
+// application is being run. Default is "development".
+var ENV = envy.Get("GO_ENV", "development")
+var app *buffalo.App
+var T *i18n.Translator
+
+// App is where all routes and middleware for buffalo
+// should be defined. This is the nerve center of your
+// application.
+func App() *buffalo.App {
+	if app == nil {
+		app = buffalo.Automatic(buffalo.Options{
+			Env:         ENV,
+			SessionName: "_coke_session",
+		})
+		if ENV == "development" {
+			app.Use(middleware.ParameterLogger)
+		}
+		// Protect against CSRF attacks. https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)
+		// Remove to disable this.
+		app.Use(middleware.CSRF)
+
+		app.Use(middleware.PopTransaction(models.DB))
+
+		// Setup and use translations:
+		var err error
+		T, err = i18n.New(packr.NewBox("../locales"), "en-US")
+		if err != nil {
+			log.Fatal(err)
+		}
+		app.Use(T.Middleware())
+
+		app.GET("/", HomeHandler)
+
+		app.ServeFiles("/assets", packr.NewBox("../public/assets"))
+		app.Resource("/users", UsersResource{&buffalo.BaseResource{}})
+		auth := app.Group("/auth")
+		auth.GET("/{provider}", buffalo.WrapHandlerFunc(gothic.BeginAuthHandler))
+		auth.GET("/{provider}/callback", AuthCallback)
+	}
+
+	return app
+}
