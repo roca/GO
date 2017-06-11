d24f6fdb4ef80e37b8cc23c797863f09c1fde9b6
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-06-11 05:55:38 -0400

    WIP: trying to get graphviz working on mac

--- pluralsight/go-testing/code8/03_standalone_goroutines/src/profiles/prof.go
+++ pluralsight/go-testing/code8/03_standalone_goroutines/src/profiles/prof.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"poms"
-	"poms/ctrl"
 	"runtime/pprof"
+
+	"github.com/GOCODE/pluralsight/go-testing/code2/src/poms"
+	"github.com/GOCODE/pluralsight/go-testing/code2/src/poms/ctrl"
 )
 
 func main() {
@@ -14,14 +15,15 @@ func main() {
 
 	go http.ListenAndServe(":3000", new(poms.GZipServer))
 
-	f, err := os.Create("goroutines.prof")
+	f, err := os.Create("cpu.prof")
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	for i := 0; i < 5; i++ {
+
+	pprof.StartCPUProfile(f)
+	for i := 0; i < 1000; i++ {
 		http.Get("http://localhost:3000/api/purchaseOrders/1")
 	}
-
-	pprof.Lookup("goroutine").WriteTo(f, 1)
+	pprof.StopCPUProfile()
 }
