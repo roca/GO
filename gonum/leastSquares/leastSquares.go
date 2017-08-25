3609d4cad61f03d84ed8b523e4c7858b58a28d52
Romel Campbell <romel.campbell@regeneron.com>
2017-08-25 11:01:23 -0400

    Code can except arguments now

--- gonum/leastSquares/leastSquares.go
+++ gonum/leastSquares/leastSquares.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
+
+	flag.String("h", "help", "Method of Least Squares")
+	var nPtr = flag.Int("n", 1, "order of eqution y = AX^n + BX^(n-1) + .... CX^0")
+	flag.Parse()
+
+	nOrder := *nPtr
+
+	if len(os.Args) == 1 {
+		fmt.Printf(usage(filepath.Base(os.Args[0])))
+		os.Exit(1)
+	}
+	dataFilePath := os.Args[len(os.Args)-1]
+	_, err := os.Stat(dataFilePath)
+	if err != nil {
+		fmt.Println(MyError{fmt.Sprintf("NonExisting file path : %s", dataFilePath)})
+		fmt.Printf(usage(filepath.Base(os.Args[0])))
+		os.Exit(1)
+	}
+	fmt.Printf("Data points in %s will be fit to %d order polynomial\n", dataFilePath, nOrder)
 
 	m := [2][2]float64{}
 	fmt.Println(m)
@@ -14,4 +38,17 @@ func main() {
 		fmt.Printf("\n")
 	}
 
+}
+
+func usage(arg string) string {
+	return fmt.Sprintf("usage: %s -n=[5|4|3...1] <path_of_data_points_file>\n", arg)
+}
+
+// MyError is an error implementation that includes a time and message.
+type MyError struct {
+	What string
+}
+
+func (e MyError) Error() string {
+	return fmt.Sprintf("%v", e.What)
 }
