5b90239b159298411a97cb6671b3cda5c3aba8f8
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-10-29 14:12:36 -0400

    Update to Beego

--- pluralsight/beego/code/src/lastchance/models/Account.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Account struct {
-	ID     int
-	Name   string
-	Number string
-	Amount float64
-}
--- pluralsight/beego/code/src/lastchance/models/LoginForm.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type LoginForm struct {
-	Username string `form:"username"`
-	Password string `form:"password"`
-}
