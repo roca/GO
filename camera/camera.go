c81d1417127a38523ec3957d4af199d060d39fa0
Romel Campbell <romel.campbell@regeneron.com>
2018-05-25 20:35:16 -0400

    Atemp as geting hostname of node

--- camera/camera.go
+++ camera/camera.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 const resp = `<html>
@@ -12,12 +13,25 @@ const resp = `<html>
     </head>
     <body>
         <h1>Simple Web App</h1>
-        <p>Hello World : Added GOCV</p>
+        <p>Hello World : Added GOCV to {{.Hostname}}</p>
     </body>
 </html>`
 
+type Data struct {
+	Hostname string // has to be uppercase/exportable/public
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(resp))
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	substitute := Data{hostname}
+	tmpl, err := template.New("resp").Parse(resp)
+	err = tmpl.Execute(w, substitute)
+
 }
 
 func main() {
