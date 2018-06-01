1ec31500569be908c91c57fcea75918bcda135e9
Romel Campbell <romel.campbell@regeneron.com>
2018-06-01 13:30:07 -0400

    Install go and docker

---
+++ camera/rpi-kernel/scripts/go.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y golang-go
+
+export GOPATH="/go"
+export GOARCH="arm"
+export GOOS="linux"
+export GOARM=7
--- camera/rpi-kernel/scripts/provision.sh
+++ camera/rpi-kernel/scripts/provision.sh
@@ -1,7 +1,3 @@
 /vagrant/scripts/prepare_build_environment.sh
 /vagrant/scripts/compile_kernel.sh
 
-export GOPATH="/go"
-export GOARCH="arm"
-export GOOS="linux"
-export GOARM=7
