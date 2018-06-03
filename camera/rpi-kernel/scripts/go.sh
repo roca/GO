68d758cff911eca3f8f2cb48e7f435e12ff8ccd7
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-06-03 18:41:07 -0400

    WIP

--- camera/rpi-kernel/scripts/go.sh
+++ camera/rpi-kernel/scripts/go.sh
@@ -1,6 +1,18 @@
-sudo apt-get install -y golang-go
+# sudo apt-get install -y golang-go
 
-export GOPATH="/go"
-export GOARCH="arm"
-export GOOS="linux"
-export GOARM=7
+wget https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin:${HOME}/go/bin
+
+sudo apt-get install libopencv-dev
+
+go get -u -d gocv.io/x/gocv
+make deps
+make download
+make build
+
+# export GOPATH="/go"
+# export GOARCH="arm"
+# export GOOS="linux"
+# export GOARM=7
+
--- camera/rpi/gocv/gocv.dockerfile
+++ camera/rpi/gocv/gocv.dockerfile
@@ -12,12 +12,15 @@ RUN sudo apt-get install -y \
 RUN sudo echo "deb http://deb.debian.org/debian  jessie main" >> /etc/apt/sources.list
 RUN apt-get update 
 
+RUN apt-get install libopencv-dev
 RUN go get -u -d gocv.io/x/gocv
 WORKDIR $GOPATH/src/gocv.io/x/gocv
 RUN yes | make deps
 RUN make download
 RUN make build
 RUN make clean
+
+RUN go get github.com/hybridgroup/mjpeg
 
 # Define default command
 CMD ["bash"]
