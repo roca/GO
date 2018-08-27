456dc96398decc155067c1bc14ea9cad22b0fc90
Romel Campbell <romel.campbell@regeneron.com>
2018-08-27 14:49:57 -0400

    Table example

--- gophercon2018/day1/testingworkshop/exercise/exercise.go
+++ gophercon2018/day1/testingworkshop/exercise/exercise.go
@@ -5,3 +5,7 @@ import "strings"
 func MyHasSuffix(instring string, sufffix string) bool {
 	return strings.HasSuffix(instring, sufffix)
 }
+
+func MyIndex(instring string, substring string) int {
+	return strings.Index(instring, substring)
+}
--- gophercon2018/day1/testingworkshop/exercise/exercise_test.go
+++ gophercon2018/day1/testingworkshop/exercise/exercise_test.go
@@ -13,19 +13,6 @@ import (
 // Given the value "main.go", test that it has the prefix "main"
 // Remember that your test has to start with the name `Test` and be in an `_test.go` file
 
-// Write a table drive test for strings.Index
-// https://golang.org/pkg/strings/#Index
-// Use the following test conditions
-// |------------------------------------------------|
-// | Value                     | Substring | Answer |
-// |===========================|===========|========|
-// | "Gophers are amazing!"    | "are"     | 8      |
-// | "Testing in Go is fun."   | "fun"     | 17     |
-// | "The answer is 42."       | "is"      | 11     |
-// |------------------------------------------------|
-
-// Rewrite the above test for strings.Index using subtests
-
 // Here is a simple test that tests `strings.HasSuffix`.i
 // https://golang.org/pkg/strings/#HasSuffix
 func Test_HasSuffix(t *testing.T) {
@@ -41,4 +28,35 @@ func Test_HasSuffix(t *testing.T) {
 
 	is := is.New(t)
 	is.True(exercise.MyHasSuffix(value, ".go"))
+}
+
+// Write a table drive test for strings.Index
+// https://golang.org/pkg/strings/#Index
+// Use the following test conditions
+// |------------------------------------------------|
+// | Value                     | Substring | Answer |
+// |===========================|===========|========|
+// | "Gophers are amazing!"    | "are"     | 8      |
+// | "Testing in Go is fun."   | "fun"     | 17     |
+// | "The answer is 42."       | "is"      | 11     |
+// |------------------------------------------------|
+
+var tt = []struct {
+	Value     string
+	Substring string
+	Answer    int
+}{
+	{Value: "Gophers are amazing!", Substring: "are", Answer: 8},
+	{Value: "Testing in Go is fun.", Substring: "fun", Answer: 17},
+	{Value: "The answer is 42.", Substring: "is", Answer: 11},
+}
+
+// Rewrite the above test for strings.Index using subtests
+
+func TestTableDriven(t *testing.T) {
+
+	is := is.New(t)
+	for _, x := range tt {
+		is.Equal(exercise.MyIndex(x.Value, x.Substring), x.Answer)
+	}
 }
