1e9a2bb70813837dc9a1f0e2028e7928e56daf1a
Romel Campbell <romel.campbell@regeneron.com>
2018-09-03 16:08:35 -0400

    test

--- camera/camera.go
+++ camera/camera.go
@@ -13,7 +13,7 @@ const resp = `<html>
 	</head>
     <body>
         <h1>Simple Web App</h1>
-        <p>Hello World : Added GOCV to {{.Hostname}} xxyzzzzzz</p>
+        <p>Hello World : Added GOCV to {{.Hostname}} xxyzzz</p>
     </body>
 </html>`
 
