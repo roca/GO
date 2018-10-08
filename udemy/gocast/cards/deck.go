74e2b1436e61cd96b9e1b234d35c94a19dca071d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-10-08 03:56:29 -0400

    Creates the shuffle function

--- udemy/gocast/cards/deck.go
+++ udemy/gocast/cards/deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -29,4 +35,31 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPostion := rand.Intn(len(d) - 1)
+
+		d[i], d[newPostion] = d[newPostion], d[i]
+	}
 }
--- udemy/gocast/cards/main.go
+++ udemy/gocast/cards/main.go
@@ -2,9 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards.shuffle()
+	cards.print()
 }
