d2e8d76a2ad63103673a3d71eeb6e1316ce07505
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-10-09 03:56:34 -0400

    Wrote 2 test

---
+++ udemy/gocast/cards/deck_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of 'Ace of Spades', but got %v", d[0])
+	}
+}
