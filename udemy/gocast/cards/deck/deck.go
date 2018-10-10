18a8ed7372e13819595d85ccc9539e727eb80b2c
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-10-10 03:56:11 -0400

    Wrote test for Saveing and Loading file from disk

--- udemy/gocast/cards/deck/deck.go
+++ udemy/gocast/cards/deck/deck.go
@@ -42,11 +42,12 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func (d deck) saveToFile(filename string) error {
+func (d deck) SaveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
-func newDeckFromFile(filename string) deck {
+//NewDeckFromFile ...
+func NewDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Error", err)
--- udemy/gocast/cards/deck/test/deck_test.go
+++ udemy/gocast/cards/deck/test/deck_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/GOCODE/udemy/gocast/cards/deck"
@@ -14,5 +15,21 @@ func TestNewDeck(t *testing.T) {
 	}
 	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of 'Ace of Spades', but got %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of 'Four of Clubs', bot got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := deck.NewDeck()
+	d.SaveToFile("_decktesting")
+
+	loadedDeck := deck.NewDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
 	}
 }
