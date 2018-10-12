fc8348d9dacc0bfe27cf607708ba67ac55571881
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-10-12 03:57:43 -0400

    Struct

--- udemy/gocast/structs/main.go
+++ udemy/gocast/structs/main.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v\n", jim)
 }
