01b8701937bd178a3a7d7b8e21909eea67716945
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-10-21 06:00:01 -0400

    Installed PostMan Created rerturning data for geting books and posting a book

--- udemy/RESTfulGolang/books-list/main.go
+++ udemy/RESTfulGolang/books-list/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -18,6 +20,15 @@ var books []book
 
 func main() {
 	router := mux.NewRouter()
+
+	books = append(books,
+		book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		book{ID: 3, Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"},
+	)
+
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/books", addBook).Methods("POST")
@@ -29,15 +40,28 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get all books is called")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get book is called")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Add book is called")
+	var newBook book
+	_ = json.NewDecoder(r.Body).Decode(&newBook)
+
+	books = append(books, newBook)
+
+	json.NewEncoder(w).Encode(books)
+
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
