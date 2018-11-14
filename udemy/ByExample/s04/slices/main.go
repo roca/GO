bf29bf5742f29a1705c25fd8fc5740abbbdffb11
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-11-14 03:55:18 -0500

    Expand

--- udemy/ByExample/s04/slices/main.go
+++ udemy/ByExample/s04/slices/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -23,5 +25,22 @@ func main() {
 	a1 := []int{1, 2, 3, 4, 5, 6, 7}
 	a2 := append(a1[0:2], a1[4:]...)
 	fmt.Println(a2)
+
+	// Delete
+	{
+		rand.Seed(time.Now().UnixNano())
+		a1 := []int{1, 2, 3, 4, 5, 6, 7}
+		c := rand.Intn(len(a1) - 1)
+		fmt.Println(c)
+		a2 := append(a1[0:c], a1[c+1:]...)
+		fmt.Println(a2)
+	}
+
+	//Expand https://github.com/golang/go/wiki/SliceTricks
+	{
+		a := []int{1, 2, 3, 4, 5, 6, 7}
+		b = append(a[:2], append(make([]int, 4), a[2:]...)...)
+		fmt.Println(b)
+	}
 
 }
