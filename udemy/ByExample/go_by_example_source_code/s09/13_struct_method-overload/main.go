36aeb21f0d0b4fa8db501e983282602b804a62f1
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-12-01 05:51:06 -0500

    Done wih  sec 09

--- udemy/ByExample/go_by_example_source_code/s09/13_struct_method-overload/main.go
+++ udemy/ByExample/go_by_example_source_code/s09/13_struct_method-overload/main.go
@@ -14,7 +14,7 @@ type movie struct {
 }
 
 type imdb struct {
-	movie
+	movie   movie
 	name    string
 	comment string
 }
@@ -24,7 +24,7 @@ func (m movie) fullInfo() string {
 }
 
 func (i imdb) fullInfo() string {
-	return strings.ToLower(i.movie.name + "-" + 
+	return strings.ToLower(i.movie.name + "-" +
 		i.movie.actor + "-" + i.name + "-" + i.comment)
 }
 
