1c82686e4f70fc6cd6edd6acfadd59612a526dad
Romel Campbell <romel.campbell@regeneron.com>
2018-12-07 15:54:27 -0500

    concurrency_paterns from : https://www.youtube.com/watch?v=f6kdp27TYZs

---
+++ concurrency_paterns/example-03/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			c <- <-input1
+		}
+	}()
+
+	go func() {
+		for {
+			c <- <-input2
+		}
+	}()
+
+	return c
+}
+
+func main() {
+
+	c := fanIn(boring("Joe"), boring("Ann")) // Function returning a channel
+
+	for i := 0; i < 100; i++ {
+		fmt.Println(<-c)
+	}
+
+	fmt.Println("You're both boring: I'm leaving.")
+
+}
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+
+	go func() { // We launch the goroutine from inside the function
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
