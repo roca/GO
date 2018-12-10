1da963c2e450af8757563962d9f7b98c0231ff99
Romel Campbell <romel.campbell@regeneron.com>
2018-12-10 09:25:11 -0500

    WIP

--- concurrency_paterns/example-03/main.go
+++ concurrency_paterns/example-03/main.go
@@ -6,17 +6,18 @@ import (
 	"time"
 )
 
-func fanIn(input1, input2 <-chan string) <-chan string {
+func fanIn(inputs ...<-chan string) <-chan string {
 	c := make(chan string)
+
 	go func() {
 		for {
-			c <- <-input1
+			c <- <-inputs[0]
 		}
 	}()
 
 	go func() {
 		for {
-			c <- <-input2
+			c <- <-inputs[1]
 		}
 	}()
 
