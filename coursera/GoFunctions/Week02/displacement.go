f90e09d6e09b339f939ce95181559161cadfc7fe
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-01-10 05:11:04 -0500

    Week01 done,Star on assignment of week 2

--- coursera/GoFunctions/Week02/displacement.go
+++ coursera/GoFunctions/Week02/displacement.go
@@ -8,7 +8,8 @@ Write a program which first prompts the user to enter values for acceleration, i
 Then the program should prompt the user to enter a value for time and the program should compute the displacement after the entered time.
 
 You will need to define and use a function called GenDisplaceFn() which takes three float64 arguments, acceleration a, initial velocity vo, and initial displacement so.
-GenDisplaceFn() should return a function which computes displacement as a function of time, assuming the given values acceleration, initial velocity, and initial displacement. The function returned by GenDisplaceFn() should take one float64 argument t, representing time, and return one float64 argument which is the displacement travelled after time t.
+GenDisplaceFn() should return a function which computes displacement as a function of time, assuming the given values acceleration, initial velocity, and initial displacement.
+The function returned by GenDisplaceFn() should take one float64 argument t, representing time, and return one float64 argument which is the displacement travelled after time t.
 
 For example, let’s say that I want to assume the following values for acceleration, initial velocity, and initial displacement: a = 10, vo = 2, so = 1.
 I can use the following statement to call GenDisplaceFn() to generate a function fn which will compute displacement as a function of time.
