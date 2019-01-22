0791d52e0688a368bfe4af09fc773dbdc8a9cd36
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-01-22 05:08:57 -0500

    Starter code for assignment

---
+++ coursera/GoConcurrency/Week04/chopsticks.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type ChopS struct{ sync.Mutex }
+
+type Philo struct {
+	leftCS, rightCS *ChopS
+}
+
+var on sync.Once
+
+func setup() {
+	fmt.Println("Init")
+}
+
+func (p Philo) eat() {
+
+	on.Do(setup)
+
+	for {
+		p.leftCS.Lock()
+		p.rightCS.Lock()
+
+		fmt.Println("eating")
+
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+	}
+}
+
+func main() {
+	Csticks := make([]*ChopS, 5)
+	for i := 0; i < 5; i++ {
+		Csticks[i] = new(ChopS)
+	}
+	philos := make([]*Philo, 5)
+	for i := 0; i < 5; i++ {
+		philos[i] = &Philo{Csticks[i], Csticks[(i+1)%5]}
+	}
+
+	for i := 0; i < 5; i++ {
+		go philos[i].eat()
+	}
+}
