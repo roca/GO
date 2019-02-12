04e1f6f5f6e4e12ca1ebe9eeb1a5681662bba3f0
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-02-12 05:16:17 -0500

    02122019/05:15

---
+++ udemy/RealTime/app.js
@@ -0,0 +1,9 @@
+class Channel extends React.Component{
+    render() {
+        return(
+            <li>Channel name</li>
+        )
+    }
+}
+
+ReactDOM.render(<Channel />, document.getElementById('app'));
