2ebe41b20a2255ae3f0e56d5919ad1ff7d709689
Romel Campbell <romel.campbell@regeneron.com>
2019-02-12 15:27:05 -0500

    04:12 02122019/15:26

--- udemy/RealTime/app.js
+++ udemy/RealTime/app.js
@@ -1,9 +1,76 @@
+let channels =[
+    {name: 'Hardware Support'},
+    {name: 'Software Support'}
+];
+
 class Channel extends React.Component{
+    onClick() {
+        console.log('I was clicked',this.props.name);
+    }
     render() {
         return(
-            <li>Channel Name</li>
+            <li  onClick={this.onClick.bind(this)}>{this.props.name}</li>
         )
     }
 }
 
-ReactDOM.render(<Channel />, document.getElementById('app'));
+class ChannelList extends React.Component {
+    render() {
+        return (
+            <ul>
+               {this.props.channels.map( (channel,index) => {
+                   return(
+                       <Channel key={index} name={channel.name}/>
+                   )
+                  }
+               )}
+            </ul>
+        )
+    }
+}
+
+class ChannelForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    onChange(e){
+        this.setState({
+            channelName: e.target.value 
+        });
+        //console.log(e.target.value);
+    }
+    onSubmit(e) {
+        let {channelName} = this.state;
+        console.log(channelName);
+        channels.push({
+            name: channelName
+        });
+        this.setState({
+            channelName: ''
+        });
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input type='text'
+                    onChange={this.onChange.bind(this)}
+                    value={this.state.channelName}></input>
+            </form>
+        )
+    }
+}
+
+class ChannelSection extends React.Component{
+    render() {
+        return(
+            <div>
+                <ChannelList channels={channels} />
+                <ChannelForm />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ChannelSection />, document.getElementById('app'));
