9d2cd97a08862643b4331764b4ab84641b907c4d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-02-13 03:53:42 -0500

    02132019/03:52

--- udemy/RealTime/app.js
+++ udemy/RealTime/app.js
@@ -1,8 +1,3 @@
-let channels =[
-    {name: 'Hardware Support'},
-    {name: 'Software Support'}
-];
-
 class Channel extends React.Component{
     onClick() {
         console.log('I was clicked',this.props.name);
@@ -43,12 +38,10 @@ class ChannelForm extends React.Component {
     onSubmit(e) {
         let {channelName} = this.state;
         console.log(channelName);
-        channels.push({
-            name: channelName
-        });
         this.setState({
             channelName: ''
         });
+        this.props.addChannel(channelName);
         e.preventDefault();
     }
     render() {
@@ -63,11 +56,27 @@ class ChannelForm extends React.Component {
 }
 
 class ChannelSection extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            channels: [
+                {name: 'Hardware Support'},
+                {name: 'Software Support'}
+            ]
+        };
+    }
+    addChannel(name){
+        let {channels} = this.state;
+        channels.push({name: name});
+        this.setState({
+            channels: channels
+        });
+    }
     render() {
         return(
             <div>
-                <ChannelList channels={channels} />
-                <ChannelForm />
+                <ChannelList channels={this.state.channels} />
+                <ChannelForm addChannel={this.addChannel.bind(this)}/>
             </div>
         )
     }
