e6b724c4c73b6793261c4bfb73cdaaa9fb372822
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-02-23 05:42:09 -0500

    Websocket can echo messages

--- udemy/RealTime/api/main.go
+++ udemy/RealTime/api/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3001", nil)
 }
 
+func logFatal(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from GOlang")
+	// fmt.Fprintf(w, "Hello from GO")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	logFatal(err)
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		logFatal(err)
+		fmt.Println(string(msg))
+		err = socket.WriteMessage(msgType, msg)
+		logFatal(err)
+	}
+
 }
