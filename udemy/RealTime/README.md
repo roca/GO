9b34c2cd1f0ed7727a6be936c47894bd4628295c
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-02-24 06:16:58 -0500

    2242019/06:16

---
+++ udemy/RealTime/README.md
@@ -0,0 +1,3 @@
+http://jsbin.com/?html,output
+
+http://websocket.org/echo.html
--- udemy/RealTime/api/main.go
+++ udemy/RealTime/api/main.go
@@ -6,12 +6,23 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+type Channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
@@ -30,11 +41,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	socket, err := upgrader.Upgrade(w, r, nil)
 	logFatal(err)
 	for {
-		msgType, msg, err := socket.ReadMessage()
-		logFatal(err)
-		fmt.Println(string(msg))
-		err = socket.WriteMessage(msgType, msg)
-		logFatal(err)
+		// msgType, msg, err := socket.ReadMessage()
+		// logFatal(err)
+		var inMessage Message
+		var outMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
+			logFatal(err)
+			break
+		}
+		switch inMessage.Name {
+		case "channel add":
+			if err := addChannel(inMessage.Data); err != nil {
+				outMessage = Message{"error", err}
+				if err := socket.WriteJSON(&outMessage); err != nil {
+					logFatal(err)
+					break
+				}
+			}
+		}
+		// fmt.Println(string(msg))
+		// err = socket.WriteMessage(msgType, msg)
+		// logFatal(err)
 	}
 
+}
+
+func addChannel(data interface{}) error {
+	var channel Channel
+
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return err
+	}
+	channel.ID = "1"
+	fmt.Printf("%#v\n", channel)
+	return nil
 }
