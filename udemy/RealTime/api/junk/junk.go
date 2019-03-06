6a6f1f80914324bee66778ce5e1a3ef5dc2b7583
Romel Campbell <romel.campbell@regeneron.com>
2019-03-06 16:59:01 -0500

    03062019/16:55

--- udemy/RealTime/api/junk/junk.go
+++ udemy/RealTime/api/junk/junk.go
@@ -8,14 +8,31 @@ import (
 )
 
 func subscribe(session *r.Session, stop <-chan bool) {
-	var change r.ChangeResponse
+	result := make(chan r.ChangeResponse)
+
 	cursor, _ := r.Table("channel").
 		Changes().
 		Run(session)
 
-	for cursor.Next(&change) {
-		fmt.Printf("%#v\n", change.NewValue)
+	go func() {
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			result <- change
+		}
+		fmt.Println("exiting cursor goroutine")
+	}()
+
+	for {
+		select {
+		case change := <-result:
+			fmt.Printf("%#v\n", change.NewValue)
+		case <-stop:
+			fmt.Println("closing cursor")
+			cursor.Close()
+			return
+		}
 	}
+
 }
 
 func main() {
@@ -26,6 +43,8 @@ func main() {
 	stop := make(chan bool)
 	go subscribe(session, stop)
 	time.Sleep(time.Second * 5)
+	fmt.Println("sending stop")
+	stop <- true
 	fmt.Println("browser closes... websocket closes")
 	time.Sleep(time.Second * 10000)
 }
