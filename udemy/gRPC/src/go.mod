a684310822b66259a538078aebf80e18e9e7d0a6
roca <romel.campbell@regeneron.com>
2019-04-25 16:29:10 -0400

    Basic setup working

---
+++ udemy/gRPC/src/go.mod
@@ -0,0 +1,8 @@
+module udemy.com/gRPC/src
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.1
+	google.golang.org/grpc v1.20.1
+)
---
+++ udemy/gRPC/src/greet/greet_server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"udemy.com/gRPC/src/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
