72b9d4f9a3181880bcd478b27cfcc20d03216c6f
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-05-27 05:28:12 -0400

    Uploaded a lambda through the cli

--- udemy/Serverless/README.md
+++ udemy/Serverless/README.md
@@ -1,3 +1,16 @@
 Save time and resources by building applications with Golang and deploying them to AWS Lambda
 
 API; endpoint, Invoke URL: https://kl4mnqn3bc.execute-api.us-east-1.amazonaws.com/Prod
+
+
+
+aws lambda create-function \
+--function-name questionresponse \
+--runtime go1.x \
+--role arn:aws:iam::132172135366:role/service-role/donothing \
+--handler main \
+--zip-file fileb://main.zip
+
+aws lambda invoke \
+--function-name questionresponse \
+--payload '{"Question": "How tall are you?"}'
--- udemy/Serverless/code/main.go
+++ udemy/Serverless/code/main.go
@@ -1,24 +1,28 @@
 package main
 
-import (
-	"fmt"
+/*
+   In this example we will up load the zipped copy
+   of the executable into the AWS console interface
+*/
 
+import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type Event struct {
-	Username string
+	Question string
 }
 
-func handler(e Event) (string, error) {
-	if len(e.Username) == 0 {
-		return "", fmt.Errorf("No Username Given")
-	}
+type Response struct {
+	Question string
+	Answer   string
+}
 
-	if e.Username[0] == 'D' {
-		return "", fmt.Errorf("Dont Like : %s", e.Username)
-	}
-	return fmt.Sprintf("<h1>Hello %s from Lambda Go</h1>", e.Username), nil
+func handler(e Event) (Response, error) {
+	return Response{
+		Question: e.Question,
+		Answer:   "I don't know. " + e.Question,
+	}, nil
 }
 
 func main() {
