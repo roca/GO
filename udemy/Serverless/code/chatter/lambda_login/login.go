ca0cdc26627a877d27c343aa58e1e9a5246e2b27
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-06-13 04:00:02 -0400

    Lambda Login code

---
+++ udemy/Serverless/code/chatter/lambda_login/login.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+
+	"udemy.com/serverless/code/chatter/chatsess"
+	"udemy.com/serverless/code/chatter/chatsess/usersess"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+type Event struct {
+	Username string
+	Password string
+}
+
+type Response struct {
+	Job    string
+	Sessid string
+	Err    string
+}
+
+func handler(c context.Context, ev Event) (Response, error) {
+	sess := session.Must(session.NewSession())
+
+	u, err := chatsess.GetDBUserPass(ev.Username, ev.Password, sess)
+
+	if err != nil {
+		return Response{
+			Job: "Login", Err: err.Error(),
+		}, err
+	}
+
+	lg := usersess.NewLogin(u.Username)
+	err = lg.Put(sess)
+
+	if err != nil {
+		return Response{
+			Job: "Login",
+			Err: err.Error(),
+		}, err
+	}
+
+	return Response{
+		Job:    "Login",
+		Sessid: lg.Sessid,
+	}, nil
+}
+
+func main() {
+
+}
