9f737acf9455cbe3053b71f2359350da5f2d1ee2
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-06-27 03:59:58 -0400

    Ran dev-initsatiat

--- udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
+++ udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
@@ -1,4 +1,4 @@
 #PEER_MODE=net
 #Command=dev-init.sh  
-#Generated: Thu Jun  6 13:15:48 UTC 2019 
+#Generated: Thu Jun 27 07:52:38 UTC 2019 
 docker-compose  -f ./compose/docker-compose.base.yaml        down 
--- udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/cc.env.sh
+++ udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/cc.env.sh
@@ -1,13 +1,13 @@
-# Generated: Fri May 10 13:59:53 UTC 2019
+# Generated: Thu Jun 27 07:58:25 UTC 2019
 export CC_LANGUAGE=golang
-export CC_PATH=token/v3
-export CC_NAME=token
-export CC_VERSION=1.0
+export CC_PATH=chaincode_example02
+export CC_NAME=gocc
+export CC_VERSION=1
 export CC_CHANNEL_ID=airlinechannel
 export CC_CONSTRUCTOR='{"Args":["init","a","100","b","300"]}'
-export CC_QUERY_ARGS='{"Args":["query","a"]}'
-export CC_INVOKE_ARGS='{"Args":["invoke","a","b","10"]}'
-export CORE_CHAINCODE_ID_NAME='token'
+export CC_QUERY_ARGS='{"Args":["query","b"]}'
+export CC_INVOKE_ARGS='{"Args":["invoke","a","b","5"]}'
+export CORE_CHAINCODE_ID_NAME='gocc'
 export CORE_CHAINCODE_LOGGING_LEVEL=''
 export CORE_CHAINCODE_LOGGING_SHIM=''
 export CC_PRIVATE_DATA_JSON=''
