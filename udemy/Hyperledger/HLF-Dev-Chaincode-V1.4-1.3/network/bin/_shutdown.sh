dc817ef7685ca17c908e896641ac75e31929e275
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-07-05 05:19:18 -0400

    07052019/05:17

--- udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
+++ udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
@@ -1,4 +1,4 @@
-#PEER_MODE=dev
-#Command=dev-init.sh -d 
-#Generated: Wed Jul  3 07:41:10 UTC 2019 
-docker-compose  -f ./compose/docker-compose.base.yaml    -f ./compose/docker-compose.dev.yaml      down 
+#PEER_MODE=net
+#Command=dev-init.sh  
+#Generated: Fri Jul  5 08:58:31 UTC 2019 
+docker-compose  -f ./compose/docker-compose.base.yaml        down 
