668cf2c560d451f9b35b6936aa23842b08cbeeba
roca <romel.campbell@regeneron.com>
2019-07-09 15:49:11 -0400

    WIP

--- exercism/go/phone-number/phone_number.go
+++ exercism/go/phone-number/phone_number.go
@@ -3,12 +3,25 @@ package phonenumber
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
+// Number ...
 func Number(input string) (string, error) {
+	number, err := Format(input)
+	if err != nil {
+		return "", err
+	}
 
-	output := regexp.MustCompile(`\(|\)|\s+|-|\.|\+|\@|\!|[a-z|A-Z]+`).Split(input, -1)
+	return number, nil
+}
+
+// Format ...
+func Format(input string) (string, error) {
+	re := regexp.MustCompile(`\:|\@|\!|\+|[a-z]|[A-Z]|\.|\-|\(|\)|\s*`)
+	output := strings.Split(re.ReplaceAllLiteralString(input, ""), "")
+
 	if len(output) >= 11 && output[0] != "1" {
 		return "", fmt.Errorf("inccorect country code: %s", output[0])
 	}
@@ -16,18 +29,32 @@ func Number(input string) (string, error) {
 	if len(output) >= 11 {
 		output = output[1:]
 	}
+	fmt.Printf("INPUT string: %s\t", input)
+	fmt.Printf("INPUT ARRAY: %s\n", output)
 
 	number := strings.Join(output[:], "")
+
 	if len(number) < 10 || len(number) > 11 {
 		return "", fmt.Errorf("number is inccorect length: %d", len(number))
 	}
+
+	areaCode, _ := strconv.Atoi(number[0:3])
+	exchangeCode, _ := strconv.Atoi(number[3:6])
+
+	if areaCode < 200 || exchangeCode < 200 {
+		return "", fmt.Errorf("area or exchange codes < 200 %s %s", number[0:3], number[3:6])
+	}
+
 	return number, nil
 }
 
-func Format(input string) (string, error) {
-	return "", nil
-}
-
+// AreaCode ...
 func AreaCode(input string) (string, error) {
-	return "", nil
+
+	number, err := Number(input)
+	if err != nil {
+		return "", err
+	}
+
+	return number[0:3], nil
 }
