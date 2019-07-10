099d42830652ede0ee585257aafa2e7cee545524
roca <romel.campbell@regeneron.com>
2019-07-10 15:13:16 -0400

    Adds better commenting

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,12 +1,12 @@
 package twofer
 
+// Package twofer implements ShareWith for manipulating a twofer phrase.
+// `Two-fer` or `2-fer` is short for two for one. One for you and one for me.
+
 import "fmt"
 
-/*
-ShareWith :
-  If the given name is "Alice", the result should be "One for Alice, one for me."
-  If no name is given, the result should be "One for you, one for me."
-*/
+// ShareWith returns "One for [NAME], one for me." if NAME is not "".
+// Otherwise it returns "One for you, one for me."
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
