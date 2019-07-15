490856e47d03887d28a701ca0725d2b28e642d34
roca <romel.campbell@regeneron.com>
2019-07-15 15:42:15 -0400

    WIP: Proverb exercisism

--- exercism/go/proverb/proverb.go
+++ exercism/go/proverb/proverb.go
@@ -1,42 +1,22 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb implements Proverb which returns a proverb given a list of inputs as []string
 package proverb
 
 import (
 	"fmt"
 )
 
-// Proverb should have a comment documenting it.
+// Proverb returns a proverb given a list of inputs as []string
 func Proverb(rhyme []string) []string {
-
-	type Nouns struct {
-		First  string
-		Second string
-	}
-	var Pairs = []Nouns{}
 
 	var proverb []string
 
-	for i, word := range rhyme {
+	for i := range rhyme {
 		if i != 0 {
-			Pairs = append(Pairs, Nouns{First: rhyme[i-1], Second: word})
+			proverb = append(proverb, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i-1], rhyme[i]))
 		}
 	}
 	if len(rhyme) != 0 {
-		Pairs = append(Pairs, Nouns{First: rhyme[0]})
-	}
-
-	for i, pair := range Pairs {
-
-		if i != len(Pairs)-1 {
-			proverb = append(proverb, fmt.Sprintf("For want of a %s the %s was lost.", pair.First, pair.Second))
-		} else {
-			proverb = append(proverb, fmt.Sprintf("And all for the want of a %s.", pair.First))
-		}
-
+		proverb = append(proverb, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
 	}
 
 	return proverb
