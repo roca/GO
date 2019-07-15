0df1cf4fcd0ea6e24aa9043eea1c1548cb22b4b1
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-07-15 05:18:32 -0400

    Delete State exercize

--- udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
+++ udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/_shutdown.sh
@@ -1,4 +1,4 @@
 #PEER_MODE=dev
 #Command=dev-init.sh -d 
-#Generated: Sun Jul 14 09:24:52 UTC 2019 
+#Generated: Mon Jul 15 09:12:17 UTC 2019 
 docker-compose  -f ./compose/docker-compose.base.yaml    -f ./compose/docker-compose.dev.yaml      down 
--- udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/cc.env.sh
+++ udemy/Hyperledger/HLF-Dev-Chaincode-V1.4-1.3/network/bin/cc.env.sh
@@ -1,12 +1,12 @@
-# Generated: Sun Jul 14 09:45:31 UTC 2019
+# Generated: Mon Jul 15 09:16:05 UTC 2019
 export CC_LANGUAGE=golang
-export CC_PATH=exercise/tokenv2
+export CC_PATH=exercise/tokenv5
 export CC_NAME=token
 export CC_VERSION=1.0
 export CC_CHANNEL_ID=airlinechannel
-export CC_CONSTRUCTOR='{"Args": ["init"]}'
-export CC_QUERY_ARGS='{"Args": ["invoke"] }'
-export CC_INVOKE_ARGS='{"Args": ["invoke"] }'
+export CC_CONSTRUCTOR='{"args":[]}'
+export CC_QUERY_ARGS='{"args":["get"]}'
+export CC_INVOKE_ARGS='{"args":["delete"]}'
 export CORE_CHAINCODE_ID_NAME='token'
 export CORE_CHAINCODE_LOGGING_LEVEL='DEBUG'
 export CORE_CHAINCODE_LOGGING_SHIM='ERROR'
