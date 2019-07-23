8701573d2c59a0a126959d6867dd2264a29b8bec
roca <romel.campbell@regeneron.com>
2019-07-23 15:10:16 -0700

    Finished the scrabble exercisim

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -5,6 +5,16 @@ import (
 	"strings"
 )
 
+var charScores = map[byte]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
 // Score returns the scrabble number score for a given word
 func Score(input string) int {
 
@@ -12,29 +22,8 @@ func Score(input string) int {
 	word := strings.ToUpper(input)
 
 	for _, c := range word {
-		score += CharScore(byte(c))
+		score += charScores[byte(c)]
 	}
 
 	return score
-}
-
-// CharScore returns the scrabble number score for a given character
-func CharScore(char byte) int {
-	charScores := map[string]int{
-		"AEIOULNRST": 1,
-		"DG":         2,
-		"BCMP":       3,
-		"FHVWY":      4,
-		"K":          5,
-		"JX":         8,
-		"QZ":         10,
-	}
-
-	for key, value := range charScores {
-		if strings.IndexByte(key, char) != -1 {
-			return value
-		}
-	}
-
-	return 0
 }
