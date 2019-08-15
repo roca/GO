38ee24863f5db80ed44dbb309d02f9ff03632c37
roca <romel.campbell@regeneron.com>
2019-08-15 11:43:57 -0400

    can resize a image

---
+++ udemy/Serverless/code/resizeImages/go.mod
@@ -0,0 +1,5 @@
+module github.com/GO
+
+go 1.12
+
+require github.com/disintegration/imaging v1.6.0
--- udemy/Serverless/code/resizeImages/main.go
+++ udemy/Serverless/code/resizeImages/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+
+	"github.com/disintegration/imaging"
+)
 
 type Event struct {
 }
@@ -15,5 +22,38 @@ func handler(e Event) (string, error) {
 }
 
 func main() {
-	lambda.Start(handler)
+
+	existingImageFile, err := os.Open("gopher.jpeg")
+	if err != nil {
+		// Handle error
+	}
+	defer existingImageFile.Close()
+
+	// Calling the generic image.Decode() will tell give us the data
+	// and type of image it is as a string. We expect "png"
+	imageData, err := jpeg.Decode(existingImageFile)
+	if err != nil {
+		// Handle error
+	}
+
+	dstImage := imaging.Resize(imageData, 400, 0, imaging.Lanczos)
+
+	// outputFile is a File type which satisfies Writer interface
+	outputFile, err := os.Create("test.png")
+	if err != nil {
+		// Handle error
+	}
+
+	// Encode takes a writer interface and an image interface
+	// We pass it the File and the RGBA
+	png.Encode(outputFile, dstImage)
+
+	// Don't forget to close files
+	outputFile.Close()
+
+	//lambda.Start(handler)
+}
+
+func resizeIamge(imageData image.Image) (image.Image, error) {
+	return imaging.Resize(imageData, 400, 0, imaging.Lanczos), nil
 }
