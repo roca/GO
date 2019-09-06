21482f2d0a7ded7418a486d6b8858708faeb6979
roca <romel.campbell@regeneron.com>
2019-09-06 07:09:30 +0200

    CRUD for dynamodb Tables

--- udemy/Serverless/code/dynamodb/sandbox/table-ops.go
+++ udemy/Serverless/code/dynamodb/sandbox/table-ops.go
@@ -54,13 +54,53 @@ func main() {
 
 	// fmt.Println(result)
 
-	input := &dynamodb.DescribeTableInput{
-		TableName: aws.String("td_notes"),
+	// input := &dynamodb.DescribeTableInput{
+	// 	TableName: aws.String("td_notes_sdk"),
+	// }
+	// result, err := svc.DescribeTable(input)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	input := &dynamodb.CreateTableInput{
+		TableName: aws.String("td_notes_sdk"),
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{AttributeName: aws.String("user_id"), AttributeType: aws.String("S")},
+			{AttributeName: aws.String("timestamp"), AttributeType: aws.String("N")},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{AttributeName: aws.String("user_id"), KeyType: aws.String("HASH")},
+			{AttributeName: aws.String("timestamp"), KeyType: aws.String("RANGE")},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(1),
+			WriteCapacityUnits: aws.Int64(1),
+		},
 	}
-	result, err := svc.DescribeTable(input)
+	result, err := svc.CreateTable(input)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
+	// input := &dynamodb.UpdateTableInput{
+	// 	TableName: aws.String("td_notes_sdk"),
+	// 	ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+	// 		ReadCapacityUnits:  aws.Int64(2),
+	// 		WriteCapacityUnits: aws.Int64(1),
+	// 	},
+	// }
+	// result, err := svc.UpdateTable(input)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// input := &dynamodb.DeleteTableInput{
+	// 	TableName: aws.String("td_notes_sdk"),
+	// }
+	// result, err := svc.DeleteTable(input)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 
 	str, err := json.MarshalIndent(result, "", "\t")
 	if err != nil {
