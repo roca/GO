4da8bc60f2cac576366a2b5177f0d022c44d760a
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-09-15 05:39:56 -0400

    Altered eventLogger to Receive .stage from the API gateway

--- udemy/Serverless/code/event_logger/main.go
+++ udemy/Serverless/code/event_logger/main.go
@@ -4,17 +4,21 @@ import (
 	"context"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
 // Event ...
 type Event struct {
+	Stage          string
 	LambdaFunction string
 	LambdaVersion  string
 }
 
-func handler(c context.Context, event Event) (Event, error) {
-
-	return Event{LambdaFunction: c.functionName, LambdaVersion: c.functionVersion}, nil
+func handler(ctx context.Context, event Event) (Event, error) {
+	// Comment
+	event.LambdaFunction = lambdacontext.FunctionName
+	event.LambdaVersion = lambdacontext.FunctionVersion
+	return event, nil
 }
 
 func main() {
