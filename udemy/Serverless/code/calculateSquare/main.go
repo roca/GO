c63a65305c07ec96a86b93bfe1a7ac40311b42b3
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-09-29 19:57:58 -0400

    Example of lambda call another lambda

---
+++ udemy/Serverless/code/calculateSquare/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	svclambda "github.com/aws/aws-sdk-go/service/lambda"
+)
+
+var sess *session.Session
+
+func init() {
+	sess = session.Must(session.NewSession())
+
+}
+
+type Event struct {
+	Operation string
+	Input     struct {
+		Operand1 int
+		Operand2 int
+	}
+}
+
+type svcEvent struct {
+	Operation string `json:"Operation"`
+	Input     struct {
+		Operand1 int `json:"Operand1"`
+		Operand2 int `json:"Operand2"`
+	} `json:"Input"`
+}
+
+func handler(ctx context.Context, event Event) (events.APIGatewayProxyResponse, error) {
+	svc := svclambda.New(sess)
+
+	payload := svcEvent{Operation: event.Operation}
+	payload.Input.Operand1 = event.Input.Operand1
+	payload.Input.Operand2 = event.Input.Operand2
+
+	b, _ := json.Marshal(&payload)
+
+	input := &svclambda.InvokeInput{
+		//ClientContext: ctx,
+		FunctionName:   aws.String("calculator"),
+		InvocationType: aws.String("RequestResponse"),
+		Payload:        b,
+	}
+
+	res := events.APIGatewayProxyResponse{}
+
+	result, err := svc.Invoke(input)
+	if err != nil {
+		return res, err
+	}
+
+	if err := json.Unmarshal(result.Payload, &res); err != nil {
+		return res, err
+	}
+
+	log.Println(event)
+	log.Println(result)
+	return res, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- udemy/Serverless/code/go.mod
+++ udemy/Serverless/code/go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/aws/aws-lambda-go v1.10.0
 	github.com/aws/aws-sdk-go v1.19.40
+	github.com/disintegration/imaging v1.6.1
 	github.com/jboursiquot/shoutouts v0.0.0-20190724224516-20f77e089950
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
