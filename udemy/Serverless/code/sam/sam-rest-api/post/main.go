c1ee105a6b4ad04e256f4f20d314316c7be940f5
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-11-02 13:20:37 -0400

    sam-rest-api POST lambda fuction

--- udemy/Serverless/code/sam/sam-rest-api/post/main.go
+++ udemy/Serverless/code/sam/sam-rest-api/post/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var sess *session.Session
 var svc *dynamodb.DynamoDB
+var tableName = os.Getenv("TABLE_NAME")
 
 func init() {
 	sess = session.Must(session.NewSession())
@@ -43,4 +47,22 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 func main() {
 	lambda.Start(handler)
+}
+
+// Put into Database
+func Put(item Item) error {
+
+	_, err := svc.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"timestamp": {S: aws.String(fmt.Sprintf("%d", time.Now().UnixNano()))},
+			"userid":    {S: aws.String(item.UserID)},
+			"firstname": {S: aws.String(item.FirstName)},
+			"lastname":  {S: aws.String(item.LastName)},
+			"email":     {S: aws.String(item.Email)},
+			"website":   {S: aws.String(item.Website)},
+		},
+	})
+
+	return err
 }
