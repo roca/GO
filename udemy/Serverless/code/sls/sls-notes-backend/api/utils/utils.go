c92c57446c36bf65a16ad054c1a08c3a187881cb
roca <romel.campbell@regeneron.com>
2019-11-25 15:34:16 -0500

    WIP

--- udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
@@ -3,10 +3,10 @@ package utils
 // GetResponseHeaders return default response headers
 func GetResponseHeaders() map[string]string {
 
-	header := make(map[string]string)
+	headers := make(map[string]string)
 
-	header["key string"] = "value string"
+	headers["key string"] = "value string"
 
-	return header
+	return headers
 
 }
