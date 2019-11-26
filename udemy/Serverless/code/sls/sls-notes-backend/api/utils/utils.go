0cb88b0df89504fdf1f0dd843f42188d444682c0
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-11-26 03:55:31 -0500

    WIP: add a function to get user_id and name from the request header

--- udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
@@ -1,5 +1,19 @@
 package utils
 
+import (
+	"net/http"
+)
+
+// GetUserID return the User ID in the request header
+func GetUserID(header http.Header) string {
+	return header.Get("app_user_id")
+}
+
+// GetUserName return the User Name in the request header
+func GetUserName(header http.Header) string {
+	return header.Get("app_user_name")
+}
+
 // GetResponseHeaders return default response headers
 func GetResponseHeaders() map[string]string {
 
