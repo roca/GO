f00850b2fd169d4823217e09af2d3f7400281a01
roca <romel.campbell@regeneron.com>
2019-11-27 10:59:38 -0500

    WIP: Add Note lambda

--- udemy/Serverless/code/sls/sls-notes-backend/api/add-note/main.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/add-note/main.go
@@ -1,17 +1,23 @@
 package main
 
 /*
-	POST note
+	Route: POST /note
 */
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"time"
 
+	"encoding/json"
 	"os"
+
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"udemy.com/sls/sls-notes-backend/api/utils"
@@ -20,6 +26,14 @@ import (
 var sess *session.Session
 var svc *dynamodb.DynamoDB
 var tableName string
+
+type Item struct {
+	UserID    string `json:"user_id"`
+	UserName  string `json:"user_name"`
+	NoteID    string `json:"note_id"`
+	TimeStamp int64  `json:"timestamp"`
+	Expires   int64  `json:"expires"`
+}
 
 func init() {
 	sess = session.Must(session.NewSession())
@@ -30,10 +44,43 @@ func init() {
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
+	item := Item{}
+	uuid := uuid.NewV4()
+
+	if err := json.Unmarshal([]byte(event.Body), &item); err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	item.UserID = utils.GetUserID(event.Headers)
+	item.UserName = utils.GetUserName(event.Headers)
+	item.NoteID = fmt.Sprintf("%s:%s", item.UserID, uuid)
+	item.TimeStamp = time.Now().Unix()
+	item.Expires = time.Now().AddDate(0, 0, 90).Unix()
+
+	_, err := svc.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"user_id":   {S: aws.String(item.UserID)},
+			"user_name": {S: aws.String(item.UserName)},
+			"note_id":   {S: aws.String(item.NoteID)},
+			"timestamp": {N: aws.String(fmt.Sprintf("%d", item.TimeStamp))},
+			"expires":   {N: aws.String(fmt.Sprintf("%d", item.Expires))},
+		},
+	})
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	b, err := json.Marshal(&item)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	response := events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers:    utils.GetResponseHeaders(),
-		Body:       "Success",
+		Body:       string(b),
 	}
 
 	return response, nil
--- udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/utils/utils.go
@@ -1,17 +1,13 @@
 package utils
 
-import (
-	"net/http"
-)
-
 // GetUserID return the User ID in the request header
-func GetUserID(header http.Header) string {
-	return header.Get("app_user_id")
+func GetUserID(headers map[string]string) string {
+	return headers["app_user_id"]
 }
 
 // GetUserName return the User Name in the request header
-func GetUserName(header http.Header) string {
-	return header.Get("app_user_name")
+func GetUserName(headers map[string]string) string {
+	return headers["app_user_name"]
 }
 
 // GetResponseHeaders return default response headers
@@ -19,7 +15,7 @@ func GetResponseHeaders() map[string]string {
 
 	headers := make(map[string]string)
 
-	headers["key string"] = "value string"
+	headers["Access-Control-Allow-Origin"] = "*"
 
 	return headers
 
