3c7c2c24be516f7f4a3d6f9458d85c59f8feed5c
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-01-02 03:44:01 -0500

    Creates notes archive table

--- udemy/Serverless/code/sls/sls-notes-backend/api/archive/main.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/archive/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+var sess *session.Session
+var svc *dynamodb.DynamoDB
+var tableName string
 
 // Event ...
 type Event interface{}
 
-func handler(ctx context.Context, event Event) {
-	log.Println(event)
+func init() {
+	sess = session.Must(session.NewSession())
+	svc = dynamodb.New(sess)
+	tableName = os.Getenv("NOTES_ARCHIVE_TABLE")
+}
+
+func handler(ctx context.Context, event events.DynamoDBEvent) {
+	for _, record := range event.Records {
+		log.Println(record.EventName)
+	}
 }
 
 func main() {
