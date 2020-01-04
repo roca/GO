5f75d2fe9665facd1319f4b5086e69142303ab3e
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-01-03 21:14:28 -0500

    Archive works now

--- udemy/Serverless/code/sls/sls-notes-backend/api/archive/main.go
+++ udemy/Serverless/code/sls/sls-notes-backend/api/archive/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -12,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"udemy.com/sls/sls-notes-backend/api/models"
 )
 
 var sess *session.Session
@@ -27,26 +29,53 @@ func init() {
 	tableName = os.Getenv("NOTES_ARCHIVE_TABLE")
 }
 
+func UnmarshalStreamImage(attribute map[string]events.DynamoDBAttributeValue, out interface{}) error {
+
+	dbAttrMap := make(map[string]*dynamodb.AttributeValue)
+
+	for k, v := range attribute {
+
+		var dbAttr dynamodb.AttributeValue
+
+		bytes, marshalErr := v.MarshalJSON()
+		if marshalErr != nil {
+			return marshalErr
+		}
+
+		json.Unmarshal(bytes, &dbAttr)
+		dbAttrMap[k] = &dbAttr
+	}
+
+	return dynamodbattribute.UnmarshalMap(dbAttrMap, out)
+
+}
+
 func handler(ctx context.Context, event events.DynamoDBEvent) error {
+	var note models.Note
+
 	for _, record := range event.Records {
 
 		if record.EventName == "REMOVE" {
 
 			log.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
 
-			// Print new values for attributes of type String
-			for name, value := range record.Change.OldImage {
-				fmt.Printf("Attribute name: %s, value: %v\n", name, value)
-			}
-
-			av, err := dynamodbattribute.MarshalMap(record.Change.OldImage)
+			err := UnmarshalStreamImage(record.Change.OldImage, &note)
 			if err != nil {
 				return err
 			}
 
 			_, err = svc.PutItem(&dynamodb.PutItemInput{
 				TableName: aws.String(tableName),
-				Item:      av,
+				Item: map[string]*dynamodb.AttributeValue{
+					"user_id":   {S: aws.String(note.UserID)},
+					"user_name": {S: aws.String(note.UserName)},
+					"note_id":   {S: aws.String(note.NoteID)},
+					"timestamp": {N: aws.String(fmt.Sprintf("%d", note.TimeStamp))},
+					"expires":   {N: aws.String(fmt.Sprintf("%d", note.Expires))},
+					"cat":       {S: aws.String(note.Cat)},
+					"title":     {S: aws.String(note.Title)},
+					"content":   {S: aws.String(note.Content)},
+				},
 			})
 			if err != nil {
 				return err
