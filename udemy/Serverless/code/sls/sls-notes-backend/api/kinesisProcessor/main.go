d60982cb5d19ad6c7b11859e375407692853cd24
roca <romel.campbell@regeneron.com>
2020-01-06 09:32:17 -0500

    Change to build spec

---
+++ udemy/Serverless/code/sls/sls-notes-backend/api/kinesisProcessor/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"udemy.com/sls/sls-notes-backend/api/models"
+)
+
+var sess *session.Session
+var svc *dynamodb.DynamoDB
+var tableName string
+
+// Event ...
+type Event interface{}
+
+func init() {
+	sess = session.Must(session.NewSession())
+	svc = dynamodb.New(sess)
+	tableName = os.Getenv("NOTES_TABLE")
+}
+
+func handler(ctx context.Context, event events.KinesisEvent) error {
+	log.Println(event)
+
+	var writeRequests map[string][]*dynamodb.WriteRequest
+
+	for _, record := range event.Records {
+		dataText := string(record.Kinesis.Data)
+		log.Printf("%s Data = %s \n", record.EventName, dataText)
+
+		note := models.Note{}
+
+		if err := json.Unmarshal([]byte(dataText), &note); err != nil {
+			return err
+		}
+
+		writeRequests[tableName] = append(writeRequests[tableName], &dynamodb.WriteRequest{
+			PutRequest: &dynamodb.PutRequest{
+				Item: map[string]*dynamodb.AttributeValue{
+					"user_id":   {S: aws.String(note.UserID)},
+					"user_name": {S: aws.String(note.UserName)},
+					"note_id":   {S: aws.String(note.NoteID)},
+					"timestamp": {N: aws.String(fmt.Sprintf("%d", note.TimeStamp))},
+					"expires":   {N: aws.String(fmt.Sprintf("%d", note.Expires))},
+					"cat":       {S: aws.String(note.Cat)},
+					"title":     {S: aws.String(note.Title)},
+					"content":   {S: aws.String(note.Content)},
+				},
+			},
+		})
+	}
+	_, err := svc.BatchWriteItem(&dynamodb.BatchWriteItemInput{
+		RequestItems: writeRequests,
+	})
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
---
+++ udemy/Serverless/code/sls/sls-notes-backend/api/kinesisProducer/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+)
+
+var sess *session.Session
+var svc *kinesis.Kinesis
+var streamName *string
+
+func init() {
+	sess = session.Must(session.NewSession())
+	svc = kinesis.New(sess)
+	streamName = aws.String("ServerlessNotesStream")
+}
+
+func main() {
+
+	out, err := svc.CreateStream(&kinesis.CreateStreamInput{
+		ShardCount: aws.Int64(1),
+		StreamName: streamName,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", out)
+
+	if err := svc.WaitUntilStreamExists(&kinesis.DescribeStreamInput{StreamName: streamName}); err != nil {
+		panic(err)
+	}
+
+	streams, err := svc.DescribeStream(&kinesis.DescribeStreamInput{StreamName: streamName})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", streams)
+
+}
