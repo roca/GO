63b2098ddee7378ba9e988420f5fe526f2c73e56
roca <romel.campbell@regeneron.com>
2020-01-16 15:03:31 -0500

    Updates card titles in Handlers

--- udemy/Serverless/code/sls/alexa-serverless/alexa/handlers.go
+++ udemy/Serverless/code/sls/alexa-serverless/alexa/handlers.go
@@ -1,15 +1,11 @@
 package alexa
 
-func HandleFrontpageDealIntent(request Request) Response {
-	return NewSimpleResponse("Frontpage Deals", "Frontpage deal data here")
-}
-
 func HandleFallbackIntent(request Request) Response {
-	return NewSimpleResponse("Popular Deals", "Popular deal data here")
+	return NewSimpleResponse("Fallback", "Popular deal data here")
 }
 
 func HandleStopIntent(request Request) Response {
-	return NewSimpleResponse("Help", "Help regarding the available commands here")
+	return NewSimpleResponse("Stop", "Help regarding the available commands here")
 }
 
 func HandleHelpIntent(request Request) Response {
@@ -17,31 +13,31 @@ func HandleHelpIntent(request Request) Response {
 }
 
 func HandleCancelIntent(request Request) Response {
-	return NewSimpleResponse("Help", "Help regarding the available commands here")
+	return NewSimpleResponse("Cancel", "Help regarding the available commands here")
 }
 
 func HandleNavigateHomeIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("Navigate", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleGetNewFactIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("Get New Fact", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleAnotherFactIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("Another Fact", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleRepeatIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("Repeat", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleYesIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("Yes", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleNoIntent(request Request) Response {
-	return NewSimpleResponse("About", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
+	return NewSimpleResponse("No", "Slick Dealer was created by Nic Raboy in Tracy, California as an unofficial Slick Deals application.")
 }
 
 func HandleAboutIntent(request Request) Response {
