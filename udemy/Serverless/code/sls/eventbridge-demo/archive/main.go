4b7a03116b1220637d42d08e3fb28b011ce603b6
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-01-26 18:41:28 -0500

    WIP

---
+++ udemy/Serverless/code/sls/eventbridge-demo/archive/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+var sess *session.Session
+var svc *dynamodb.DynamoDB
+var tableName string
+
+type FileItem struct {
+	FileID          string            `json:"file_id"`
+	FileName        string            `json:"file_name"`
+	TimeStamp       int64             `json:"timestamp"`
+	Expires         int64             `json:"expires"`
+	AttributeValues map[string]string `json:"attribute_values"`
+}
+
+func init() {
+	sess = session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+	svc = dynamodb.New(sess)
+	tableName = os.Getenv("FILES_ARCHIVE_TABLE")
+}
+
+func UnmarshalStreamImage(attribute map[string]events.DynamoDBAttributeValue, out interface{}) error {
+
+	dbAttrMap := make(map[string]*dynamodb.AttributeValue)
+
+	for k, v := range attribute {
+
+		var dbAttr dynamodb.AttributeValue
+
+		bytes, marshalErr := v.MarshalJSON()
+		if marshalErr != nil {
+			return marshalErr
+		}
+
+		json.Unmarshal(bytes, &dbAttr)
+		dbAttrMap[k] = &dbAttr
+	}
+
+	return dynamodbattribute.UnmarshalMap(dbAttrMap, out)
+
+}
+
+func handler(ctx context.Context, event events.DynamoDBEvent) error {
+	var fileItem  FileItem
+
+	for _, record := range event.Records {
+
+		if record.EventName == "REMOVE" {
+
+			log.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
+
+			err := UnmarshalStreamImage(record.Change.OldImage, &fileItem)
+			if err != nil {
+				return err
+			}
+
+			b, err := json.Marshal(&fileItem.AttributeValues)
+		if err != nil {
+			return err
+		}
+
+
+			_, err = svc.PutItem(&dynamodb.PutItemInput{
+				TableName: aws.String(tableName),
+				Item: map[string]*dynamodb.AttributeValue{
+					"file_id":   {S: aws.String(fileItem.FileID)},
+					"file_name": {S: aws.String(fileItem.FileName)},
+					"timestamp": {N: aws.String(fmt.Sprintf("%d", fileItem.TimeStamp))},
+					"expires":   {N: aws.String(fmt.Sprintf("%d", fileItem.Expires))},
+				    "attributes_values":   {S: aws.String(string(b))},
+				},
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- udemy/Serverless/code/sls/eventbridge-demo/bob/main.go
+++ udemy/Serverless/code/sls/eventbridge-demo/bob/main.go
@@ -14,6 +14,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type Message struct {
@@ -23,17 +26,29 @@ type Message struct {
 	ObjectKey   string    `json:"objectKey"`
 }
 
+type FileItem struct {
+	FileID    string `json:"file_id"`
+	FileName  string `json:"file_name"`
+	TimeStamp int64  `json:"timestamp"`
+	Expires   int64  `json:"expires"`
+	AttributeValues map[string]string `json:"attribute_values"`
+}
+
 
 var sess *session.Session
 var svc *s3.S3
+var dbSvc *dynamodb.DynamoDB
 var bobsBucketName string
+var tableName string
 
 func init() {
 	sess = session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	}))
 	svc = s3.New(sess)
+	dbSvc = dynamodb.New(sess)
 	bobsBucketName = os.Getenv("BOBS_BUCKET_NAME")
+	tableName = os.Getenv("FILES_TABLE")
 }
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
@@ -63,7 +78,43 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) (Message, error)
 
 	log.Printf(string(b))
 
+	dbErr := processFile(message)
+	if dbErr != nil {
+		return Message{}, dbErr
+	}
+
 	return message, nil
+}
+
+func processFile(message Message) error {
+
+
+	uuid := uuid.NewV4()
+
+	fileItem := FileItem{}
+	
+	fileItem.FileID =  fmt.Sprintf("%s:%s", message.ObjectKey, uuid)
+	fileItem.FileName =  message.ObjectKey
+	fileItem.TimeStamp = time.Now().Unix()
+	fileItem.Expires = time.Now().AddDate(0, 0, 90).Unix()
+
+
+	_, err := dbSvc.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"file_id":   {S: aws.String(fileItem.FileID)},
+			"file_name": {S: aws.String(fileItem.FileName)},
+			"timestamp": {N: aws.String(fmt.Sprintf("%d", fileItem.TimeStamp))},
+			"expires":   {N: aws.String(fmt.Sprintf("%d", fileItem.Expires))},
+			"content":   {S: aws.String("")},
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
