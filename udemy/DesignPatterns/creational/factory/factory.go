6f67b4d4db6cb590c300c7af8f3c43818720e428
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-02-16 05:56:32 -0500

    Factory tests passing

---
+++ udemy/DesignPatterns/creational/factory/factory.go
@@ -0,0 +1,39 @@
+package factory
+
+import (
+	"errors"
+	"fmt"
+)
+
+type IPaymentMethod interface {
+	Pay(amount float32) string
+}
+
+//Our current implemented Payment methods ar described here
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+//CreditPaymentMethod returns a pointer to a PaymentMethod object or an error
+//if the method is not registered.
+func GetPaymentMethod(m int) (IPaymentMethod, error) {
+	paymentMethod := map[int]IPaymentMethod{
+		1: new(CashPM),
+		2: new(DebitCardPM),
+	}
+	if m, ok := paymentMethod[m]; ok {
+		return m, nil
+	}
+	return nil, errors.New(fmt.Sprintf("Payment method %d not recognized", m))
+}
+
+type CashPM struct{}
+type DebitCardPM struct{}
+
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("payed using cash. amount: %#0.2f", amount)
+}
+func (c *DebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("payed using debit card. amount: %#0.2f", amount)
+}
