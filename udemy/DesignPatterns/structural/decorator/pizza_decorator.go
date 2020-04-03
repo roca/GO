2fe41f7b3716b11d06dc477623ca4d63a8ea47a0
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-04-03 15:36:55 -0400

    Decorator Pattern

---
+++ udemy/DesignPatterns/structural/decorator/pizza_decorator.go
@@ -0,0 +1,49 @@
+package decorator
+
+import (
+	"errors"
+	"fmt"
+)
+
+type IngredientAdd interface {
+	AddIngredient() (string, error)
+}
+type PizzaDecorator struct {
+	Ingredient IngredientAdd
+}
+
+func (p *PizzaDecorator) AddIngredient() (string, error) {
+	return "Pizza with the following ingredients:", nil
+}
+
+type Meat struct {
+	Ingredient IngredientAdd
+}
+
+func (m *Meat) AddIngredient() (string, error) {
+	if m.Ingredient == nil {
+		return "", errors.New("An IngredientAdd is needed in the Ingredient field of the Meat")
+	}
+	s, err := m.Ingredient.AddIngredient()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s meat,", s), nil
+}
+
+type Onion struct {
+	Ingredient IngredientAdd
+}
+
+func (o *Onion) AddIngredient() (string, error) {
+	if o.Ingredient == nil {
+		return "", errors.New("An IngredientAdd is needed in the Ingredient field of the Onion")
+	}
+	s, err := o.Ingredient.AddIngredient()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s onion,", s), nil
+}
