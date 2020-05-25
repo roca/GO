11e43983b8c8c3700cad3b0291b9ad155b92bae5
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-05-25 08:43:15 -0400

    Template design pattern

---
+++ udemy/DesignPatterns/behavioural/template/sort/sort_example.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*type Interface interface {
+		Len() int
+		Less(i,j int) bool
+		Swap(i,j int)
+}*/
+
+type MyList []int
+
+func (m MyList) Len() int {
+	return len(m)
+}
+func (m MyList) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+func (m MyList) Less(i, j int) bool {
+	return m[i] < m[j]
+}
+
+func main() {
+	var myList MyList = []int{6, 4, 2, 8, 1}
+
+	fmt.Println(myList)
+	sort.Sort(myList)
+	fmt.Println(myList)
+}
