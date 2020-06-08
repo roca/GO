9b5c67b355cc7228819c058bb8b41a1cb36ba4b7
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-06-07 20:59:02 -0400

    Second round of test passing for refactored interpreter pattern

--- udemy/DesignPatterns/behavioural/interpreter/interpreter.go
+++ udemy/DesignPatterns/behavioural/interpreter/interpreter.go
@@ -8,28 +8,64 @@ import (
 const (
 	SUM = "sum"
 	SUB = "sub"
-	MUL = "mul"
-	DIV = "div"
 )
 
-func getOperationFunc(o string) func(a, b int) int {
+//-------------------------------------------------------------------------------
+type Interpreter interface {
+	Read() int
+}
+
+type value int
+
+func (v *value) Read() int {
+	return int(*v)
+}
+
+type operationSum struct {
+	Left  Interpreter
+	Right Interpreter
+}
+
+func (a *operationSum) Read() int {
+	return a.Left.Read() + a.Right.Read()
+}
+
+type operationSubtract struct {
+	Left  Interpreter
+	Right Interpreter
+}
+
+func (a *operationSubtract) Read() int {
+	return a.Left.Read() - a.Right.Read()
+}
+
+func operatorFactory(o string, left, right Interpreter) Interpreter {
 	switch o {
 	case SUM:
-		return func(a, b int) int {
-			return a + b
+		return &operationSum{
+			Left:  left,
+			Right: right,
 		}
 	case SUB:
-		return func(a, b int) int {
-			return a - b
+		return &operationSubtract{
+			Left:  left,
+			Right: right,
 		}
-	case MUL:
-		return func(a, b int) int {
-			return a * b
-		}
-	case DIV:
-		return func(a, b int) int {
-			return a / b
-		}
+	}
+	return nil
+}
+
+type polishNotationStack []Interpreter
+
+func (p *polishNotationStack) Push(s Interpreter) {
+	*p = append(*p, s)
+}
+func (p *polishNotationStack) Pop() Interpreter {
+	length := len(*p)
+	if length > 0 {
+		temp := (*p)[length-1]
+		*p = (*p)[:length-1]
+		return temp
 	}
 	return nil
 }
@@ -38,42 +74,21 @@ func Calculate(o string) (int, error) {
 	stack := polishNotationStack{}
 	operators := strings.Split(o, " ")
 	for _, v := range operators {
-		if isOperator(v) {
+		if v == SUM || v == SUB {
 			right := stack.Pop()
 			left := stack.Pop()
-			mathFunc := getOperationFunc(v)
-			res := mathFunc(left, right)
-			stack.Push(res)
+			mathFunc := operatorFactory(v, left, right)
+			res := value(mathFunc.Read())
+			stack.Push(&res)
 		} else {
 			val, err := strconv.Atoi(v)
 			if err != nil {
-				return 0, err
+				panic(err)
 			}
-			stack.Push(val)
+			temp := value(val)
+			stack.Push(&temp)
 		}
 
 	}
-	return int(stack.Pop()), nil
-}
-
-func isOperator(o string) bool {
-	if o == SUM || o == SUB || o == MUL || o == DIV {
-		return true
-	}
-	return false
-}
-
-type polishNotationStack []int
-
-func (p *polishNotationStack) Push(s int) {
-	*p = append(*p, s)
-}
-func (p *polishNotationStack) Pop() int {
-	length := len(*p)
-	if length > 0 {
-		temp := (*p)[length-1]
-		*p = (*p)[:length-1]
-		return temp
-	}
-	return 0
+	return int(stack.Pop().Read()), nil
 }
