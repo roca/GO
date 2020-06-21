09e93928fb812959f9680af133ca2e53d6f38c4e
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-06-21 05:46:01 -0400

    Done with state pattern. Now starting the mediator pattern

---
+++ udemy/DesignPatterns/behavioural/mediator/mediator.go
@@ -0,0 +1,12 @@
+package main 
+
+type One struct {}
+type Two struct {}
+type Three struct {}
+type Four struct {}
+type Five struct {}
+type Six struct {}
+type Seven struct {}
+type Eight struct {}
+type Nine struct {}
+type Zero struct {}
