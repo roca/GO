aa59afc50e4516c81e76118dff2c74251f3c2b1d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-07-13 11:29:00 -0400

    Adds theme.js for Sigin Look and feel

--- udemy/AppSync/blogfinal/src/App.js
+++ udemy/AppSync/blogfinal/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Bootstrap from "./theme";
 import DisplayPosts from './components/DisplayPosts';
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
@@ -14,5 +15,6 @@ function App() {
 }
 
 export default withAuthenticator(App,{
-  includeGreetings: true
+  includeGreetings: true,
+  theme: Bootstrap
 });
