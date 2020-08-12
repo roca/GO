cb4edc2695d78989ba656cff1123492a695cf41d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-08-12 16:16:57 -0400

    Packet sniffer

---
+++ udemy/Networking/README.md
@@ -0,0 +1,9 @@
+
+# Course links
+
+- https://www.udemy.com/course/network-programming-in-golang-part-1
+
+- https://www.wireshark.org/
+
+
+Deprecated: this package is locked down. Callers should use the corresponding package in the golang.org/x/sys repository instead. That is also where updates required by new systems or versions should be applied. See https://golang.org/s/go1.4-syscall for more information.
---
+++ udemy/Networking/netutils/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+
+	//devices()
+	//capture()
+
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println("======================================================================================")
+		printPacketInfo(packet)
+	}
+}
