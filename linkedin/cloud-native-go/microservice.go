c80fe1b5262647d6f2bc08d84c9c96e4b4c91c38
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-12-22 14:52:19 -0500

    12222020/14:51

--- linkedin/cloud-native-go/microservice.go
+++ linkedin/cloud-native-go/microservice.go
@@ -3,14 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello Cloud Native Go.")
+	fmt.Fprint(w, "Hello Cloud Native Go.")
+}
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprint(w, message)
+
 }
