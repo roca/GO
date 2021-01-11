b2ccad22d9e931b87336c57c437bcb330309f26c
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-01-11 11:22:45 -0500

    Work with docker buildx

--- linkedin/cloud-native-go/docker_build.sh
+++ linkedin/cloud-native-go/docker_build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh 
 
 # CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o cloud-native-go
-docker build -t cloud-native-go:1.0.2 .
+# docker build -t cloud-native-go:1.0.2 .
 
-docker tag cloud-native-go:1.0.2 rcampbell/cloud-native-go:1.0.2
+docker buildx use rpi3-ssh
+docker buildx build -t rcampbell/cloud-native-go:1.0.2 --platform linux/arm/v7  .
+
+# docker tag cloud-native-go:1.0.2 rcampbell/cloud-native-go:1.0.2
 docker push rcampbell/cloud-native-go:1.0.2
