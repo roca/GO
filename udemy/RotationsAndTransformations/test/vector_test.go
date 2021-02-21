156cc02a215f9cee0e6775e3ec6049b992f327c9
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-02-21 14:59:47 -0500

    WIP: New function to handle test cases

--- udemy/RotationsAndTransformations/test/vector_test.go
+++ udemy/RotationsAndTransformations/test/vector_test.go
@@ -1,32 +1,11 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gonum.org/v1/gonum/mat"
-	"udemy.com/aml"
 	"udemy.com/aml/vector"
 )
-
-func TestMatPrint(t *testing.T) {
-	v := make([]float64, 12)
-	for i := 0; i < 12; i++ {
-		v[i] = float64(i)
-	}
-	// Create a new matrix
-	A := mat.NewDense(3, 4, v)
-	println("A:")
-	formatedString := aml.MatPrint(A)
-
-	fa := mat.Formatted(A, mat.Prefix(""), mat.Squeeze())
-
-	assert.Equal(t, formatedString, fmt.Sprintf("%v\n", fa), "Matrix string output did not match")
-
-}
-
-// Vectot test
 
 // Case 01: Default valuses for Vector should be 0.0
 func TestCase01(t *testing.T) {
@@ -60,9 +39,9 @@ func TestCase03(t *testing.T) {
 
 // Case 03: Should initialize with slice of three values
 func TestCase04(t *testing.T) {
-	v, _ := vector.New([]float64{1.0, 2.0, 3.0})
+	v, _ := vector.New([]float64{3.0, 2.0, 1.0})
 
-	expected := []float64{1.0, 2.0, 3.0}
+	expected := []float64{3.0, 2.0, 1.0}
 	actual := []float64{v.X, v.Y, v.Z}
 
 	assert.Equal(t, actual, expected, "Should initialize with slice of three values")
