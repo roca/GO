ddaef65790d9d1b64b874f3e48c04cc1a357b230
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-02-28 19:47:45 -0500

    WIP: Matrix test suit

--- udemy/RotationsAndTransformations/test/matrix_test.go
+++ udemy/RotationsAndTransformations/test/matrix_test.go
@@ -1,26 +1,42 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gonum.org/v1/gonum/mat"
-	"udemy.com/aml"
+	"udemy.com/aml/matrix"
 )
 
-func TestMatPrint(t *testing.T) {
-	v := make([]float64, 12)
-	for i := 0; i < 12; i++ {
-		v[i] = float64(i)
+// Case 01: Default valuses for Matrixshould be 0.0
+func TestCase01(t *testing.T) {
+	m := matrix.Matrix{}
+
+	expected := []float64{
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
 	}
-	// Create a new matrix
-	A := mat.NewDense(3, 4, v)
-	println("A:")
-	formatedString := aml.MatPrint(A)
+	actual := []float64{
+		m.M11, m.M12, m.M13,
+		m.M21, m.M22, m.M23,
+		m.M31, m.M32, m.M33,
+	}
 
-	fa := mat.Formatted(A, mat.Prefix(""), mat.Squeeze())
-
-	assert.Equal(t, formatedString, fmt.Sprintf("%v\n", fa), "Matrix string output did not match")
-
+	assert.Equal(t, actual, expected, "Default valuses for Vector should be 0.0 float64")
 }
+
+// func TestMatPrint(t *testing.T) {
+// 	v := make([]float64, 12)
+// 	for i := 0; i < 12; i++ {
+// 		v[i] = float64(i)
+// 	}
+// 	// Create a new matrix
+// 	A := mat.NewDense(3, 4, v)
+// 	println("A:")
+// 	formatedString := aml.MatPrint(A)
+
+// 	fa := mat.Formatted(A, mat.Prefix(""), mat.Squeeze())
+
+// 	assert.Equal(t, formatedString, fmt.Sprintf("%v\n", fa), "Matrix string output did not match")
+
+// }
