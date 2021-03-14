0847ed2aa433b7991e78f9f8c15a9737fca15eba
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-03-14 14:37:24 -0400

    WIP: Wrote to tests

--- udemy/RotationsAndTransformations/test/matrix_test.go
+++ udemy/RotationsAndTransformations/test/matrix_test.go
@@ -96,9 +96,9 @@ func TestConstructWith2DSlice(t *testing.T) {
 }
 
 func TestConstructWithVectors(t *testing.T) {
-	v1,_ := vector.New(1., 2., 3.)
-	v2,_ := vector.New(4., 5., 6.)
-	v3,_ := vector.New(7., 8., 9.)
+	v1, _ := vector.New(1., 2., 3.)
+	v2, _ := vector.New(4., 5., 6.)
+	v3, _ := vector.New(7., 8., 9.)
 	m, _ := matrix.New(v1, v2, v3)
 
 	expected := []float64{
@@ -114,9 +114,9 @@ func TestConstructWithVectors(t *testing.T) {
 	assert.Equal(t, actual, expected, "Valuses for Matrix for each element incorrectly assigned")
 }
 func TestConstructWithSliceOfVectors(t *testing.T) {
-	v1,_ := vector.New(1., 2., 3.)
-	v2,_ := vector.New(4., 5., 6.)
-	v3,_ := vector.New(7., 8., 9.)
+	v1, _ := vector.New(1., 2., 3.)
+	v2, _ := vector.New(4., 5., 6.)
+	v3, _ := vector.New(7., 8., 9.)
 	m, _ := matrix.New([]vector.Vector{v1, v2, v3})
 
 	expected := []float64{
@@ -131,26 +131,54 @@ func TestConstructWithSliceOfVectors(t *testing.T) {
 	}
 	assert.Equal(t, actual, expected, "Valuses for Matrix for each element incorrectly assigned")
 }
+
 //Operations with a matrix
-func TestAdditionWithMatrix(t *testing.T){}
-func TestSubtractionWithMatrix(t *testing.T){}
-func TestMultiplicationWithMatrix(t *testing.T){}
-func TestDivisionWithMatrix(t *testing.T){}
+func TestAdditionWithMatrix(t *testing.T)       {}
+func TestSubtractionWithMatrix(t *testing.T)    {}
+func TestMultiplicationWithMatrix(t *testing.T) {}
+func TestDivisionWithMatrix(t *testing.T)       {}
 
 //Operations with a vector
-func TestMultiplicationWithVector(t *testing.T){}
+func TestMultiplicationWithVector(t *testing.T) {}
 
 //Operations with a scalar
-func TestAdditionWithScalar(t *testing.T){}
-func TestSubtractionWithScalar(t *testing.T){}
-func TestMultiplicationWithScalar(t *testing.T){}
-func TestDivisionWithScalar(t *testing.T){}
+func TestAdditionWithScalar(t *testing.T)       {}
+func TestSubtractionWithScalar(t *testing.T)    {}
+func TestMultiplicationWithScalar(t *testing.T) {}
+func TestDivisionWithScalar(t *testing.T)       {}
 
 //Special operations
-func TestDiagM(t *testing.T){}
-func TestDiagV(t *testing.T){}
-func TestTranspose(t *testing.T){}
-func TestDeterminant(t *testing.T){}
-func TestInverse(t *testing.T){}
-func TestIdentity(t *testing.T){}
-func TestNegative(t *testing.T){}
+func TestDiagM(t *testing.T)       {}
+func TestDiagV(t *testing.T)       {}
+func TestTranspose(t *testing.T)   {}
+func TestDeterminant(t *testing.T) {}
+func TestInverse(t *testing.T)     {}
+func TestIdentity(t *testing.T)    {
+
+	expected := [][]float64{
+		{1., 0., 0.},
+		{0., 1., 0.},
+		{0., 0., 1.},
+	}
+	identityM := matrix.Identity()
+	actual := identityM.Data()
+
+	assert.Equal(t, actual, expected, "All valuse should along the diaganal should be equal to 1.0. All others 0.0")
+
+}
+func TestNegative(t *testing.T) {
+	m, _ := matrix.New([][]float64{
+		{1., 2., 3.},
+		{4., 5., 6.},
+		{7., 8., 9.},
+	})
+	expected := [][]float64{
+		{-1., -2., -3.},
+		{-4., -5., -6.},
+		{-7., -8., -9.},
+	}
+	negativeM, _ := m.Negate()
+	actual := negativeM.Data()
+
+	assert.Equal(t, actual, expected, "All valuse should be the negative of the original")
+}
