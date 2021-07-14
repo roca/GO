2ee23d503e226af937958b04a1abcb12b54d125a
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-07-14 03:50:16 -0400

    07142021/03:49: Uses html form data

--- udemy/DataScienceNLP/main.go
+++ udemy/DataScienceNLP/main.go
@@ -59,9 +59,14 @@ func fiberExample03() {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		message := "Hello Data Scientist & Developers"
+		initMessage := "Hello Data Scientist & Developers"
+		fname := c.FormValue("firstname")
+		message := c.FormValue("message")
+
 		return c.Render("index", fiber.Map{
-			"coolMessage": message,
+			"coolMessage": initMessage,
+			"firstName": fname,
+			"newMessage": message,
 		})
 
 	})
