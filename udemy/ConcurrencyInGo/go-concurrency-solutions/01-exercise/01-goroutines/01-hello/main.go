4989a80d4f2f181236072a44c46473f3e181b1c9
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-07-30 04:49:10 -0400

    Solutions folder

---
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/01-goroutines/01-hello/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fun(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func main() {
+	// Direct call
+	fun("direct call")
+
+	// TODO: write goroutine with different variants for function call.
+
+	// goroutine function call
+	go fun("goroutine 1")
+
+	// goroutine with anonymous function
+	go func() {
+		go fun("goroutine 2")
+	}()
+
+	// goroutine with function value call
+	fv := fun
+	go fv("goroutine 3")
+
+	// wait for goroutines to end
+
+	fmt.Println("wait for goroutine..")
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("done..")
+}
---
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/01-goroutines/02-client-server/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	// TODO: connect to server on localhost port 8000
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+
+}
+
+// mustCopy - utility function
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
