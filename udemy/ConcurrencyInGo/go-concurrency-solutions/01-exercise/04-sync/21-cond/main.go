1ef19667cc23cd8ba43f8982fe98faec5f9b485b
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-08-09 05:20:04 -0400

    Learning how sync.Signal and Broacast work

--- udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/04-sync/21-cond/main.go
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/04-sync/21-cond/main.go
@@ -3,29 +3,35 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var sharedRsc = make(map[string]interface{})
 
 func main() {
 	var wg sync.WaitGroup
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		//TODO: suspend goroutine until sharedRsc is populated.
+		// suspend goroutine until sharedRsc is populated.
 
+		c.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc1"])
+		c.L.Unlock()
 	}()
 
 	// writes changes to sharedRsc
+	c.L.Lock()
 	sharedRsc["rsc1"] = "foo"
+	c.Signal()
+	c.L.Unlock()
 
 	wg.Wait()
 }
--- udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/04-sync/22-cond/main.go
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/01-exercise/04-sync/22-cond/main.go
@@ -3,25 +3,27 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var sharedRsc = make(map[string]interface{})
 
 func main() {
 	var wg sync.WaitGroup
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
 		//TODO: suspend goroutine until sharedRsc is populated.
-
+		c.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc1"])
+		c.L.Unlock()
 	}()
 
 	wg.Add(1)
@@ -29,17 +31,21 @@ func main() {
 		defer wg.Done()
 
 		//TODO: suspend goroutine until sharedRsc is populated.
-
+		c.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc2"])
+		c.L.Unlock()
 	}()
 
 	// writes changes to sharedRsc
+	c.L.Lock()
 	sharedRsc["rsc1"] = "foo"
 	sharedRsc["rsc2"] = "bar"
+	c.Broadcast()
+	c.L.Unlock()
 
 	wg.Wait()
 }
