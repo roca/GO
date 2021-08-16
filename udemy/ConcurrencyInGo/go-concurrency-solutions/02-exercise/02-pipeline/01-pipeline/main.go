bd54f12f55c59088ddf4f4274aa3c39b05ad90a9
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-08-16 05:31:27 -0400

    Fan in example

--- udemy/ConcurrencyInGo/go-concurrency-solutions/02-exercise/02-pipeline/01-pipeline/main.go
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/02-exercise/02-pipeline/01-pipeline/main.go
@@ -1,18 +1,34 @@
 package main
 
+import "fmt"
+
 // TODO: Build a Pipeline
 // generator() -> square() -> print
 
 // generator - convertes a list of integers to a channel
-func generator(nums ...int) {
-
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			out <- n
+		}
+	}()
+	return out
 }
 
 // square - receive on inbound channel
 // square the number
 // output on outbound channel
-func square() {
-
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			out <- n * n
+		}
+	}()
+	return out
 }
 
 func main() {
@@ -21,5 +37,10 @@ func main() {
 	// run the last stage of pipeline
 	// receive the values from square stage
 	// print each one, until channel is closed.
+
+	out := square(square(generator(1, 2, 3, 4, 5)))
+	for i := range out {
+		fmt.Println(i)
+	}
 
 }
--- udemy/ConcurrencyInGo/go-concurrency-solutions/02-exercise/02-pipeline/02-pipeline/main.go
+++ udemy/ConcurrencyInGo/go-concurrency-solutions/02-exercise/02-pipeline/02-pipeline/main.go
@@ -2,6 +2,11 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func generator(nums ...int) <-chan int {
 	out := make(chan int)
 
@@ -28,13 +33,40 @@ func square(in <-chan int) <-chan int {
 func merge(cs ...<-chan int) <-chan int {
 	// Implement fan-in
 	// merge a list of channels to a single channel
+	out := make(chan int)
+	var wg sync.WaitGroup
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+
 }
 
 func main() {
 	in := generator(2, 3)
 
 	// TODO: fan out square stage to run two instances.
+	ch1 := square(in)
+	ch2 := square(in)
 
 	// TODO: fan in the results of square stages.
+
+	out := merge(ch1, ch2)
+	for i := range out {
+		fmt.Println(i)
+	}
 
 }
