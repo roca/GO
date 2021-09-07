89d77e91e1fb673fa0a375bbfa30d858d370d5f4
romel.campbell <romel.campbell@regeneron.com>
2021-09-07 10:54:49 -0400

    Modeul01 Lesson 6

--- calhoun.io/algorithmswithgo.com/module01/sum.go
+++ calhoun.io/algorithmswithgo.com/module01/sum.go
@@ -2,6 +2,19 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _, v := range numbers {
+// 		sum += v
+// 	}
+// 	return sum
+// }
+
+// Recursive approach
 func Sum(numbers []int) int {
-	return 0
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + Sum(numbers[1:])
 }
+
