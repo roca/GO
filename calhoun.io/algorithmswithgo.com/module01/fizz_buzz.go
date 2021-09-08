4bb857f974fb78db08f554af198e04fd98ddfef3
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-09-08 04:23:44 -0400

    WIP: M1 L8

--- calhoun.io/algorithmswithgo.com/module01/fizz_buzz.go
+++ calhoun.io/algorithmswithgo.com/module01/fizz_buzz.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"fmt"
+	"strings"
+)
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +16,17 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	s := []string{}
+	for i := 1; 1 <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			s = append(s, "Fizz Buzz")
+		} else if i%3 == 0 {
+			s = append(s, "Fizz")
+		} else if i%5 == 0 {
+			s = append(s, "Buzz")
+		} else {
+			s = append(s, fmt.Sprintf("%d", i))
+		}
+	}
+	fmt.Println(strings.Join(s, ", "))
 }
