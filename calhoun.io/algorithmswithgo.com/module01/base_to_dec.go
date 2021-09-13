fb5e65a40d12ebd0df014fcaafe9460d56f207eb
Romel Campbell <RomelCampbell@gmail.com>
2021-09-13 09:36:39 -0400

    BaseToDec

--- calhoun.io/algorithmswithgo.com/module01/base_to_dec.go
+++ calhoun.io/algorithmswithgo.com/module01/base_to_dec.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"math"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +13,16 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	var d float64
+	r := []rune(value)
+	for i := len(r); i > 0; i-- {
+		n := int(r[i-1])
+		if n >= 65 && n <= 90 {
+			n -= 55
+		} else if n >= 48 && n <= 57 {
+			n -= 48
+		}
+		d += float64(n) * math.Pow(float64(base), float64(len(r)-i))
+	}
+	return int(d)
 }
--- calhoun.io/algorithmswithgo.com/module01/dec_to_base.go
+++ calhoun.io/algorithmswithgo.com/module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +12,14 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	s := ""
+	for i := dec; i > 0; i /= base {
+		m := i % base
+		if m < 10 {
+			s = fmt.Sprintf("%d", m) + s
+		} else {
+			s = fmt.Sprintf("%c", rune(m+55)) + s
+		}
+	}
+	return s
 }
