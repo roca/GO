d289881db44d67ed3a5ee325d568fc96f563bca4
Romel Campbell <RomelCampbell@gmail.com>
2021-09-17 13:34:57 -0400

    GCD

--- calhoun.io/algorithmswithgo.com/module01/gcd.go
+++ calhoun.io/algorithmswithgo.com/module01/gcd.go
@@ -1,5 +1,15 @@
 package module01
 
+// func GCD(a, b int) int {
+// 	if b == 0 {
+// 		return a
+// 	}
+// 	return GCD(b, a%b)
+// }
+
 func GCD(a, b int) int {
-	return 0
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
