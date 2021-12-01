8f83aee464adb239ffa76cc99cebba654643c23c
romel.campbell <romel.campbell@regeneron.com>
2021-12-01 14:18:35 -0500

    ISP

---
+++ udemy/DesignPatterns2/SOLID/dip/main.go
@@ -0,0 +1,9 @@
+package main
+
+/* 
+   DIP: Dependency Inversion Principle:
+   
+   DIP is a software design principle that states that 
+   "High-level modules should not depend on low-level modules. 
+   Both should depend on abstractions."
+*/
