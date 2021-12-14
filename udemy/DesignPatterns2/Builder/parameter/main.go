2e8119e49b97a77a472648441abd1e26cf08e401
Romel Campbell <RomelCampbell@gmail.com>
2021-12-14 03:54:01 -0500

    WIP: BuilderParameter

---
+++ udemy/DesignPatterns2/Builder/parameter/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "strings"
+
+type email struct {
+	from, to, subject, body string
+}
+
+type EmailBuilder struct {
+	email *email
+}
+
+func (b *EmailBuilder) From(from string) *EmailBuilder {
+	if !strings.Contains(from, "@") {
+		panic("Invalid email address")
+	}
+	b.email.from = from
+	return b
+}
+
+func (b *EmailBuilder) To(to string) *EmailBuilder {
+	b.email.to = to
+	return b
+}
+
+func (b *EmailBuilder) Subject(subject string) *EmailBuilder {
+	b.email.subject = subject
+	return b
+}
+
+func (b *EmailBuilder) Body(body string) *EmailBuilder {
+	b.email.body = body
+	return b
+}
+
+func sendMailImpl(email *email) {
+
+}
+
+type build func(*EmailBuilder)
