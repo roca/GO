096fa0a758f40e2cab24452a0ebe0bd9bb57868b
romel.campbell <romel.campbell@regeneron.com>
2022-06-20 11:35:57 -0400

    Syncs the velocity of the boid with the boids in its viewRadius

--- udemy/multithreadingingo/boids/boid.go
+++ udemy/multithreadingingo/boids/boid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -19,13 +20,35 @@ func (b *Boid) start() {
 }
 
 func (b *Boid) calcAcceleration() Vector2D {
+	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
+	avgVelocity := Vector2D{x: 0, y: 0}
+	count := 0.0
+
+	for i := math.Max(lower.x, 0); i < math.Min(upper.x, screenWidth); i++ {
+		for j := math.Max(lower.y, 0); j < math.Min(upper.y, screenHeight); j++ {
+			if otherBoidId := boidMap[int(i)][int(j)]; otherBoidId != -1 && otherBoidId != b.id {
+				if dist := boids[otherBoidId].position.Distance(b.position); dist < viewRadius {
+					count++
+					avgVelocity = avgVelocity.Add(boids[otherBoidId].velocity)
+				}
+			}
+		}
+	}
+
 	accel := Vector2D{x: 0, y: 0}
+	if count > 0 {
+		avgVelocity = avgVelocity.DivisionV(count)
+		accel = avgVelocity.Subtract(b.velocity).MultiplyV(adjRate)
+	}
 
 	return accel
 }
 
 func (b *Boid) moveOne() {
+
+	// This will sync the velocity of the boid with the boids in its viewRadius
 	b.velocity = b.velocity.Add(b.calcAcceleration()).limit(-1, 1)
+
 	boidMap[int(b.position.x)][int(b.position.y)] = -1
 	b.position = b.position.Add(b.velocity)
 	boidMap[int(b.position.x)][int(b.position.y)] = b.id
