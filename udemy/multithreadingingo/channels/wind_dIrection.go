f1109d41e4945ecd4e7deb2ba867aaae022d979d
romel.campbell <romel.campbell@regeneron.com>
2022-06-27 10:08:36 -0400

    after channel implementation

--- udemy/multithreadingingo/channels/wind_dIrection.go
+++ udemy/multithreadingingo/channels/wind_dIrection.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -22,56 +23,81 @@ var (
 	windDist      [8]int
 )
 
-func parseToArray(text string) []string {
+func parseToArray(textChannel chan string, metarChannel chan []string) {
 
-	lines := strings.Split(text, "\n")
-	metarSlice := make([]string, 0, len(lines))
-	metarStr := ""
-	for _, line := range lines {
-		if tafValidation.MatchString(line) {
-			break
-		}
-		if !comment.MatchString(line) {
-			metarStr += strings.Trim(line, " ")
-		}
-		if metarClose.MatchString(line) {
-			metarSlice = append(metarSlice, metarStr)
-			metarStr = ""
-		}
-	}
+	for text := range textChannel {
 
-	return metarSlice
-}
-
-func extractWindDirection(metars []string) []string {
-
-	winds := make([]string, 0, len(metars))
-	for _, metar := range metars {
-		if windRegex.MatchString(metar) {
-			winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
-		}
-	}
-
-	return winds
-}
-
-func mineWindDistribution(winds []string) {
-	for _, wind := range winds {
-		if variableWind.MatchString(wind) {
-			for i := 0; i < 8; i++ {
-				windDist[i]++
+		lines := strings.Split(text, "\n")
+		metarSlice := make([]string, 0, len(lines))
+		metarStr := ""
+		for _, line := range lines {
+			if tafValidation.MatchString(line) {
+				break
 			}
-		} else if validWind.MatchString(wind) {
-			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
-			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
-				dirIndex := int(math.Round(d/45.0)) % 8
-				windDist[dirIndex]++
+			if !comment.MatchString(line) {
+				metarStr += strings.Trim(line, " ")
+			}
+			if metarClose.MatchString(line) {
+				metarSlice = append(metarSlice, metarStr)
+				metarStr = ""
 			}
 		}
+
+		metarChannel <- metarSlice
 	}
+
+	close(metarChannel)
+}
+
+func extractWindDirection(metarChannel chan []string, windsChannel chan []string) {
+	for metars := range metarChannel {
+		winds := make([]string, 0, len(metars))
+		for _, metar := range metars {
+			if windRegex.MatchString(metar) {
+				winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
+			}
+		}
+
+		windsChannel <- winds
+	}
+	close(windsChannel)
+}
+
+func mineWindDistribution(windsChannel chan []string, distChannel chan [8]int) {
+	for winds := range windsChannel {
+		for _, wind := range winds {
+			if variableWind.MatchString(wind) {
+				for i := 0; i < 8; i++ {
+					windDist[i]++
+				}
+			} else if validWind.MatchString(wind) {
+				windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+				if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+					dirIndex := int(math.Round(d/45.0)) % 8
+					windDist[dirIndex]++
+				}
+			}
+		}
+	}
+	distChannel <- windDist
+	close(distChannel)
 }
 
 func main() {
+	cores := 2
+	runtime.GOMAXPROCS(cores)
+	textChannel := make(chan string)
+	metarChannel := make(chan []string)
+	windsChannel := make(chan []string)
+	resultsChannel := make(chan [8]int)
+
+	//1. Change to array, each metar report is a separate item in the array
+	go parseToArray(textChannel, metarChannel)
+	//2. Extract wind dirextion, EGLL 312350Z 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
+	go extractWindDirection(metarChannel, windsChannel)
+	//3. Asign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
+	go mineWindDistribution(windsChannel, resultsChannel)
+
 	absPath, _ := filepath.Abs("./metarfiles/")
 	files, _ := ioutil.ReadDir(absPath)
 	start := time.Now()
@@ -81,11 +107,16 @@ func main() {
 			panic(err)
 		}
 		text := string(dat)
-		metarsReports := parseToArray(text)
-		windDirections := extractWindDirection(metarsReports)
-		mineWindDistribution(windDirections)
+		textChannel <- text
 	}
+	close(textChannel)
+	results := <-resultsChannel
 	elapsed := time.Since(start)
-	fmt.Printf("%v\n", windDist)
+	directions := []string{"N", "NE", "E", "SE", "S", "SW", "W", "NW"}
+
+	for i, result := range results {
+		fmt.Printf("%s:\t%v\n", directions[i], result)
+	}
+	fmt.Println("Using ", cores, "Cores out of a total of", runtime.NumCPU())
 	fmt.Printf("Processing took %s\n", elapsed)
 }
