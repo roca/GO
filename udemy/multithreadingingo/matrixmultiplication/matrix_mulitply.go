aa907028f7368e8c3a56dca725860ad27f94618c
romel.campbell <romel.campbell@regeneron.com>
2022-06-28 13:17:44 -0400

    Matrix multiplier code

---
+++ udemy/multithreadingingo/matrixmultiplication/matrix_mulitply.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	matrixSize = 250
+)
+
+var (
+	matrixA = [matrixSize][matrixSize]int{}
+	matrixB = [matrixSize][matrixSize]int{}
+	result  = [matrixSize][matrixSize]int{}
+)
+
+func generateRandomMatrix(matrix *[matrixSize][matrixSize]int) {
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+			rand.Seed(time.Now().UTC().UnixNano())
+			matrix[row][col] = rand.Intn(10) - 5
+		}
+	}
+}
+
+func workOutRow(row int) {
+	for col := 0; col < matrixSize; col++ {
+		for i := 0; i < matrixSize; i++ {
+			result[row][col] += matrixA[row][i] * matrixB[i][col]
+		}
+	}
+}
+
+func main() {
+
+	fmt.Println("Working...")
+	start := time.Now()
+	for i := 0; i < 100; i++ {
+		generateRandomMatrix(&matrixA)
+		generateRandomMatrix(&matrixB)
+		for row := 0; row < matrixSize; row++ {
+			workOutRow(row)
+			//fmt.Println(result[row])
+		}
+		//fmt.Println("------------------------")
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Done...")
+	fmt.Printf("Processing took %s\n", elapsed)
+}
