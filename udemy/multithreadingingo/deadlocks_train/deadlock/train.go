3c41f64bbeb5af96550aaa8053aeddf085356924
romel.campbell <romel.campbell@regeneron.com>
2022-06-30 15:54:19 -0400

    WIP IsDrawingSkipped not declared by package ebiten

--- udemy/multithreadingingo/deadlocks_train/deadlock/train.go
+++ udemy/multithreadingingo/deadlocks_train/deadlock/train.go
@@ -1,8 +1,26 @@
 package deadlock
 
 import (
+	"time"
+
 	. "github.com/roca/GO/tree/staging/udemy/multithreadingingo/deadlocks_train/common"
 )
 
-func MoveTrain(train *Train) {
+func MoveTrain(train *Train, distance int, crossings []*Crossing) {
+	for train.Front < distance {
+		train.Front += 1
+		for _, crossing := range crossings {
+			if train.Front == crossing.Position {
+				crossing.Intersections.Mutex.Lock()
+				crossing.Intersections.LockedBy = train.Id
+			}
+			back := train.Front + train.TrainLength
+			if back == crossing.Position {
+				crossing.Intersections.LockedBy = -1
+				crossing.Intersections.Mutex.Unlock()
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+	}
+
 }
--- udemy/multithreadingingo/deadlocks_train/launcher.go
+++ udemy/multithreadingingo/deadlocks_train/launcher.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+
 	. "github.com/roca/GO/tree/staging/udemy/multithreadingingo/deadlocks_train/common"
 	. "github.com/roca/GO/tree/staging/udemy/multithreadingingo/deadlocks_train/deadlock"
-	"github.com/hajimehoshi/ebiten/v2"
-	"log"
-	"sync"
 )
 
 var (
@@ -15,46 +16,17 @@ var (
 
 const trainLength = 70
 
-type Game struct{}
-
-func (g *Game) Update() error {
+func update(screen *ebiten.Image) error {
+	if !ebiten.IsDrawingSkipped() {
+		DrawTracks(screen)
+		DrawIntersections(screen)
+		DrawTrains(screen)
+	}
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	DrawTracks(screen)
-	DrawIntersections(screen)
-	DrawTrains(screen)
-}
-
-func (g *Game) Layout(_, _ int) (w, h int) {
-	return 320, 320
-}
-
 func main() {
-	for i := 0; i < 4; i++ {
-		trains[i] = &Train{Id: i, TrainLength: trainLength, Front: 0}
-	}
-
-	for i := 0; i < 4; i++ {
-		intersections[i] = &Intersection{Id: i, Mutex: sync.Mutex{}, LockedBy: -1}
-	}
-
-	go MoveTrain(trains[0], 300, []*Crossing{{Position: 125, Intersection: intersections[0]},
-		{Position: 175, Intersection: intersections[1]}})
-
-	go MoveTrain(trains[1], 300, []*Crossing{{Position: 125, Intersection: intersections[1]},
-		{Position: 175, Intersection: intersections[2]}})
-
-	go MoveTrain(trains[2], 300, []*Crossing{{Position: 125, Intersection: intersections[2]},
-		{Position: 175, Intersection: intersections[3]}})
-
-	go MoveTrain(trains[3], 300, []*Crossing{{Position: 125, Intersection: intersections[3]},
-		{Position: 175, Intersection: intersections[0]}})
-
-	ebiten.SetWindowSize(320*3, 320*3)
-	ebiten.SetWindowTitle("Trains in a box")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.Run(update, 320, 320, 3, "Trains in a box"); err != nil {
 		log.Fatal(err)
 	}
 }
