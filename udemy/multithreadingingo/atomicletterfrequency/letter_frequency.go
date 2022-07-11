f3540928142ac5ba0d684752304b795ab0ee5083
Romel Campbell <RomelCampbell@gmail.com>
2022-07-11 04:01:45 -0400

    Using WaitGroups and Atomic variables

--- udemy/multithreadingingo/atomicletterfrequency/letter_frequency.go
+++ udemy/multithreadingingo/atomicletterfrequency/letter_frequency.go
@@ -5,30 +5,45 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
-func countLetters(url string, frequency *[26]int32) {
+//var lock = sync.Mutex{}
+
+func countLetters(url string, frequency *[26]int32, wg *sync.WaitGroup) {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		index := strings.Index(allLetters, c)
-		if index >= 0 {
-			frequency[index] += 1
+	for i := 0; i <= 20; i++ {
+		for _, b := range body {
+			c := strings.ToLower(string(b))
+			//lock.Lock()
+			index := strings.Index(allLetters, c)
+			if index >= 0 {
+				atomic.AddInt32(&frequency[index], 1)
+			}
+			//lock.Unlock()
 		}
 	}
+	wg.Done()
 }
 
 func main() {
+	// cores := runtime.NumCPU()
+	// runtime.GOMAXPROCS(cores)
+
 	var frequency [26]int32
+	wg := sync.WaitGroup{}
 	start := time.Now()
 	for i := 1000; i <= 1200; i++ {
-		go countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency)
+		wg.Add(1)
+		go countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency, &wg)
 	}
+	wg.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("Processing took %s\n", elapsed)
 	fmt.Println("Done")
