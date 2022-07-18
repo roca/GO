2d934b929095bbb6a84cb7fb81f73b8c052b09ba
Romel Campbell <RomelCampbell@gmail.com>
2022-07-18 04:15:42 -0400

    Coverted code to get and valid urls

--- udemy/DevopsAndCloudEngineers/helloworld/main.go
+++ udemy/DevopsAndCloudEngineers/helloworld/main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
 	args := os.Args
-	fmt.Printf("hello world\nos.Args %v\nArguments: %v\n", args, args[1:])
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url>n")
+		os.Exit(1)
+	}
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
 }
