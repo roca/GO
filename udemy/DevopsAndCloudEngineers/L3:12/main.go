69ca278af5544b53ce92133208fa71a02059c82a
Romel Campbell <RomelCampbell@gmail.com>
2022-07-24 15:03:09 -0400

    Example using interfaces

---
+++ udemy/DevopsAndCloudEngineers/L3:12/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type IResponse interface {
+	GetResponse() string
+}
+
+// {"page":"words","input":"word1","words":["word1"]}
+type Page struct {
+	Name string `json:"page"`
+}
+
+type Words struct {
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
+
+func (w Words) GetResponse() string {
+	return fmt.Sprintf("%s", strings.Join(w.Words, ", "))
+}
+
+type Occurrence struct {
+	Words map[string]int `json:"words"`
+}
+
+func (o Occurrence) GetResponse() string {
+	out := []string{}
+	for word, occurrence := range o.Words {
+		out = append(out, fmt.Sprintf("%s (%d)", word, occurrence))
+	}
+	return fmt.Sprintf("%s", strings.Join(out, ", "))
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Usage: ./http-get <url>")
+		os.Exit(1)
+	}
+
+	res, err := doRequest(args[1])
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+
+	if res == nil {
+		fmt.Println("No response")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Response: %s\n", res.GetResponse())
+}
+
+func doRequest(requestURL string) (IResponse, error) {
+
+	if _, err := url.ParseRequestURI(requestURL); err != nil {
+		return nil, fmt.Errorf("URL Validation error: %s\n", err)
+	}
+
+	response, err := http.Get(requestURL)
+	if err != nil {
+		return nil, fmt.Errorf("http Get error: %s\n", err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("ReadAll error: %s\n", err)
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, body)
+	}
+
+	var page Page
+	if err := json.Unmarshal(body, &page); err != nil {
+		return nil, fmt.Errorf("Unmarshal error: %s\n", err)
+	}
+
+	switch page.Name {
+	case "words":
+		var words Words
+		if err := json.Unmarshal(body, &words); err != nil {
+			return nil, fmt.Errorf("Unmarshal error: %s\n", err)
+		}
+		return words, nil
+	case "occurrence":
+		var occurrence Occurrence
+		if err := json.Unmarshal(body, &occurrence); err != nil {
+			return nil, fmt.Errorf("Unmarshal error: %s\n", err)
+		}
+		return occurrence, nil
+	}
+
+	return nil, fmt.Errorf("No responses for this url: %s", requestURL)
+}
