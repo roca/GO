ec91b213d55a95a6bc37715a69217b43008d8e17
Romel Campbell <RomelCampbell@gmail.com>
2022-08-03 16:35:25 -0400

    WIP

--- udemy/DevopsAndCloudEngineers/L3.21-Assignment-1/cmd/http-login-packaged/main.go
+++ udemy/DevopsAndCloudEngineers/L3.21-Assignment-1/cmd/http-login-packaged/main.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/roca/GO/tree/staging/udemy/DevopsAndCloudEngineers/L3.17/pkg/api"
+	"github.com/roca/GO/tree/staging/udemy/DevopsAndCloudEngineers/L3.21-Assignment-1/pkg/api"
 )
 
 func main() {
