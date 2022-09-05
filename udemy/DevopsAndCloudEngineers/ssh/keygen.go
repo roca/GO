7884d80d61a84267f86279843f58e3f5b8412364
Romel Campbell <RomelCampbell@gmail.com>
2022-09-05 10:31:57 -0400

    Simple SSH generator

---
+++ udemy/DevopsAndCloudEngineers/ssh/keygen.go
@@ -0,0 +1,30 @@
+package ssh
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func GenerateKeys() ([]byte, []byte, error) {
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("GenerateKey error: %s", err)
+	}
+	privateKeyPem := &pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: map[string]string{},
+		Bytes:   x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("NewPublicKey error: %s", err)
+	}
+
+	return pem.EncodeToMemory(privateKeyPem), ssh.MarshalAuthorizedKey(publicKey), nil
+}
