85a41c82796c907c4981a5c7609df90f1e7cbe9c
romel.campbell <romel.campbell@regeneron.com>
2022-09-22 15:51:42 -0400

    WIP: L10.69

--- udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/discovery.go
+++ udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/discovery.go
@@ -1,9 +1,38 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func (s *server) discovery(w http.ResponseWriter, r *http.Request) {
+	var (
+		clientID string
+	)
+
+	if clientID = r.URL.Query().Get("client_id"); clientID == "" {
+		returnError(w, http.StatusBadRequest, fmt.Errorf("client_id is missing"))
+		return
+	}
+
+	appConfig := AppConfig{}
+	for _, app := range s.Config.Apps {
+		if app.ClientID == clientID {
+			appConfig = app
+		}
+	}
+	if appConfig.ClientID == "" {
+		returnError(w, http.StatusBadRequest, fmt.Errorf("client_id not found"))
+		return
+	}
+	body, err := json.MarshalIndent(appConfig, "", "  ")
+	if err != nil {
+		returnError(w, http.StatusInternalServerError, fmt.Errorf("json.MarshalIndent error: %s", err))
+		return
+	}
+
+	w.WriteHeader(http.StatusFound)
+	w.Write(body)
 
 }
