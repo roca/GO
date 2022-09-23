b46d7205884ba873987a6d368e2c1f33c5ba264c
Romel Campbell <RomelCampbell@gmail.com>
2022-09-23 09:03:57 -0400

    L10.70 Unmarchaled the discovery struct for testing values returned

--- udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/discovery_test.go
+++ udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/discovery_test.go
@@ -1,13 +1,17 @@
 package server
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/roca/GO/tree/staging/udemy/DevopsAndCloudEngineers/oidc-start/pkg/oidc"
 )
 
 func TestDiscovery(t *testing.T) {
+	s := newServer(privkeyPem, testConfig)
 
 	endpoint := "/.well-known/openid-configuration"
 	req := httptest.NewRequest(http.MethodGet, endpoint, nil)
@@ -22,6 +26,16 @@ func TestDiscovery(t *testing.T) {
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("Readall error: %s", err)
+	}
+
+	discovery := oidc.Discovery{}
+	err = json.Unmarshal(body, &discovery)
+	if err != nil {
+		t.Errorf("json.Unmarshal error: %s", err)
+	}
+
+	if discovery.Issuer != s.Config.Url {
+		t.Errorf("Expected issuer %s, got %s", s.Config.Url, discovery.Issuer)
 	}
 
 	t.Log(string(body), res.StatusCode)
