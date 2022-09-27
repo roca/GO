54e9c8af0df2f69263917eba5d8cf58bf0f435e2
romel.campbell <romel.campbell@regeneron.com>
2022-09-27 09:51:29 -0400

    L10.73 Task: token endpoint

--- udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/token.go
+++ udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/token.go
@@ -1,9 +1,19 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func (s *server) token(w http.ResponseWriter, r *http.Request) {
+
+	var code string
+
+	if code = r.URL.Query().Get("code"); code == "" {
+		returnError(w, http.StatusBadRequest, fmt.Errorf("code is missing"))
+		return
+	}
+
+	fmt.Println(s.Codes[code])
 
 }
