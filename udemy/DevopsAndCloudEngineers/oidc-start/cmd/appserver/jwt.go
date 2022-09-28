e4e1deb492351a245692a7c7856a924871ca8d7a
Romel Campbell <RomelCampbell@gmail.com>
2022-09-28 09:47:12 -0400

    WIP: L10.75 Task: appserver code exchange

--- udemy/DevopsAndCloudEngineers/oidc-start/cmd/appserver/jwt.go
+++ udemy/DevopsAndCloudEngineers/oidc-start/cmd/appserver/jwt.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 // gets token from tokenUrl validating token with jwksUrl and returning token & claims
 func getTokenFromCode(tokenUrl, jwksUrl, redirectUri, clientID, clientSecret, code string) (*jwt.Token, *jwt.StandardClaims, error) {
+
+	form := url.Values{}
+	form.Add("grant_type", "authorization_code")
+	form.Add("redirect_uri", redirectUri)
+	form.Add("client_id", clientID)
+	form.Add("client_secret", clientSecret)
+	form.Add("code", code)
+
+	res, err := http.Post(tokenUrl, "application/x-www-form-urlencoded", bytes.NewBufferString(form.Encode()))
+	if err != nil {
+		return nil, nil, err
+	}
+
 	return nil, nil, nil
 }
