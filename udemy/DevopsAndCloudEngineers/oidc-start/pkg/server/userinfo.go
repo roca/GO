b3b714f8c83ac747f46045acdaf0f94984223eeb
romel.campbell <romel.campbell@regeneron.com>
2022-10-03 11:47:48 -0400

    L10.81 TestUserInfo passes returning the correct user json

--- udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/userinfo.go
+++ udemy/DevopsAndCloudEngineers/oidc-start/pkg/server/userinfo.go
@@ -1,9 +1,48 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/roca/GO/tree/staging/udemy/DevopsAndCloudEngineers/oidc-start/pkg/users"
 )
 
 func (s *server) userinfo(w http.ResponseWriter, r *http.Request) {
+
+	bearerText := r.Header.Get("Authorization")
+	if bearerText == "" {
+		returnError(w, http.StatusUnauthorized, fmt.Errorf("Authorization header not set"))
+		return
+	}
+	accessToken := bearerText[7:]
+
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(accessToken, &claims, func(token *jwt.Token) (interface{}, error) {
+		privateKeyParsed, err := jwt.ParseRSAPrivateKeyFromPEM(s.PrivateKey)
+		if err != nil {
+			return nil, err
+		}
+		return &privateKeyParsed.PublicKey, nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		returnError(w, http.StatusUnauthorized, fmt.Errorf("Invalid token error: %s", err))
+		return
+	}
+	for _, user := range users.GetAllUsers() {
+		if user.Sub == claims["sub"].(string) {
+			bytes, err := json.Marshal(user)
+			if err != nil {
+				returnError(w, http.StatusInternalServerError, fmt.Errorf("userinfo marshall error: %s", err))
+				return
+			}
+			w.Write(bytes)
+			return
+		}
+	}
+
+	w.Write([]byte("User not found"))
 
 }
