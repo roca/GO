56908d88a6b75860d482dc7351755b467af101bd
romel.campbell <romel.campbell@regeneron.com>
2022-10-14 19:58:57 -0400

    Chapter 1 complete. final modifications to the word count function

--- pragprog/PowerfullCliApps/firstProgram/wc/main.go
+++ pragprog/PowerfullCliApps/firstProgram/wc/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "Count lines instead of words")
+	bytes := flag.Bool("b", false, "Count bytes instead of words or lines")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
 	wc := 0
 	for scanner.Scan() {
 		wc++
--- pragprog/PowerfullCliApps/firstProgram/wc/main_test.go
+++ pragprog/PowerfullCliApps/firstProgram/wc/main_test.go
@@ -5,10 +5,29 @@ import (
 	"testing"
 )
 
-func TestCount(t *testing.T) {
+func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b)
+	res := count(b, false, false)
+	if exp != res {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1\n")
+	exp := 3
+	res := count(b, true, false)
+	if exp != res {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	s := "word1 word2 word3 word4\n"
+	b := bytes.NewBufferString(s)
+	exp := len([]byte(s))
+	res := count(b, true, true)
 	if exp != res {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
