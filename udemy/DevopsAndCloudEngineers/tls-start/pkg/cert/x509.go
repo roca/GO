f6cb61cc566b2f57d0f1c21914cbc2aa4fc931e9
Romel Campbell <RomelCampbell@gmail.com>
2022-10-22 04:51:31 -0400

    WIP: L11.94

--- udemy/DevopsAndCloudEngineers/tls-start/pkg/cert/x509.go
+++ udemy/DevopsAndCloudEngineers/tls-start/pkg/cert/x509.go
@@ -1,9 +1,67 @@
 package cert
 
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"time"
+)
+
 func CreateCACert(ca *CACert, keyFilePath, caCertFilePath string) error {
+	tempplate := &x509.Certificate{
+		SerialNumber: ca.Serial,
+		Subject: pkix.Name{
+			Country:            removeEmptyString([]string{ca.Subject.Country}),
+			Organization:       removeEmptyString([]string{ca.Subject.Organization}),
+			OrganizationalUnit: removeEmptyString([]string{ca.Subject.OrganizationalUnit}),
+			Locality:           removeEmptyString([]string{ca.Subject.Locality}),
+			Province:           removeEmptyString([]string{ca.Subject.Province}),
+			StreetAddress:      removeEmptyString([]string{ca.Subject.StreetAddress}),
+			PostalCode:         removeEmptyString([]string{ca.Subject.PostalCode}),
+			CommonName:        ca.Subject.CommonName,
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().AddDate(ca.ValidForYears, 0, 0),
+		IsCA:        true,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign ,
+		BasicConstraintsValid: true,
+	}
+	_,_, err := createCert(tempplate, nil, nil)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func CreateCert(cert *Cert, caKey []byte, caCert []byte, keyFilePath, certFilePath string) error {
+	_ = &x509.Certificate{
+		SerialNumber: cert.Serial,
+		Subject: pkix.Name{
+			Country:            removeEmptyString([]string{cert.Subject.Country}),
+			Organization:       removeEmptyString([]string{cert.Subject.Organization}),
+			OrganizationalUnit: removeEmptyString([]string{cert.Subject.OrganizationalUnit}),
+			Locality:           removeEmptyString([]string{cert.Subject.Locality}),
+			Province:           removeEmptyString([]string{cert.Subject.Province}),
+			StreetAddress:      removeEmptyString([]string{cert.Subject.StreetAddress}),
+			PostalCode:         removeEmptyString([]string{cert.Subject.PostalCode}),
+			CommonName:        cert.Subject.CommonName,
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().AddDate(cert.ValidForYears, 0, 0),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
 	return nil
+}
+
+func createCert(template *x509.Certificate, caKey *rsa.PrivateKey, caCert *x509.Certificate) ([]byte,[]byte,error ){
+	return nil,nil,nil
+}
+
+func removeEmptyString(input []string) []string {
+	if len(input) == 1 && input[0] == "" {
+		return []string{}
+	}
+	return input
 }
