8490c2dc7960b33b5e734a187942bbe1ea640e39
romel.campbell <romel.campbell@regeneron.com>
2022-10-23 16:11:20 -0400

    First version of ToDo code with passing tests page 27.

---
+++ pragprog/PowerfullCliApps/todo/cmd/todo/main_test.go
@@ -0,0 +1,73 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTAsk", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := task + "\n"
+
+		if expected != string(out) {
+			t.Fatalf("Expected %q, got %q instead", expected, string(out))
+		}
+
+	})
+}
