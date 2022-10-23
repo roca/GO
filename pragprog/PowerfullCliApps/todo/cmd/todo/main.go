36df77dc4489715f98841e75c2f30953cfb0c626
romel.campbell <romel.campbell@regeneron.com>
2022-10-23 16:11:20 -0400

    First version of ToDo code with passing tests page 27.

---
+++ pragprog/PowerfullCliApps/todo/cmd/todo/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+
+	l := &todo.List{}
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
