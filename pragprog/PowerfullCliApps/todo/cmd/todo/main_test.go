f1b9a56d4dc45e8b3c85f612a9f0761858b0ccc7
romel.campbell <romel.campbell@regeneron.com>
2022-10-28 17:40:27 -0400

    Page 31 adds flag options

--- pragprog/PowerfullCliApps/todo/cmd/todo/main_test.go
+++ pragprog/PowerfullCliApps/todo/cmd/todo/main_test.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 )
 
@@ -50,14 +49,14 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTAsk", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-task", task)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
