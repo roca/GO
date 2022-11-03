0aeb3818fbf9b8923a4360d051458784399dc52a
romel.campbell <romel.campbell@regeneron.com>
2022-11-03 19:12:21 -0400

    TestRun pass

---
+++ pragprog/PowerfullCliApps/workingFiles/mdp/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const (
+	inputFile  = "./testdata/test1.md"
+	resultFile = "test1.md.html"
+	goldenFile = "./testdata/test1.md.html"
+)
+
+func TestParseContent(t *testing.T) {
+	input, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := parseContent(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(result, expected) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+}
+
+func TestRun(t *testing.T) {
+	if err := run(inputFile); err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := ioutil.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(result, expected) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+
+	os.Remove(resultFile)
+}
---
+++ pragprog/PowerfullCliApps/workingFiles/mdp/testdata/test1.md
@@ -0,0 +1,13 @@
+# Test Markdown File
+
+Just a test
+
+## Bullets:
+
+* Links [Link1] (https://example.com)
+
+## Code Block
+
+```sh
+some code
+```
