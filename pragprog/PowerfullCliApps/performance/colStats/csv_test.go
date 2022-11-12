e201fa09c34a660cf0aa197ec69e33e13386d4b9
romel.campbell <romel.campbell@regeneron.com>
2022-11-12 07:48:22 -0500

    Ch 4, Page 127: Unit test done

--- pragprog/PowerfullCliApps/performance/colStats/csv_test.go
+++ pragprog/PowerfullCliApps/performance/colStats/csv_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"math"
 	"testing"
+	"testing/iotest"
 )
 
 func TestOperations(t *testing.T) {
@@ -28,10 +32,78 @@ func TestOperations(t *testing.T) {
 			t.Run(name, func(t *testing.T) {
 				res := tc.op(data[k])
 
-				if math.Round(res*1000)/1000 != math.Round(exp*1000)/1000  {
+				if math.Round(res*1000)/1000 != math.Round(exp*1000)/1000 {
 					t.Errorf("Expected %g, got %g instead", exp, res)
 				}
 			})
 		}
+	}
+}
+
+func TestCSV2Float(t *testing.T) {
+	csvData := `IP Address,Requests,Response Time
+192.168.0.199,2056,236
+192.168.0.88,899,220
+192.168.0.199,3054,226
+192.168.0.100,4133,218
+192.168.0.199,950,238
+`
+
+	testCases := []struct {
+		name   string
+		col    int
+		exp    []float64
+		expErr error
+		r      io.Reader
+	}{
+		{name: "Column2", col: 2,
+			exp:    []float64{2056, 899, 3054, 4133, 950},
+			expErr: nil,
+			r:      bytes.NewBufferString(csvData),
+		},
+		{name: "Column3", col: 3,
+			exp:    []float64{236, 220, 226, 218, 238},
+			expErr: nil,
+			r:      bytes.NewBufferString(csvData),
+		},
+		{name: "FailedRead", col: 1,
+			exp:    nil,
+			expErr: iotest.ErrTimeout,
+			r:      iotest.TimeoutReader(bytes.NewReader([]byte{0})),
+		},
+		{name: "FailedNotNumber", col: 1,
+			exp:    nil,
+			expErr: ErrNotNumber,
+			r:      bytes.NewBufferString(csvData),
+		},
+		{name: "FailedInvalidColumn", col: 4,
+			exp:    nil,
+			expErr: ErrInvalidColumn,
+			r:      bytes.NewBufferString(csvData),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := csv2float(tc.r, tc.col)
+			if tc.expErr != nil {
+				if err == nil {
+					t.Errorf("Expected error %v, Got nil instead", tc.expErr)
+				}
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("Expected error %q, got %q instead", tc.expErr, err)
+				}
+				return
+			}
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+
+			for i, exp := range tc.exp {
+				if res[i] != exp {
+					t.Errorf("Expected %g, got %g instead", exp, res[i])
+				}
+			}
+		})
 	}
 }
