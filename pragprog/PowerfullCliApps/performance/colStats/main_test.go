f45df6fca0d7f947d6f52ae89a5659f150a59d11
romel.campbell <romel.campbell@regeneron.com>
2022-11-15 06:49:08 -0500

    Ch 4, Page 141: Understanding Benchmarking

--- pragprog/PowerfullCliApps/performance/colStats/main_test.go
+++ pragprog/PowerfullCliApps/performance/colStats/main_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"errors"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -60,4 +62,19 @@ func TestRun(t *testing.T) {
 			}
 		})
 	}
+}
+
+func BenchmarkRun(b *testing.B) {
+	filenames , err := filepath.Glob("./testdata/benchmark/*.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {	
+		if err := run(filenames, "avg", 2, ioutil.Discard); err != nil {
+			b.Error(err)
+		}
+	}
+
 }
