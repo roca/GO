8d29a4e71fb991899b00c5080a467d7276abc86a
romel.campbell <romel.campbell@regeneron.com>
2022-11-23 07:06:27 -0500

    Ch 6: complete

--- pragprog/PowerfullCliApps/processes/goci/main.go
+++ pragprog/PowerfullCliApps/processes/goci/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -61,17 +63,39 @@ func run(proj string, out io.Writer) error {
 		10*time.Second,
 	)
 
-	for _, s := range pipeline {
-		msg, err := s.execute()
-		if err != nil {
-			return err
-		}
+	sig := make(chan os.Signal, 1)
+	errCh := make(chan error)
+	done := make(chan struct{})
 
-		_, err = fmt.Fprintln(out, msg)
-		if err != nil {
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+
+		for _, s := range pipeline {
+			msg, err := s.execute()
+			if err != nil {
+				errCh <- err
+				return
+			}
+
+			_, err = fmt.Fprintln(out, msg)
+			if err != nil {
+				errCh <- err
+				return
+			}
+		}
+		close(done)
+	}()
+
+	for {
+		select {
+		case rec := <-sig:
+			signal.Stop(sig)
+			return fmt.Errorf("%s: Exiting: %w", rec, ErrSignal)
+		case err := <-errCh:
 			return err
+		case <-done:
+			return nil
 		}
 	}
-
-	return nil
 }
--- pragprog/PowerfullCliApps/processes/goci/main_test.go
+++ pragprog/PowerfullCliApps/processes/goci/main_test.go
@@ -8,7 +8,9 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -156,4 +158,58 @@ func TestHelperProcess(t *testing.T) {
 	}
 
 	os.Exit(1)
+}
+
+func TestRunKill(t *testing.T) {
+	var testCases = []struct {
+		name string
+		proj string
+		sig syscall.Signal
+		expErr error}{
+		{name: "SIGINT", proj: "./testdata/tool/", sig: syscall.SIGINT, expErr: ErrSignal},
+		{name: "SIGTERM", proj: "./testdata/tool/", sig: syscall.SIGTERM, expErr: ErrSignal},
+		{name: "SIGQUIT", proj: "./testdata/tool/", sig: syscall.SIGQUIT, expErr: nil},
+		}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T){
+			command = mockCmdTimeout
+			errCh := make(chan error)
+			ignSigCh := make(chan os.Signal, 1)
+			expSigCh := make(chan os.Signal, 1)
+
+			signal.Notify(ignSigCh, syscall.SIGQUIT)
+			defer signal.Stop(ignSigCh)
+			signal.Notify(expSigCh, tc.sig)
+			defer signal.Stop(expSigCh)
+
+			go func() {
+				errCh <- run(tc.proj, ioutil.Discard)
+			}()
+			go func() {
+				time.Sleep(2 * time.Second)
+				syscall.Kill(syscall.Getpid(), tc.sig)
+			}()
+
+			select {
+				case err := <-errCh:
+					if err == nil {
+						t.Errorf("Expected error: %q, Got 'nil' instead.", tc.expErr)
+						return
+					}
+					if !errors.Is(err, tc.expErr) {
+						t.Errorf("Expected error: %q, Got: %q", tc.expErr, err)
+					}
+					select{
+					case rec := <-expSigCh:
+						if rec != tc.sig {
+							t.Errorf("Expected signal: %q, Got: %q", tc.sig, rec)
+						}
+					default:
+						t.Errorf("Signal not received")
+					}
+				case <-ignSigCh:
+			}
+		})
+	}
 }
