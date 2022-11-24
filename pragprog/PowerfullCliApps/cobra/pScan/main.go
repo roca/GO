0665b9715034be4c9e3d386a89957a8d67d6f453
Romel Campbell <RomelCampbell@gmail.com>
2022-11-24 08:31:42 -0500

    Ch 7, Page 217: Initial cobar setup

---
+++ pragprog/PowerfullCliApps/cobra/pScan/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/cobra/pScan/cmd"
+
+func main() {
+	cmd.Execute()
+}
