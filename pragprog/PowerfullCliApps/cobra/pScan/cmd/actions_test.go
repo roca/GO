028c18f799c5c22a366bd50095eff6b6fbb56527
Romel Campbell <RomelCampbell@gmail.com>
2022-11-27 12:57:57 -0500

    Ch7, P 246: Hoast management unit and integration tests

--- pragprog/PowerfullCliApps/cobra/pScan/cmd/actions_test.go
+++ pragprog/PowerfullCliApps/cobra/pScan/cmd/actions_test.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/cobra/pScan/scan"
@@ -26,5 +30,97 @@ func setup(t *testing.T, hosts []string, initList bool) (string, func()) {
 	}
 	return tf.Name(), func() {
 		os.Remove(tf.Name())
+	}
+}
+
+func TestHostActions(t *testing.T) {
+	hosts := []string{"host1", "host2", "host3"}
+	testCases := []struct {
+		name           string
+		args           []string
+		expectedOut    string
+		initList       bool
+		actionFunction func(io.Writer, string, []string) error
+	}{
+		{
+			name:           "AddAction",
+			args:           hosts,
+			expectedOut:    "Added host: host1\nAdded host: host2\nAdded host: host3\n",
+			initList:       false,
+			actionFunction: addAction,
+		},
+		{
+			name:           "ListAction",
+			args:           []string{},
+			expectedOut:    "host1\nhost2\nhost3\n",
+			initList:       true,
+			actionFunction: listAction,
+		},
+		{
+			name:           "DeleteAction",
+			args:           []string{"host1", "host2"},
+			expectedOut:    "Deleted host: host1\nDeleted host: host2\n",
+			initList:       true,
+			actionFunction: deleteAction,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tf, cleanup := setup(t, hosts, tc.initList)
+			defer cleanup()
+
+			var out bytes.Buffer
+
+			if err := tc.actionFunction(&out, tf, tc.args); err != nil {
+				t.Fatalf("Expected no error, got %q instead\n", err)
+			}
+
+			if out.String() != tc.expectedOut {
+				t.Errorf("Expected output %q, got %q instead\n", tc.expectedOut, out.String())
+			}
+		})
+	}
+}
+
+func TestInregration(t *testing.T) {
+	hosts := []string{"host1", "host2", "host3"}
+	tf, cleanup := setup(t, hosts, false)
+	defer cleanup()
+
+	delHost := "host2"
+
+	hostsEnd := []string{"host1", "host3"}
+
+	var out bytes.Buffer
+
+	expectedOut := ""
+	for _, v := range hosts {
+		expectedOut += fmt.Sprintf("Added host: %s\n", v)
+	}
+	expectedOut += strings.Join(hosts, "\n")
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
+	expectedOut += strings.Join(hostsEnd, "\n")
+	expectedOut += fmt.Sprintln()
+
+	if err := addAction(&out, tf, hosts); err != nil {
+		t.Fatalf("Expected no error, got %q instead\n", err)
+	}
+
+	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q instead\n", err)
+	}
+
+	if err := deleteAction(&out, tf, []string{delHost}); err != nil {
+		t.Fatalf("Expected no error, got %q instead\n", err)
+	}
+
+	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q instead\n", err)
+	}
+
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q instead\n", expectedOut, out.String())
 	}
 }
