43ffe8c2a91835b4513380bf3ffcebc8ba0cb9c2
Romel Campbell <RomelCampbell@gmail.com>
2022-11-27 07:03:09 -0500

    Ch 7, P 240: add, delete actions and test setup

--- pragprog/PowerfullCliApps/cobra/pScan/cmd/delete.go
+++ pragprog/PowerfullCliApps/cobra/pScan/cmd/delete.go
@@ -6,22 +6,26 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/cobra/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+	Use:          "delete <host1>...<hostN>",
+	Aliases:      []string{"d"},
+	Short:        "Delete host(s) from list",
+	SilenceUsage: true,
+	Args:         cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+		return deleteAction(os.Stdout, hostsFile, args)
 	},
 }
 
@@ -37,4 +41,19 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func deleteAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range args {
+		if err := hl.Remove(h); err != nil {
+			return err
+		}
+		fmt.Fprintln(out, "Deleted host:", h)
+	}
+	return hl.Save(hostsFile)
 }
