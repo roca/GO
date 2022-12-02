240c3d7048fa8650f4f27cc664e4fa9c0b6ee708
Romel Campbell <RomelCampbell@gmail.com>
2022-12-02 07:02:43 -0500

    Ch 7, P 266: Add Configuration Management using Viper

--- pragprog/PowerfullCliApps/cobra/pScan/cmd/delete.go
+++ pragprog/PowerfullCliApps/cobra/pScan/cmd/delete.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/cobra/pScan/scan"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // deleteCmd represents the delete command
@@ -21,10 +22,7 @@ var deleteCmd = &cobra.Command{
 	SilenceUsage: true,
 	Args:         cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		hostsFile, err := cmd.Flags().GetString("hosts-file")
-		if err != nil {
-			return err
-		}
+		hostsFile := viper.GetString("hosts-file")
 		return deleteAction(os.Stdout, hostsFile, args)
 	},
 }
--- pragprog/PowerfullCliApps/cobra/pScan/cmd/root.go
+++ pragprog/PowerfullCliApps/cobra/pScan/cmd/root.go
@@ -5,9 +5,13 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -36,17 +40,47 @@ func Execute() {
 	}
 }
 
+var cfgFile string
+
 func init() {
 	cobra.OnInitialize(initConfig)
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
-	rootCmd.PersistentFlags().StringP("hosts-file", "f", "pScan.hosts","pScan hosts file")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
+	rootCmd.PersistentFlags().StringP("hosts-file", "f", "pScan.hosts", "pScan hosts file")
+
+	replacer := strings.NewReplacer("-", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetEnvPrefix("PSCAN")
+
+	viper.BindPFlag("hosts-file", rootCmd.PersistentFlags().Lookup("hosts-file"))
 
 	versionTemplate := `{{ printf "%s: %s -version %s\n" .Name .Short .Version}}`
 	rootCmd.SetVersionTemplate(versionTemplate)
 }
 
-func initConfig() {}
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".pScan" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pScan")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
