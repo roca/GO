13ff9710b32f576644ccd7d75cb62b3683be2512
romel.campbell <romel.campbell@regeneron.com>
2022-12-05 09:56:47 -0500

    Ch 8, P 292: Time for more tests

--- pragprog/PowerfullCliApps/apis/todoServer/handlers.go
+++ pragprog/PowerfullCliApps/apis/todoServer/handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/todo"
@@ -15,7 +18,7 @@ var (
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		replyError(w, r, http.StatusNotFound, "")
 		return
 	}
 	content := "There's an API here"
@@ -55,7 +58,7 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 			replyError(w, r, http.StatusBadRequest, err.Error())
 		}
 
-		switch  r.Method {
+		switch r.Method {
 		case http.MethodGet:
 			getOneHandler(w, r, list, id)
 		case http.MethodDelete:
@@ -69,11 +72,78 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 	}
 }
 
-func getAllHandler(w http.ResponseWriter, r *http.Request, list *todo.List)               {}
-func addHandler(w http.ResponseWriter, r *http.Request, list *todo.List, todoFile string) {}
-func validateID(urlPath string, list *todo.List) (int, error) {
-	return -1, nil
+func getAllHandler(w http.ResponseWriter, r *http.Request, list *todo.List) {
+	resp := &todoResponse{
+		Results: *list,
+	}
+	replyJSONContent(w, r, http.StatusOK, resp)
 }
-func getOneHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int) {}
-func deleteHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {}
-func patchHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {}
+
+func getOneHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int) {
+	resp := &todoResponse{
+		Results: (*list)[id-1 : id],
+	}
+	replyJSONContent(w, r, http.StatusOK, resp)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {
+	list.Delete(id)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	replyTextContent(w, r, http.StatusNoContent, "")
+}
+
+func patchHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {
+	q := r.URL.Query()
+
+	if _, ok := q["cpmplete"]; !ok {
+		message := "Missing query parameter 'complete'"
+		replyError(w, r, http.StatusBadRequest, message)
+		return
+	}
+	list.Complete(id)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	replyTextContent(w, r, http.StatusNoContent, "")
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request, list *todo.List, todoFile string) {
+	item := struct {
+		Task string `json:"task"`
+	}{}
+
+	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+		message := fmt.Sprintf("Invalid JSON: %s", err)
+		replyError(w, r, http.StatusBadRequest, message)
+		return
+	}
+
+	list.Add(item.Task)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	replyTextContent(w, r, http.StatusCreated, "")
+}
+
+func validateID(path string, list *todo.List) (int, error) {
+	id, err := strconv.Atoi(path)
+	if err != nil {
+		return 0, fmt.Errorf("%w: Invalid ID: %s", ErrInvalidData, err)
+	}
+
+	if id < 1 {
+		return 0, fmt.Errorf("%w: Invalid ID: Less than one", ErrInvalidData)
+	}
+
+	if id > len(*list) {
+		return 0, fmt.Errorf("%w: ID %d not found", ErrNotFound, id)
+	}
+
+	return id, nil
+}
--- pragprog/PowerfullCliApps/apis/todoServer/server.go
+++ pragprog/PowerfullCliApps/apis/todoServer/server.go
@@ -4,11 +4,20 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
 )
 
 func newMux(todoFile string) *http.ServeMux {
 	m := http.NewServeMux()
+	mu := &sync.Mutex{}
+
 	m.HandleFunc("/", rootHandler)
+
+	t := todoRouter(todoFile, mu)
+
+	m.Handle("/todo", http.StripPrefix("/todo", t))
+	m.Handle("/todo/", http.StripPrefix("/todo/", t))
+
 	return m
 }
 
