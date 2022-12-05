5aaf49802219b0bcf932179b5365628fbad207bc
romel.campbell <romel.campbell@regeneron.com>
2022-12-05 09:19:30 -0500

    Ch 8, P 289, WIP: Must implement handler for each of the cases

--- pragprog/PowerfullCliApps/apis/todoServer/handlers.go
+++ pragprog/PowerfullCliApps/apis/todoServer/handlers.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	ErrNotFount    = errors.New("not found")
+	ErrNotFound    = errors.New("not found")
 	ErrInvalidData = errors.New("invalid data")
 )
 
@@ -34,7 +34,46 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 		}
 
 		if r.URL.Path == "" {
-			
+			switch r.Method {
+			case http.MethodGet:
+				getAllHandler(w, r, list)
+			case http.MethodPost:
+				addHandler(w, r, list, todoFile)
+			default:
+				message := "Method not support"
+				replyError(w, r, http.StatusMethodNotAllowed, message)
+			}
+			return
+		}
+
+		id, err := validateID(r.URL.Path, list)
+		if err != nil {
+			if errors.Is(err, ErrNotFound) {
+				replyError(w, r, http.StatusNotFound, err.Error())
+				return
+			}
+			replyError(w, r, http.StatusBadRequest, err.Error())
+		}
+
+		switch  r.Method {
+		case http.MethodGet:
+			getOneHandler(w, r, list, id)
+		case http.MethodDelete:
+			deleteHandler(w, r, list, id, todoFile)
+		case http.MethodPatch:
+			patchHandler(w, r, list, id, todoFile)
+		default:
+			message := "Method not support"
+			replyError(w, r, http.StatusMethodNotAllowed, message)
 		}
 	}
 }
+
+func getAllHandler(w http.ResponseWriter, r *http.Request, list *todo.List)               {}
+func addHandler(w http.ResponseWriter, r *http.Request, list *todo.List, todoFile string) {}
+func validateID(urlPath string, list *todo.List) (int, error) {
+	return -1, nil
+}
+func getOneHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int) {}
+func deleteHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {}
+func patchHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {}
