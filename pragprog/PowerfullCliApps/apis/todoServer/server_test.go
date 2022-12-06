9aaf87519752f7e43f6ba878fc82934d09bee197
romel.campbell <romel.campbell@regeneron.com>
2022-12-06 06:34:34 -0500

    WIP

--- pragprog/PowerfullCliApps/apis/todoServer/server_test.go
+++ pragprog/PowerfullCliApps/apis/todoServer/server_test.go
@@ -1,20 +1,55 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
+
+	"github.com/roca/GO/tree/staging/pragprog/PowerfullCliApps/todo"
 )
 
 func setupAPI(t *testing.T) (string, func()) {
 	t.Helper()
 
-	ts := httptest.NewServer(newMux(""))
+	tempTodoFile, err := ioutil.TempFile("", "todotest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts := httptest.NewServer(newMux(tempTodoFile.Name()))
+
+	for i := 1; i < 3; i++ {
+		var body bytes.Buffer
+		taskName := fmt.Sprintf("Task number %d", i)
+		item := struct {
+			Task string `json:"task"`
+		}{
+			Task: taskName,
+		}
+
+		if err := json.NewEncoder(&body).Encode(item); err != nil {
+			t.Fatal(err)
+		}
+
+		r, err := http.Post(ts.URL+"/todo", "application/json", &body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if r.StatusCode != http.StatusCreated {
+			t.Fatalf("Failed to add initial items. Expected status %q, got %q", http.StatusText(http.StatusCreated), http.StatusText(r.StatusCode))
+		}
+	}
 
 	return ts.URL, func() {
 		ts.Close()
+		os.Remove(tempTodoFile.Name())
 	}
 }
 
@@ -27,6 +62,8 @@ func TestGet(t *testing.T) {
 		expContent string
 	}{
 		{name: "GetRoot", path: "/", expCode: http.StatusOK, expContent: "There's an API here"},
+		{name: "GetAll", path: "/todo", expCode: http.StatusOK, expItems: 2, expContent: "Task number 1"},
+		{name: "GetOne", path: "/todo/1", expCode: http.StatusOK, expItems: 1, expContent: "Task number 1"},
 		{name: "NotFound", path: "/todo/500", expCode: http.StatusNotFound},
 	}
 
@@ -36,6 +73,11 @@ func TestGet(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			var (
+				resp struct {
+					Results      todo.List `json:"results"`
+					Date         int64     `json:"date"`
+					TotalResults int       `json:"total_results"`
+				}
 				body []byte
 				err  error
 			)
@@ -52,6 +94,10 @@ func TestGet(t *testing.T) {
 			}
 
 			switch {
+			case r.Header.Get("Content-Type") == "application/json":
+				if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+					t.Error(err)
+				}
 			case strings.Contains(r.Header.Get("Content-Type"), "text/plain"):
 				if body, err = ioutil.ReadAll(r.Body); err != nil {
 					t.Error(err)
