58390f5a00b536f89d9e6ddfe0fddf1e1813aa11
Romel Campbell <RomelCampbell@gmail.com>
2022-12-08 07:16:20 -0500

    Ch 8, P 310, WIP: mock_test.go

---
+++ pragprog/PowerfullCliApps/apis/todoClient/cmd/list.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:          "list",
+	Short:        "List todo items",
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiRoot := viper.GetString("api-root")
+		return listAction(os.Stdout, apiRoot)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func listAction(out io.Writer, apiRoot string) error {
+	items, err := getAll(apiRoot)
+	if err != nil {
+		return err
+	}
+
+	return printAll(out, items)
+}
+
+func printAll(out io.Writer, items []item) error {
+	w := tabwriter.NewWriter(out, 3, 2, 0, ' ', 0)
+
+	for k, v := range items {
+		done := "-"
+		if v.Done {
+			done = "X"
+		}
+		fmt.Fprintf(w, "%s\t%d\t%s\t\n", done, k+1, v.Task)
+	}
+
+	return w.Flush()
+}
---
+++ pragprog/PowerfullCliApps/apis/todoClient/cmd/mock_test.go
@@ -0,0 +1,61 @@
+package cmd
+
+import "net/http"
+
+var testResp = map[string]struct {
+	Status int
+	Body   string
+}{
+	"resultMany": {
+		Status: http.StatusOK,
+		Body: `{
+			"results": [
+				{
+					"Task": "Task 1",
+					"Done": false,
+					"CreatedAt": "2019-10-28T08:23:38.310097076-04:00",
+					"CompletedAt": "0001-01-01T00:00:00Z"
+				},
+				{
+					"Task": "Task 2",
+					"Done": false,
+					"CreatedAt": "2019-10-28T08:23:38.323447798-04:00",
+					"CompletedAt": "2020-01-01T00:00:00Z"
+				}
+			],
+			"date": 1572265440,
+			"total_results": 2
+		}`,
+	},
+	"resultOne": {
+		Status: http.StatusOK,
+		Body: `{
+			"results": [
+				{
+					"Task": "Task 1",
+					"Done": false,
+					"CreatedAt": "2019-10-28T08:23:38.310097076-04:00",
+					"CompletedAt": "2020-01-01T00:00:00Z"
+				}
+			],
+			"date": 1572265440,
+			"total_results": 1
+		}`,
+	},
+	"noResults": {
+		Status: http.StatusOK,
+		Body: `{
+			"results": [],
+			"date": 1572265440,
+			"total_results": 0
+		}`,
+	},
+	"root": {
+		Status: http.StatusOK,
+		Body: "There's an API here",
+	},
+	"notFound": {
+		Status: http.StatusNotFound,
+		Body:   "404 - not found",
+	},
+}
