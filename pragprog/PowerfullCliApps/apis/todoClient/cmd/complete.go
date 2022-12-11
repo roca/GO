9fdda86093a379da41271fd1b514b2d5b1b35465
Romel Campbell <RomelCampbell@gmail.com>
2022-12-11 13:18:00 -0500

    Ch 8, P 333: Code for completing and deleting a  tasking

---
+++ pragprog/PowerfullCliApps/apis/todoClient/cmd/complete.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// completeCmd represents the complete command
+var completeCmd = &cobra.Command{
+	Use:          "complete",
+	Short:        "Mark a todo item as completed",
+	SilenceUsage: true,
+	Args:         cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiRoot := viper.GetString("api-root")
+
+		return completeAction(os.Stdout, apiRoot, args[0])
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// completeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// completeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func completeAction(out io.Writer, apiRoot, arg string) error {
+	id, err := strconv.Atoi(arg)
+	if err != nil {
+		return fmt.Errorf("%w: Item id must be a number", ErrNotNumber)
+	}
+
+	err = completeItem(apiRoot, id)
+	if err != nil {
+		return err
+	}
+
+	return printComplete(out, id)
+}
+
+func printComplete(out io.Writer, id int) error {
+	_, err := fmt.Fprintf(out, "Item number %d marked as completed.\n", id)
+	return err
+}
