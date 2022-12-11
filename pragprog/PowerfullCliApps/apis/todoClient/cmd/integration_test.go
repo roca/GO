1139a84550da9ff166d6e334fa9d91ad4bb83ffc
Romel Campbell <RomelCampbell@gmail.com>
2022-12-11 15:07:48 -0500

    Ch 8, P 336: Test for completing and deleting a tasking passing

--- pragprog/PowerfullCliApps/apis/todoClient/cmd/integration_test.go
+++ pragprog/PowerfullCliApps/apis/todoClient/cmd/integration_test.go
@@ -1,3 +1,27 @@
-// +build integration
+//go:build integration
 
 package cmd
+
+func randomTaskName(t *testing.T) string {
+	t.Helper()
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var p strings.Builder
+	for i := 0; i < 32; i++ {
+		p.WriteByte(chars[r.Intn(len(chars))])
+	}
+
+	return p.String()
+}
+
+func TestIntegration(t *testing.T) {
+	t.Run("AddTask", func(t *testing.T) {})
+	t.Run("ListTask", func(t *testing.T) {})
+	t.Run("ViewTask", func(t *testing.T) {})
+	t.Run("CompleteTask", func(t *testing.T) {})
+	t.Run("ListCompletedTask", func(t *testing.T) {})
+	t.Run("DeleteTask", func(t *testing.T) {})
+	t.Run("ListDeletedTask", func(t *testing.T) {})
+}
