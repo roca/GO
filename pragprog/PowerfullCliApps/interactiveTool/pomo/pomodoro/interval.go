921c821e2148c45c0a0e5a0888483d0f4fe207e0
Romel Campbell <RomelCampbell@gmail.com>
2022-12-14 07:00:46 -0500

    Ch 9, P354: WIP - interval.go

--- pragprog/PowerfullCliApps/interactiveTool/pomo/pomodoro/interval.go
+++ pragprog/PowerfullCliApps/interactiveTool/pomo/pomodoro/interval.go
@@ -1,6 +1,7 @@
 package pomodoro
 
 import (
+	"context"
 	"errors"
 	"time"
 )
@@ -45,8 +46,140 @@ var (
 )
 
 type IntervalConfig struct {
-	repo Repository
+	repo               Repository
 	PomodoroDuration   time.Duration
 	ShortBreakDuration time.Duration
 	LongBreakDuration  time.Duration
+}
+
+func NewConfig(repo Repository, pomodoro, shortBreak, longBreak time.Duration) *IntervalConfig {
+	c := &IntervalConfig{
+		repo:               repo,
+		PomodoroDuration:   25 * time.Minute,
+		ShortBreakDuration: 5 * time.Minute,
+		LongBreakDuration:  15 * time.Minute,
+	}
+
+	if pomodoro != 0 {
+		c.PomodoroDuration = pomodoro
+	}
+
+	if shortBreak != 0 {
+		c.ShortBreakDuration = shortBreak
+	}
+
+	if longBreak != 0 {
+		c.LongBreakDuration = longBreak
+	}
+
+	return c
+}
+
+func nextCategory(r Repository) (string, error) {
+	li, err := r.Last()
+	if err != nil && err == ErrNoIntervals {
+		return CategoryPomodoro, nil
+	}
+	if err != nil {
+		return "", err
+	}
+
+	if li.Category == CategoryLongBreak || li.Category == CategoryShortBreak {
+		return CategoryPomodoro, nil
+	}
+
+	lastBreaks, err := r.Breaks(3)
+	if err != nil {
+		return "", err
+	}
+
+	if len(lastBreaks) < 3 {
+		return CategoryShortBreak, nil
+	}
+
+	for _, i := range lastBreaks {
+		if i.Category == CategoryLongBreak {
+			return CategoryShortBreak, nil
+		}
+	}
+
+	return CategoryLongBreak, nil
+}
+
+type Callback func(Interval)
+
+func tick(ctx context.Context, id int64, config *IntervalConfig, start, periodic, end Callback) error {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	i, err := config.repo.ByID(id)
+	if err != nil {
+		return err
+	}
+
+	expire := time.After(i.PlannedDuration - i.ActualDuration)
+
+	start(i)
+
+	for {
+		select {
+		case <-ticker.C:
+			i, err = config.repo.ByID(id)
+			if err != nil {
+				return err
+			}
+
+			if i.State == StatePaused {
+				return nil
+			}
+
+			i.ActualDuration += time.Second
+			err = config.repo.Update(i)
+			if err := config.repo.Update(i); err != nil {
+				return err
+			}
+
+			periodic(i)
+		case <-expire:
+			i, err = config.repo.ByID(id)
+			if err != nil {
+				return err
+			}
+			i.State = StateDone
+			end(i)
+			return config.repo.Update(i)
+		case <-ctx.Done():
+			i, err = config.repo.ByID(id)
+			if err != nil {
+				return err
+			}
+			i.State = StateCancelled
+			return config.repo.Update(i)
+		}
+	}
+}
+
+func newInterval(config *IntervalConfig, category string) (Interval, error) {
+	i := Interval{}
+
+	category, err := nextCategory(config.repo)
+	if err != nil {
+		return i, err
+	}
+	i.Category = category
+
+	switch category {
+		case CategoryPomodoro:
+			i.PlannedDuration = config.PomodoroDuration
+		case CategoryShortBreak:
+			i.PlannedDuration = config.ShortBreakDuration
+		case CategoryLongBreak:
+			i.PlannedDuration = config.LongBreakDuration
+	}
+
+	if i.ID, err = config.repo.Create(i); err != nil {
+		return i, err
+	}
+
+	return i, nil
 }
