859bd6b5a283485d5018f819605a9ed37e828618
Romel Campbell <RomelCampbell@gmail.com>
2023-01-05 16:18:36 -0500

    Simple dns resolver

--- udemy/DevopsAndCloudEngineers/Lectures/L12.104/dns-start/pkg/dns/resolver.go
+++ udemy/DevopsAndCloudEngineers/Lectures/L12.104/dns-start/pkg/dns/resolver.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math/big"
 	"net"
+	"strings"
 
 	"golang.org/x/net/dns/dnsmessage"
 )
@@ -29,7 +30,104 @@ func handlePacket(pc net.PacketConn, addr net.Addr, buf []byte) error {
 		return err
 	}
 
-	return fmt.Errorf("not implemented yet")
+	response, err := dnsQuery(getRootServers(), question)
+	if err != nil {
+		return err
+	}
+	response.Header.ID = header.ID
+	responseBuf, err := response.Pack()
+	if err != nil {
+		return err
+	}
+
+	_, err = pc.WriteTo(responseBuf, addr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func dnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Message, error) {
+	fmt.Printf("Question %+v\n", question)
+
+	message := dnsmessage.Message{}
+	for i := 0; i < 3; i++ {
+		dnsAnswer, header, err := outgoingDnsQuery(servers, question)
+		if err != nil {
+			return nil, err
+		}
+
+		parsedAnswers, err := dnsAnswer.AllAnswers()
+		if err != nil {
+			return nil, err
+		}
+		if header.Authoritative {
+			message.Header.Response = true
+			message.Answers = parsedAnswers
+			return &message, nil
+		}
+
+		authorities, err := dnsAnswer.AllAuthorities()
+		if err != nil {
+			return nil, err
+		}
+
+		if len(authorities) == 0 {
+			message.Header.RCode = dnsmessage.RCodeNameError
+			return &message, nil
+		}
+
+		nameservers := make([]string, len(authorities))
+		for k, authority := range authorities {
+			if authority.Header.Type == dnsmessage.TypeNS {
+				nameservers[k] = authority.Body.(*dnsmessage.NSResource).NS.String()
+			}
+		}
+
+		additionals, err := dnsAnswer.AllAdditionals()
+		if err != nil {
+			return nil, err
+		}
+		newResolverServersFound := false
+		resolvedServers := []net.IP{}
+		for _, additional := range additionals {
+			if additional.Header.Type == dnsmessage.TypeA {
+				for _, nameserver := range nameservers {
+					if additional.Header.Name.String() == nameserver {
+						newResolverServersFound = true
+						resolvedServers = append(resolvedServers, additional.Body.(*dnsmessage.AResource).A[:])
+					}
+				}
+			}
+		}
+
+		servers = resolvedServers
+
+		if !newResolverServersFound {
+			for _, nameserver := range nameservers {
+				if !newResolverServersFound {
+					response, err := dnsQuery(getRootServers(), dnsmessage.Question{Name: dnsmessage.MustNewName(nameserver), Type: dnsmessage.TypeA, Class: dnsmessage.ClassINET})
+					if err != nil {
+						fmt.Printf("Warning: lookup of nameserver %s failed: %s\n", nameserver, err)
+					} else {
+						newResolverServersFound = true
+						for _, answer := range response.Answers {
+							if answer.Header.Type == dnsmessage.TypeA {
+								servers = append(servers, answer.Body.(*dnsmessage.AResource).A[:])
+							}
+						}
+					}
+				}
+
+			}
+			
+		}
+
+	}
+
+	message.Header.RCode = dnsmessage.RCodeServerFailure
+	return &message, nil
 }
 
 func outgoingDnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Parser, *dnsmessage.Header, error) {
@@ -113,4 +211,12 @@ func RandUint16() (uint16, error) {
 	}
 
 	return uint16(randomNumber.Int64()), nil
+}
+
+func getRootServers() []net.IP {
+	rootServers := []net.IP{}
+	for _, rootServer := range strings.Split(ROOT_SERVERS, ",") {
+		rootServers = append(rootServers, net.ParseIP(rootServer))
+	}
+	return rootServers
 }
