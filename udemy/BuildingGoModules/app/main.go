bac7723bc39e4f4cfbf290103221cb07b9357e63
Romel Campbell <RomelCampbell@gmail.com>
2023-01-09 05:33:48 -0500

    adds go mod to toolskit

--- udemy/BuildingGoModules/app/main.go
+++ udemy/BuildingGoModules/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/roca/GO/tree/staging/udemy/BuildingGoModules/toolkit"
 )
@@ -9,5 +10,13 @@ import (
 func main() {
 	var tools toolkit.Tools
 
+	http.HandleFunc("/upload", HandleFileUpload)
+
 	fmt.Println(tools.RandomString(10))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func HandleFileUpload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File upload request received")
 }
