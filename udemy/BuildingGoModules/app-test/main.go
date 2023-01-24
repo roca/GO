800015e53b493b8bbcb52b95bc7d6c0424b7f17f
Romel Campbell <RomelCampbell@gmail.com>
2023-01-24 05:42:51 -0500

    Course completed

--- udemy/BuildingGoModules/app-test/main.go
+++ udemy/BuildingGoModules/app-test/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/roca/go-toolkit/v2"
 )
 
 func main() {
@@ -18,12 +20,49 @@ func main() {
 func routes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
 	mux.HandleFunc("/api/login", login)
 	mux.HandleFunc("/api/logout", logout)
 
 	return mux
 }
 
-func login(w http.ResponseWriter, r *http.Request) {}
+func login(w http.ResponseWriter, r *http.Request) {
+	var tools toolkit.Tools
 
-func logout(w http.ResponseWriter, r *http.Request) {}
+	var payload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := tools.ReadJSON(w, r, &payload)
+	if err != nil {
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	var resPayLoad toolkit.JSONResponse
+
+	if payload.Email == "me@here.com" && payload.Password == "password" {
+		resPayLoad.Error = false
+		resPayLoad.Message = "You have been logged in"
+		_ = tools.WriteJSON(w, http.StatusAccepted, resPayLoad)
+		return
+	}
+
+	resPayLoad.Error = true
+	resPayLoad.Message = "Invalid credentials"
+	_ = tools.WriteJSON(w, http.StatusUnauthorized, resPayLoad)
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	var tools toolkit.Tools
+
+	payload := toolkit.JSONResponse{
+		Message: "You have been logged out",
+	}
+
+	_ = tools.WriteJSON(w, http.StatusAccepted, payload)
+}
