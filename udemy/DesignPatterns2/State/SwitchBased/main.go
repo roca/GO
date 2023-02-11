8627ff29d2c6360dbf423dca8fc280d8b4cc74f8
Romel Campbell <RomelCampbell@gmail.com>
2023-02-11 05:44:25 -0500

    Strategy

--- udemy/DesignPatterns2/State/SwitchBased/main.go
+++ udemy/DesignPatterns2/State/SwitchBased/main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Switch is a simple state machine
+
+// Combination lock
+
+type State int
+
+const (
+	Locked State = iota
+	Failed
+	Unlocked
+)
+
+func main() {
+	code := "1234"
+	state := Locked
+	entry := strings.Builder{}
+
+	for {
+		switch state {
+		case Locked:
+			r, _, _ := bufio.NewReader(os.Stdin).ReadRune()
+			entry.WriteRune(r)
+
+			if entry.String() == code {
+				state = Unlocked
+				break
+			}
+
+			if strings.Index(code, entry.String()) != 0 {
+				state = Failed
+			}
+		case Failed:
+			fmt.Println("FAILED")
+			entry.Reset()
+			state = Locked
+		case Unlocked:
+			fmt.Println("UNLOCKED")
+			return
+		}
+	}
+
+}
