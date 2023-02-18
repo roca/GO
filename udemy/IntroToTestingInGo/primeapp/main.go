402a1775ea3f45e6307014b3237d2c41c379b104
Romel Campbell <RomelCampbell@gmail.com>
2023-02-18 05:19:42 -0500

    Program takes user input  now

--- udemy/IntroToTestingInGo/primeapp/main.go
+++ udemy/IntroToTestingInGo/primeapp/main.go
@@ -1,16 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	
-	for i := 0; i < 10; i++ {
-		prime, msg := isPrime(i)
-		if prime {
-			fmt.Println(msg)
+	// print a welcome message
+	intro()
+
+	//  create a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
+
+	// start a goroutine to read user input and run program
+	go readUserInput(doneChan)
+
+	// block util the doneChan get a value
+	<-doneChan
+
+	// close the channel
+	close(doneChan)
+
+	// say goodbye
+	fmt.Println("Goodbye!")
+
+}
+
+// readUserInput reads user input and runs the prime checker
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
 		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+// checkNumbers checks if the user input is a number and if it is, checks if it is prime
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	scanner.Scan()
+	input := scanner.Text()
+
+	if input == "q" {
+		return "Goodbye!", true
 	}
 
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		return "Please enter a whole number or 'q' to quit", false
+	}
+
+	_, msg := isPrime(num)
+
+	return msg, false
+}
+
+func intro() {
+	fmt.Println("Welcome to the Prime Number Checker!")
+	fmt.Println("Enter a number to check if it is prime.")
+	fmt.Println("Enter 'q' to quit.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
 
 // isPrime returns true if the number is prime and a message if it is not
@@ -23,9 +85,9 @@ func isPrime(n int) (bool, string) {
 	if n < 0 {
 		return false, "Negative numbers are not prime, by definition!"
 	}
-	
+
 	// use the modulus operator to check if the number is prime
-	for i := 2; i < n/2; i++ {
+	for i := 2; i < n; i++ {
 		if n%i == 0 {
 			return false, fmt.Sprintf("%d is not prime, it is divisible by %d", n, i)
 		}
--- udemy/IntroToTestingInGo/primeapp/main_test.go
+++ udemy/IntroToTestingInGo/primeapp/main_test.go
@@ -3,12 +3,29 @@ package main
 import "testing"
 
 func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("with %d as test parameter, expected %t, got %t", 0, false, result)
+	primeTests := []struct {
+		name string
+		testNum int
+		expected bool
+		msg string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"not prime", 8, false, "8 is not prime, it is divisible by 2"},
+		{"Zero", 0, false, "0 is not prime, by definition!"},
+		{"One", 1, false, "1 is not prime, by definition!"},
+		{"Negative", -1, false, "Negative numbers are not prime, by definition!"},
+		{"Four", 4, false, "4 is not prime, it is divisible by 2"},
 	}
 
-	if msg != "0 is not prime, by definition!" {
-		t.Errorf("with %d as test parameter, expected %s, got %s", 0, "0 is not prime, by definition!", msg)
+	for _, e := range primeTests {
+		t.Run(e.name, func(t *testing.T) {
+			prime, msg := isPrime(e.testNum)
+			if prime != e.expected {
+				t.Errorf("Expected %t, got %t", e.expected, prime)
+			}
+			if msg != e.msg {
+				t.Errorf("Expected %s, got %s", e.msg, msg)
+			}
+		})	
 	}
 }
