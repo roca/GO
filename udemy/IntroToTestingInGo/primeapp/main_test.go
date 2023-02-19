7433380cec1c2db2a9c301d414fd76c1b52b02f6
Romel Campbell <RomelCampbell@gmail.com>
2023-02-19 05:54:52 -0500

    Uppwer and lower q

--- udemy/IntroToTestingInGo/primeapp/main_test.go
+++ udemy/IntroToTestingInGo/primeapp/main_test.go
@@ -84,6 +84,7 @@ func Test_checkNumbers(t *testing.T) {
 		done     bool
 	}{
 		{"Quit", "Q", "Goodbye!", true},
+		{"quit", "q", "Goodbye!", true},
 		{"Not a number", "a", "Please enter a whole number or 'q' to quit", false},
 		{"Prime", "7", "7 is a prime number!", false},
 		{"Not prime", "8", "8 is not prime, it is divisible by 2", false},
