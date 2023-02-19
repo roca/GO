6fd3b7700e23efed43218774a706bd835fc5d413
Romel Campbell <RomelCampbell@gmail.com>
2023-02-19 14:29:06 -0500

    Basic app run

--- udemy/IntroToTestingInGo/webapp/cmd/web/main.go
+++ udemy/IntroToTestingInGo/webapp/cmd/web/main.go
@@ -1,13 +1,24 @@
 package main
 
-type application struct {}
+import (
+	"log"
+	"net/http"
+)
+
+type application struct{}
 
 func main() {
-    // set up an app config
+	// set up an app config
+	app := application{}
 
-    // get application routes
+	// get application routes
+	mux := app.routes()
 
-    // print out a message to the console
-
-    // start the server
+	// print out a message to the console
+	log.Println("Starting server on :8080...")
+	// start the server
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
