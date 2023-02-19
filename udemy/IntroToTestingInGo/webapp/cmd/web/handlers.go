7cb0c48718d4839609baea0a529ac161399a453f
Romel Campbell <RomelCampbell@gmail.com>
2023-02-19 17:42:31 -0500

    rendering html

---
+++ udemy/IntroToTestingInGo/webapp/cmd/web/handlers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]interface{}
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+	parsedTemplate.Execute(w, data)
+	return nil
+}
--- udemy/IntroToTestingInGo/webapp/cmd/web/routes.go
+++ udemy/IntroToTestingInGo/webapp/cmd/web/routes.go
@@ -14,6 +14,7 @@ func (app *application) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 
 	// register routes
+	mux.Get("/", app.Home)
 
 	// static assets
 
