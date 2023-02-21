c1490875d57d85969dd6576c5a0d90a215394602
Romel Campbell <RomelCampbell@gmail.com>
2023-02-21 05:13:17 -0500

    Test_application_handlers

---
+++ udemy/IntroToTestingInGo/webapp/cmd/web/handlers_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_handlers(t *testing.T) {
+	theTests := []struct {
+		name               string
+		url                string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"404", "/notfound", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	pathToTemplates = "./../../templates/"
+
+	for _, e := range theTests {
+		t.Run(e.name, func(t *testing.T) {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("want %d; got %d", e.expectedStatusCode, resp.StatusCode)
+			}
+		})
+	}
+}
