d081ac6f47c2c76211145edfee1b486c9baf0e3c
Romel Campbell <RomelCampbell@gmail.com>
2023-02-21 16:00:32 -0500

    mux.Use(app.addIPToContext)

---
+++ udemy/IntroToTestingInGo/webapp/cmd/web/middleware.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type contextKey string
+
+const contextUserKey contextKey = "user_ip"
+
+func (app *application) ipFromContext(ctx context.Context) string {
+	ip := ctx.Value(contextUserKey).(string)
+	return ip
+}
+
+func (app *application) addIPToContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var ctx = context.Background()
+		ip, err := getIP(r)
+		if err != nil {
+			ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+			if len(ip) == 0 {
+				ip = "unknown"
+			}
+		}
+		ctx = context.WithValue(ctx, contextUserKey, ip)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getIP(r *http.Request) (string, error) {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "unknown", err
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return "", fmt.Errorf("userip: %q is not IP:port", r.RemoteAddr)
+	}
+
+	forward := r.Header.Get("X-Forwarded-For")
+	if len(forward) > 0 {
+		ip = forward
+	}
+
+	if len(ip) == 0 {
+		ip = "forward"
+	}
+
+	return ip, nil
+}
