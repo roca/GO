3a5b280ead4afd8c798e067a81fb29fb9ccbc96b
Romel Campbell <RomelCampbell@gmail.com>
2023-02-21 05:13:17 -0500

    Test_application_handlers

--- udemy/IntroToTestingInGo/webapp/cmd/web/handlers.go
+++ udemy/IntroToTestingInGo/webapp/cmd/web/handlers.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates/"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
@@ -15,7 +18,7 @@ type TemplateData struct {
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
-	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return err
