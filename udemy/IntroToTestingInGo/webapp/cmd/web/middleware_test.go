4a8242120855457ee7c7ec5373c17c94600f4161
Romel Campbell <RomelCampbell@gmail.com>
2023-02-22 05:12:57 -0500

    func Test_application_addIPToContest(t *testing.T) {

---
+++ udemy/IntroToTestingInGo/webapp/cmd/web/middleware_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_addIPToContest(t *testing.T) {
+	tests := []struct {
+		headerName  string
+		headerValue string
+		addr        string
+		emptyAddr   bool
+	}{
+		{"", "", "", false},
+		{"", "", "", true},
+		{"X-Forwarded-For", "192.3.2.1", "", false},
+		{"", "", "hello:world", false},
+	}
+
+	var app application
+
+	// create a dummy handler that will check the context
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		val := r.Context().Value(contextUserKey)
+		if val == nil {
+			t.Error(contextUserKey, "is not present")
+		}
+		ip, ok := val.(string)
+		if !ok {
+			t.Error("context value is not a string")
+		}
+		t.Log("ip:", ip)
+	})
+
+	for _, e := range tests {
+		handlerToTest := app.addIPToContext(nextHandler)
+
+		req := httptest.NewRequest("GET", "http://testing", nil)
+
+		if e.emptyAddr {
+			req.RemoteAddr = ""
+		}
+
+		if len(e.headerName) > 0 {
+			req.Header.Add(e.headerName, e.headerValue)
+		}
+
+		if len(e.addr) > 0 {
+			req.RemoteAddr = e.addr
+		}
+
+		handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
+	}
+}
