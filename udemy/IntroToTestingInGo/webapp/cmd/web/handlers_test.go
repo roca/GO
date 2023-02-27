9bce35a7ccffcd588504df1d76790bc1663178d8
Romel Campbell <RomelCampbell@gmail.com>
2023-02-27 12:19:59 -0500

    req = addContextAndSessionToRequest(req, app)

--- udemy/IntroToTestingInGo/webapp/cmd/web/handlers_test.go
+++ udemy/IntroToTestingInGo/webapp/cmd/web/handlers_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -35,4 +38,38 @@ func Test_application_handlers(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAppHome(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = addContextAndSessionToRequest(req, app)
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(app.Home)
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("TestAppHome returned wrong status code; expected %d but got %d", http.StatusOK, rr.Code)
+	}
+
+	body, _ := io.ReadAll(rr.Body)
+
+	if  !strings.Contains(string(body), `<small>From Session:`) {
+		t.Error("TestAppHome returned unexpected body")
+	}
+
+}
+
+func getCtx(req *http.Request) context.Context {
+	ctx := context.WithValue(req.Context(), contextUserKey, "unknown")
+	return ctx
+}
+
+func addContextAndSessionToRequest(req *http.Request, app application) *http.Request {
+	req = req.WithContext(getCtx(req))
+	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
+
+	return req.WithContext(ctx)
 }
--- udemy/IntroToTestingInGo/webapp/cmd/web/setup_test.go
+++ udemy/IntroToTestingInGo/webapp/cmd/web/setup_test.go
@@ -8,7 +8,7 @@ import (
 var app application
 
 func TestMain(m *testing.M) {
-	app = application{}
+	app.Session = getSession()
 	
 	code := m.Run()
 
