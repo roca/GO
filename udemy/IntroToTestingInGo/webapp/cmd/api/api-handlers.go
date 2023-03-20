9d7efab5bd53878ed5d5e157ab0b6f85240c61f1
Romel Campbell <RomelCampbell@gmail.com>
2023-03-20 05:24:03 -0400

    WIP: getTokenFromHeaderAndVerify

--- udemy/IntroToTestingInGo/webapp/cmd/api/api-handlers.go
+++ udemy/IntroToTestingInGo/webapp/cmd/api/api-handlers.go
@@ -2,7 +2,17 @@ package main
 
 import "net/http"
 
-func (app *application) authenticate(w http.ResponseWriter, r *http.Request){}
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request){
+	// read a json payload
+
+	// look up the user by email address
+
+	// check the password
+
+	// generate a tokens
+
+	// send token to user
+}
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request){}
 
