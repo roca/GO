aa8c67426cca216b22a590e881acf766c4df108c
Romel Campbell <RomelCampbell@gmail.com>
2023-03-27 05:13:09 -0400

    Test_app_enableCORS

---
+++ udemy/IntroToTestingInGo/webapp/cmd/api/api-middleware.go
@@ -0,0 +1,29 @@
+package main
+
+import "net/http"
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8090")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+			return
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func (app *application) authRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_,_,err := app.getTokenFromHeaderAndVerify(w,r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
