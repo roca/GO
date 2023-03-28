f45567e0a79cd50b5ec339c07b25b0b8791124ec
Romel Campbell <RomelCampbell@gmail.com>
2023-03-28 03:53:58 -0400

    Test_app_authRequired

--- udemy/IntroToTestingInGo/webapp/cmd/api/api-middleware_test.go
+++ udemy/IntroToTestingInGo/webapp/cmd/api/api-middleware_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"webapp/pkg/data"
 )
 
 func Test_app_enableCORS(t *testing.T) {
@@ -38,4 +40,48 @@ func Test_app_enableCORS(t *testing.T) {
 
 		})
 	}
+}
+
+func Test_app_authRequired(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	tokens, _ := app.generateTokenPair(&testUser)
+
+	var tests = []struct {
+		name            string
+		token           string
+		expectedAuthorized bool
+		setHeader       bool
+	}{
+		{"valid token", fmt.Sprintf("Bearer %s", tokens.Token), true, true},
+	}
+
+	for _, e := range tests {
+		t.Run(e.name, func(t *testing.T) {
+			handlerToTest := app.authRequired(nextHandler)
+
+			req, _ := http.NewRequest("GET", "http://", nil)
+			if e.setHeader {
+				req.Header.Set("Authorization", e.token)
+			}
+			rr := httptest.NewRecorder()
+			handlerToTest.ServeHTTP(rr, req)
+
+			if e.expectedAuthorized && rr.Code == http.StatusUnauthorized {
+				t.Errorf("%s: got code 402, and should not have", e.name)
+			}
+
+			if !e.expectedAuthorized && rr.Code != http.StatusUnauthorized {
+				t.Errorf("%s: Unexpected error: %d", e.name, rr.Code)
+			}
+		})
+	}
+
 }
