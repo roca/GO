2ae88369d1a36219bcae9981cb40e0e6d2b6fd04
Romel Campbell <RomelCampbell@gmail.com>
2023-04-10 05:14:59 -0400

    Get user and logout features

--- udemy/IntroToTestingInGo/webapp/cmd/api/api-routes.go
+++ udemy/IntroToTestingInGo/webapp/cmd/api/api-routes.go
@@ -14,12 +14,12 @@ func (app *application) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(app.enableCORS)
 
-	mux.Handle("/",http.StripPrefix("/",http.FileServer(http.Dir("./html/"))))
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./html/"))))
 
 	mux.Route("/web", func(mux chi.Router) {
 		mux.Post("/auth", app.authenticate)
-		mux.Get("/refresh-token",app.refreshUsingCookie)
-		// /logout
+		mux.Get("/refresh-token", app.refreshUsingCookie)
+		mux.Get("/logout", app.deleteRefreshCookie)
 	})
 
 	// authentication routes - auth handler, refresh
