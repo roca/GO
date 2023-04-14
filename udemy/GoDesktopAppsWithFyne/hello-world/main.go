c5324b0e3d72c1235beabea4fbbed3c09722e630
Romel Campbell <RomelCampbell@gmail.com>
2023-04-14 05:05:38 -0400

    WIP

--- udemy/GoDesktopAppsWithFyne/hello-world/main.go
+++ udemy/GoDesktopAppsWithFyne/hello-world/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type App struct {
+	output *widget.Label
+}
+
+var myApp App
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello, World")
 
-	w.SetContent(widget.NewLabel("Hello, world!"))
+	output, entry, btn := myApp.makeUI()
+
+	w.SetContent(container.NewVBox(output, entry, btn))
+	w.Resize(fyne.NewSize(500, 500))
 	w.ShowAndRun()
+}
+
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello, World!")
+	entry := widget.NewEntry()
+	btn := widget.NewButton("Enter", func() {
+		app.output.SetText(entry.Text)
+	})
+
+	app.output = output
+
+	return output, entry, btn
 }
