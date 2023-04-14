550a6f60650bae39c42d5ffd78292e74557ce9f7
Romel Campbell <RomelCampbell@gmail.com>
2023-04-14 05:22:08 -0400

    WIP: Markdown App

---
+++ udemy/GoDesktopAppsWithFyne/fyne-md/go.mod
@@ -0,0 +1,5 @@
+module fynemd
+
+go 1.19
+
+require fyne.io/fyne/v2 v2.3.3 // indirect
---
+++ udemy/GoDesktopAppsWithFyne/fyne-md/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fyne.io/fyne/v2/widget"
+
+type config struct {
+	EditWidget *widget.Entry
+}
