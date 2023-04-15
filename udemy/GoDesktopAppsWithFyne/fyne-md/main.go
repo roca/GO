2e50d2aff74b8e2292c28ffe539d5be1a51dccf9
Romel Campbell <RomelCampbell@gmail.com>
2023-04-15 05:03:25 -0400

    WIP: MarkDown editor. Showing panes

--- udemy/GoDesktopAppsWithFyne/fyne-md/main.go
+++ udemy/GoDesktopAppsWithFyne/fyne-md/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fyne.io/fyne/v2/widget"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 type config struct {
-	EditWidget *widget.Entry
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+
+	// create a window for the app
+	win := a.NewWindow("Markdown Editor")
+
+	// get the user interface
+	edit, preview := cfg.makeUI()
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	// create widgets
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	edit.OnChanged = preview.ParseMarkdown
+
+	// return widgets
+	return edit, preview
 }
