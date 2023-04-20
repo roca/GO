1f0166396811313c998140e4ac023bc95712bd4c
Romel Campbell <RomelCampbell@gmail.com>
2023-04-20 03:52:56 -0400

    WIP: GoldWatcher

--- udemy/GoDesktopAppsWithFyne/goldwatcher/main.go
+++ udemy/GoDesktopAppsWithFyne/goldwatcher/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
 type Config struct {
-	App      fyne.App
-	InfoLog  *log.Logger
-	ErrorLog *log.Logger
+	App        fyne.App
+	InfoLog    *log.Logger
+	ErrorLog   *log.Logger
+	MainWindow fyne.Window
 }
 
 var myApp Config
@@ -18,15 +20,23 @@ var myApp Config
 func main() {
 	// create a fyne application
 	fyneApp := app.NewWithID("desertfoxdev.org.goldwatcher.preferences")
+	myApp.App = fyneApp
 
 	//create our loggers
+	myApp.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	myApp.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// open a connection to our local database. SQLite
 
 	// create a database repository using the Repo interface
 
 	// create and size a fyne window
+	myApp.MainWindow = myApp.App.NewWindow("GoldWatcher")
+	myApp.MainWindow.Resize(fyne.NewSize(300, 200))
+	myApp.MainWindow.SetFixedSize(true)
+	myApp.MainWindow.CenterOnScreen()
 
 	// show and run the application
+	myApp.MainWindow.ShowAndRun()
 
 }
