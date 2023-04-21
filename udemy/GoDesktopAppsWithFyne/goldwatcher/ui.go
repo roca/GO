e04c4e2cee87208d54a80f5e675c8d346abf5a29
Romel Campbell <RomelCampbell@gmail.com>
2023-04-21 16:30:58 -0400

    WIP

---
+++ udemy/GoDesktopAppsWithFyne/goldwatcher/ui.go
@@ -0,0 +1,13 @@
+package main
+
+func (app *Config) makeUI() {
+
+	// get the current price of gold
+	// opening-price, current-price, price-change
+	// https://data-asg.goldprice.org/dbxRates/USD
+
+	// put price information into a container
+
+	// add container to the window
+
+}
