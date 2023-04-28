4d19fd3f38e173ff18f4469fda18a66450128cc9
Romel Campbell <RomelCampbell@gmail.com>
2023-04-28 05:10:06 -0400

    WIP: SQLite repo methods

--- udemy/GoDesktopAppsWithFyne/goldwatcher/repository/repository.go
+++ udemy/GoDesktopAppsWithFyne/goldwatcher/repository/repository.go
@@ -3,13 +3,28 @@ package repository
 import (
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var (
+	errInsertFailed = errors.New("insert failed")
 	errUpdateFailed = errors.New("update failed")
 	errDeleteFailed = errors.New("delete failed")
 )
 
-type DatabaseRepo interface {
+type Repository interface {
 	Connection() *sql.DB
+	Migrate() error
+	InsertHoldings(h Holdings) (*Holdings, error)
+	AllHoldings() ([]Holdings, error)
+	GetHoldingByID(id int) (*Holdings, error)
+	UpdateHolding(id int64, updated Holdings) error
+	DeleteHolding(id int64) error
+}
+
+type Holdings struct {
+	ID            int64     `json:"id"`
+	Amount        float64   `json:"amount"`
+	PurchaseDate  time.Time `json:"purchase_date"`
+	PurchasePrice int       `json:"purchase_price"`
 }
