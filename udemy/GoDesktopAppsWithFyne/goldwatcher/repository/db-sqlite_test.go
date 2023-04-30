415402154cc59b5e6fbdccdd8f24a7ebc10bcf73
Romel Campbell <RomelCampbell@gmail.com>
2023-04-30 05:24:11 -0400

    WIP: More repo tests

--- udemy/GoDesktopAppsWithFyne/goldwatcher/repository/db-sqlite_test.go
+++ udemy/GoDesktopAppsWithFyne/goldwatcher/repository/db-sqlite_test.go
@@ -1,10 +1,48 @@
 package repository
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestSQLiteRepository(t *testing.T) {
+func TestSQLiteRepository_Migrate(t *testing.T) {
 	err := testRepo.Migrate()
 	if err != nil {
 		t.Error("migrate failed", err)
+	}
+}
+
+func TestSQLiteRepository_InsertHoldings(t *testing.T) {
+	_ = testRepo.Migrate()
+	h := Holdings{
+		Amount:        1.0,
+		PurchaseDate:  time.Now(),
+		PurchasePrice: 1000,
+	}
+	result, err := testRepo.InsertHoldings(h)
+	if err != nil {
+		t.Error("insert failed", err)
+	}
+
+	if result.ID <= 0 {
+		t.Error("invalid id sent back: ", err)
+	}
+}
+
+func TestSQLiteRepository_AllHoldings(t *testing.T) {
+	_ = testRepo.Migrate()
+	h := Holdings{
+		Amount:        1.0,
+		PurchaseDate:  time.Now(),
+		PurchasePrice: 1000,
+	}
+	_, _ = testRepo.InsertHoldings(h)
+	holdings, err := testRepo.AllHoldings()
+	if err != nil {
+		t.Error("all holdings failed", err)
+	}
+
+	if len(holdings) < 1 {
+		t.Error("invalid number of holdings returned: ", len(holdings))
 	}
 }
