30aa38ebb31dc4777ffe55f0668fb7b8d1c0a7d0
Romel Campbell <RomelCampbell@gmail.com>
2023-05-18 03:52:15 -0400

    wip

--- udemy/GoLaravel/myapp/main.go
+++ udemy/GoLaravel/myapp/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/roca/celeritas"
-)
-
 func main() {
-	result := celeritas.TestFunc(1, 2)
-	fmt.Println(result)
 
-	result  = celeritas.TestFunc2(1, 2)
-	fmt.Println(result)
-
-	result = celeritas.TestFunc3(1, 2)
-	fmt.Println(result)
 }
