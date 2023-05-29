dc6b56f2151b509b33d764b3e0992a10bbe3c4d5
Romel Campbell <RomelCampbell@gmail.com>
2023-05-29 05:13:12 -0400

    WIP: Sessions

--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -11,6 +11,8 @@ func (a *application) routes() *chi.Mux {
 
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
+	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
+	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
