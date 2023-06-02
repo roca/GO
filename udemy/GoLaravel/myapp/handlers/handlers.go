76f9c14e57738f4c803e586e7aa5899ec9eb5fef
Romel Campbell <RomelCampbell@gmail.com>
2023-06-02 05:15:05 -0400

    Displaying sesson data

--- udemy/GoLaravel/myapp/handlers/handlers.go
+++ udemy/GoLaravel/myapp/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/roca/celeritas"
 )
 
@@ -13,20 +14,36 @@ type Handlers struct {
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.Page(w, r, "home", nil, nil)
 	if err != nil {
-		h.App.ErrorLog.Println("error rendering:",err)
+		h.App.ErrorLog.Println("error rendering:", err)
 	}
 }
 
 func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.GoPage(w, r, "home", nil)
 	if err != nil {
-		h.App.ErrorLog.Println("error rendering:",err)
+		h.App.ErrorLog.Println("error rendering:", err)
 	}
 }
 
 func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
 	if err != nil {
-		h.App.ErrorLog.Println("error rendering:",err)
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
+func (h *Handlers) SessionsTest(w http.ResponseWriter, r *http.Request) {
+	myData := "this is my data"
+
+	h.App.Session.Put(r.Context(), "myData", myData)
+
+	myValue := h.App.Session.GetString(r.Context(), "myData")
+
+	vars := make(jet.VarMap)
+	vars.Set("myData", myValue)
+
+	err := h.App.Render.JetPage(w, r, "sessions", vars, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
 	}
 }
--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -13,6 +13,7 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/", a.Handlers.Home)
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
+	a.App.Routes.Get("/sessions", a.Handlers.SessionsTest)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
