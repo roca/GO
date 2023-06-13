604b7156b3c3f587f94ef66a75da1d6f7b27b560
Romel Campbell <RomelCampbell@gmail.com>
2023-06-13 03:52:00 -0400

    WIP

---
+++ udemy/GoLaravel/myapp/handlers/auth-handlers.go
@@ -0,0 +1,12 @@
+package handlers
+
+import "net/http"
+
+func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.Page(w, r, "login", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+	}
+}
+
+func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {}
--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -18,6 +18,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionsTest)
 
+	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
+	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
+
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "John",
