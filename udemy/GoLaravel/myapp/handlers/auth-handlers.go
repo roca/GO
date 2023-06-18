1fd0b1af538cfd78fe2d49b1c639e99a97676f05
Romel Campbell <RomelCampbell@gmail.com>
2023-06-18 04:42:58 -0400

    Adds Login/ Logout functionality

--- udemy/GoLaravel/myapp/handlers/auth-handlers.go
+++ udemy/GoLaravel/myapp/handlers/auth-handlers.go
@@ -42,3 +42,9 @@ func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (h *Handlers) Logout(w http.ResponseWriter, r *http.Request) {
+	h.App.Session.RenewToken(r.Context())
+	h.App.Session.Remove(r.Context(), "userID")
+	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
+}
