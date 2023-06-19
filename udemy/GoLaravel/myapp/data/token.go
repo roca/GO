9452d8214605d4a5428bb2f8196d06a875c3e4ed
Romel Campbell <RomelCampbell@gmail.com>
2023-06-19 05:08:43 -0400

    Move user handelrs to a seprate file

--- udemy/GoLaravel/myapp/data/token.go
+++ udemy/GoLaravel/myapp/data/token.go
@@ -7,7 +7,7 @@ type Token struct {
 	UserID    int       `db:"user_id" json:"user_id"`
 	FirstName string    `db:"first_name" json:"first_name"`
 	Email     string    `db:"email" json:"email"`
-	PlainText string    `db:"-" json:"token"`
+	PlainText string    `db:"token" json:"token"`
 	Hash      []byte    `db:"token_hash" json:"-"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"myapp/data"
 	"net/http"
-	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -22,66 +19,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
 	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
 
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
-		u := data.User{
-			FirstName: "John",
-			LastName:  "Doe",
-			Email:     "me@here.com",
-			Active:    1,
-			Password:  "password",
-		}
-
-		id, err := a.Models.Users.Insert(u)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "User %s created with id: %d", u.FirstName, id)
-
-	})
-
-	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "User %s %s %s", u.FirstName, u.LastName, u.Email)
-	})
-
-	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		u.LastName = a.App.RandomString(10)
-
-		err = a.Models.Users.Update(*u)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "Users %s last name updated to %s", u.FirstName, u.LastName)
-	})
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
-		users, err := a.Models.Users.GetAll()
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		for _, u := range users {
-			fmt.Fprintf(w, "%s %s\n", u.FirstName, u.LastName)
-		}
-
-	})
+	a.App.Routes.Get("/create-user", a.Handlers.CreateUser)
+	a.App.Routes.Get("/get-user/{id}", a.Handlers.GetUserByID)
+	a.App.Routes.Get("/update-user/{id}", a.Handlers.UpdateUserByID)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
