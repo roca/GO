69c7d620f5b990ab33b5ef210eb55ef12ba41571
Romel Campbell <RomelCampbell@gmail.com>
2023-06-20 03:55:20 -0400

    WIP: Token CRUD

--- udemy/GoLaravel/myapp/data/token.go
+++ udemy/GoLaravel/myapp/data/token.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"time"
+
 	up "github.com/upper/db/v4"
 )
 
@@ -48,4 +49,102 @@ func (t *Token) GetUserForToken(token string) (*User, error) {
 	u.Token = theToken
 
 	return &u, nil
+}
+
+func (t *Token) GetTokensForUser(id int) ([]*Token, error) {
+	var all []*Token
+
+	collection := upper.Collection(t.Table())
+	err := collection.
+		Find(up.Cond{"user_id =": id}).
+		All(&all)
+	if err != nil {
+		return nil, err
+	}
+
+	return all, nil
+}
+
+// Get token by ID
+func (t *Token) Get(id int) (*Token, error) {
+	var theToken Token
+
+	collection := upper.Collection(t.Table())
+	err := collection.
+		Find(up.Cond{"id =": id}).
+		One(&theToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &theToken, nil
+}
+
+// Get token by token string
+func (t *Token) GetByToken(token string) (*Token, error) {
+	var theToken Token
+
+	collection := upper.Collection(t.Table())
+	err := collection.
+		Find(up.Cond{"token =": token}).
+		One(&theToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &theToken, nil
+}
+
+// Delete token by ID
+func (t *Token) Delete(id int) error {
+	collection := upper.Collection(t.Table())
+	err := collection.
+		Find(up.Cond{"id =": id}).
+		Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete token by token string
+func (t *Token) DeleteByToken(token string) error {
+	collection := upper.Collection(t.Table())
+	err := collection.
+		Find(up.Cond{"token =": token}).
+		Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Insert token
+func (t *Token) Insert(token Token, u User) error {
+	collection := upper.Collection(t.Table())
+
+	// delete existing tokens for this user
+	err := collection.
+		Find(up.Cond{"user_id =": u.ID}).
+		Delete()
+	if err != nil {
+		return err
+	}
+
+	token.CreatedAt = time.Now()
+	token.UpdatedAt = time.Now()
+	token.FirstName = u.FirstName
+	token.Email = u.Email
+	token.UserID = u.ID
+
+	// insert new token
+	_, err = collection.
+		Insert(&token)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
