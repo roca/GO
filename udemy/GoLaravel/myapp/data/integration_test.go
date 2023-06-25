137fafa9a06119628a8ea4bda822cadda7bde9eb
Romel Campbell <RomelCampbell@gmail.com>
2023-06-25 04:21:39 -0400

    WIP: Docker test setup

--- udemy/GoLaravel/myapp/data/integration_test.go
+++ udemy/GoLaravel/myapp/data/integration_test.go
@@ -1,4 +1,5 @@
 //go:build integration
+
 package data
 
 import (
@@ -6,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -27,10 +29,51 @@ var dummtUser = User{
 
 var models Models
 var testDB *sql.DB
-
 var resourse *dockertest.Resource
 var pool *dockertest.Pool
 
 func TestMain(m *testing.M) {
 	os.Setenv("DATABASE_TYPE", "postgres")
+
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	opts := dokcertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.4",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	ressourse, err = pool.RunWithOptions(opts)
+	if err != nil {
+		_ = pool.Purge(ressourse)
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("postgres", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			return err
+		}
+
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(ressourse)
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
 }
