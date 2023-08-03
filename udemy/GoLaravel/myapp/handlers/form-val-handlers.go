0ccaf52aa62b8ddf35d57d6d8dfc6c242c570feb
Romel Campbell <RomelCampbell@gmail.com>
2023-08-03 03:54:36 -0400

    WIP

--- udemy/GoLaravel/myapp/handlers/form-val-handlers.go
+++ udemy/GoLaravel/myapp/handlers/form-val-handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"myapp/data"
 	"net/http"
 
@@ -17,4 +18,37 @@ func (h *Handlers) Form(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		h.App.ErrorLog.Println(err)
 	}
+}
+
+func (h *Handlers) PostForm(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+		return
+	}
+
+	validator := h.App.Validator(nil)
+
+	validator.Required(r, "first_name", "last_name", "email")
+	validator.IsEmail("email", r.PostForm.Get("email"))
+
+	validator.Check(len(r.Form.Get("first_name")) > 1, "first_name", "Must be at least 2 characters")
+	validator.Check(len(r.Form.Get("last_name")) > 1, "last_name", "Must be at least 2 characters")
+
+	if !validator.Valid() {
+		vars := make(jet.VarMap)
+		vars.Set("validator", validator)
+		vars.Set("user", data.User{
+			FirstName: r.Form.Get("first_name"),
+			LastName:  r.Form.Get("last_name"),
+			Email:     r.Form.Get("email"),
+		})
+
+		if err := h.App.Render.Page(w, r, "form", vars, nil); err != nil {
+			h.App.ErrorLog.Println(err)
+			return
+		}
+		return
+	}
+	fmt.Fprint(w, "valid data")
 }
