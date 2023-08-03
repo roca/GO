cc48b2a960c10438cc41796be06b8c19937baf20
Romel Campbell <RomelCampbell@gmail.com>
2023-08-03 03:54:36 -0400

    WIP

--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -20,7 +20,7 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
 
 	a.App.Routes.Get("/form", a.Handlers.Form)
-	//a.App.Routes.Post("/form", a.Handlers.PostForm)
+	a.App.Routes.Post("/form", a.Handlers.PostForm)
 
 	a.App.Routes.Get("/create-user", a.Handlers.CreateUser)
 	a.App.Routes.Get("/get-user/{id}", a.Handlers.GetUserByID)
