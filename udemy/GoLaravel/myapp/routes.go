3c31e56c8f4ea8d758114879132fe0c0c1cede58
Romel Campbell <RomelCampbell@gmail.com>
2023-08-03 14:54:24 -0400

    Adds get and post convience funcs

--- udemy/GoLaravel/myapp/routes.go
+++ udemy/GoLaravel/myapp/routes.go
@@ -10,21 +10,21 @@ func (a *application) routes() *chi.Mux {
 	// middle must come before any routes
 
 	// add routes here
-	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.SessionsTest)
+	a.get("/", a.Handlers.Home)
+	a.get("/go-page", a.Handlers.GoPage)
+	a.get("/jet-page", a.Handlers.JetPage)
+	a.get("/sessions", a.Handlers.SessionsTest)
 
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
+	a.get("/users/login", a.Handlers.UserLogin)
+	a.post("/users/login", a.Handlers.PostUserLogin)
+	a.get("/users/logout", a.Handlers.Logout)
 
-	a.App.Routes.Get("/form", a.Handlers.Form)
-	a.App.Routes.Post("/form", a.Handlers.PostForm)
+	a.get("/form", a.Handlers.Form)
+	a.post("/form", a.Handlers.PostForm)
 
-	a.App.Routes.Get("/create-user", a.Handlers.CreateUser)
-	a.App.Routes.Get("/get-user/{id}", a.Handlers.GetUserByID)
-	a.App.Routes.Get("/update-user/{id}", a.Handlers.UpdateUserByID)
+	a.get("/create-user", a.Handlers.CreateUser)
+	a.get("/get-user/{id}", a.Handlers.GetUserByID)
+	a.get("/update-user/{id}", a.Handlers.UpdateUserByID)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
