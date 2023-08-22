d40acb388e48ca9c6d89c11ae5cd4817779fecec
romel.campbell <romel.campbell@regeneron.com>
2023-08-22 14:05:14 -0400

    WIP

--- udemy/GoDesktopAppsWithFyne/text-editor/main.go
+++ udemy/GoDesktopAppsWithFyne/text-editor/main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"io"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
+var window fyne.Window
+var entry *widget.Entry
+var currentFile fyne.URI
+var cursorRow *widget.Label
+var cursorCol *widget.Label
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("TestEdit")
+	window = a.NewWindow("TestEdit")
 
 	ui := makeUI()
-	w.SetContent(ui)
+	window.SetContent(ui)
 
-	w.Resize(fyne.NewSize(480, 360))
-	w.ShowAndRun()
+	window.Resize(fyne.NewSize(480, 360))
+	window.ShowAndRun()
 }
 
 func makeUI() fyne.CanvasObject {
-	entry := widget.NewMultiLineEntry()
-	cursorRow := widget.NewLabel("1")
-	cursorCol := widget.NewLabel("1")
+	entry = widget.NewMultiLineEntry()
+	cursorRow = widget.NewLabel("1")
+	cursorCol = widget.NewLabel("1")
 
 	toolbar := buildToolbar()
 
@@ -32,6 +43,8 @@ func makeUI() fyne.CanvasObject {
 		widget.NewLabel("Cursor Row: "), cursorRow,
 		widget.NewLabel("Col:"), cursorCol,
 	)
+
+	entry.OnCursorChanged = updateStatus
 
 	return container.NewBorder(
 		toolbar,
@@ -53,8 +66,43 @@ func buildToolbar() *widget.Toolbar {
 	)
 }
 
-func open()  {}
-func save()  {}
+func open() {
+	dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
+		defer r.Close()
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+		if r == nil {
+			return
+		}
+		data, err := io.ReadAll(r)
+		if err == nil {
+			entry.SetText(string(data))
+			currentFile = r.URI()
+		} else {
+			dialog.ShowError(err, window)
+		}
+	}, window)
+}
+func save() {
+	if currentFile != nil {
+		w, err := storage.Writer(currentFile)
+		defer w.Close()
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+		_, err = w.Write([]byte(entry.Text))
+		if err != nil {
+			dialog.ShowError(err, window)
+		}
+	}
+}
 func cut()   {}
 func copy()  {}
 func paste() {}
+func updateStatus() {
+	cursorRow.SetText(fmt.Sprintf("%d", entry.CursorRow+1))
+	cursorCol.SetText(fmt.Sprintf("%d", entry.CursorColumn+1))
+}
