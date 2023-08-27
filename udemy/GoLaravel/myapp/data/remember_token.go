f6cc12084d3cd020a91ffa272e91260d6abfe3fa
Romel Campbell <RomelCampbell@gmail.com>
2023-08-27 04:37:52 -0400

    WIP: middleware for "remember me" functionality

--- udemy/GoLaravel/myapp/data/remember_token.go
+++ udemy/GoLaravel/myapp/data/remember_token.go
@@ -21,18 +21,28 @@ func (t *RememberToken) Table() string {
 }
 
 func (t *RememberToken) InsertToken(userID int, token string) error {
-    _, err := t.Insert(RememberToken{
-        UserID:        userID,
-        RememberToken: token,
-    })
-    if err != nil {
-        return err
-    }
-    return nil
+	_, err := t.insert(RememberToken{
+		UserID:        userID,
+		RememberToken: token,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *RememberToken) DeleteToken(rememberToken string) error {
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"remember_token": rememberToken})
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // GetAll gets all records from the database, using upper
-func (t *RememberToken) GetAll(condition up.Cond) ([]*RememberToken, error) {
+func (t *RememberToken) getAll(condition up.Cond) ([]*RememberToken, error) {
 	collection := upper.Collection(t.Table())
 	var all []*RememberToken
 
@@ -46,7 +56,7 @@ func (t *RememberToken) GetAll(condition up.Cond) ([]*RememberToken, error) {
 }
 
 // Get gets one record from the database, by id, using upper
-func (t *RememberToken) Get(id int) (*RememberToken, error) {
+func (t *RememberToken) get(id int) (*RememberToken, error) {
 	var one RememberToken
 	collection := upper.Collection(t.Table())
 
@@ -59,7 +69,7 @@ func (t *RememberToken) Get(id int) (*RememberToken, error) {
 }
 
 // Update updates a record in the database, using upper
-func (t *RememberToken) Update(m RememberToken) error {
+func (t *RememberToken) update(m RememberToken) error {
 	m.UpdatedAt = time.Now()
 	collection := upper.Collection(t.Table())
 	res := collection.Find(m.ID)
@@ -71,7 +81,7 @@ func (t *RememberToken) Update(m RememberToken) error {
 }
 
 // Delete deletes a record from the database by id, using upper
-func (t *RememberToken) Delete(id int) error {
+func (t *RememberToken) delete(id int) error {
 	collection := upper.Collection(t.Table())
 	res := collection.Find(id)
 	err := res.Delete()
@@ -82,7 +92,7 @@ func (t *RememberToken) Delete(id int) error {
 }
 
 // Insert inserts a model into the database, using upper
-func (t *RememberToken) Insert(m RememberToken) (int, error) {
+func (t *RememberToken) insert(m RememberToken) (int, error) {
 	m.CreatedAt = time.Now()
 	m.UpdatedAt = time.Now()
 	collection := upper.Collection(t.Table())
@@ -97,7 +107,7 @@ func (t *RememberToken) Insert(m RememberToken) (int, error) {
 }
 
 // Builder is an example of using upper's sql builder
-func (t *RememberToken) Builder(id int) ([]*RememberToken, error) {
+func (t *RememberToken) builder(id int) ([]*RememberToken, error) {
 	collection := upper.Collection(t.Table())
 
 	var result []*RememberToken
