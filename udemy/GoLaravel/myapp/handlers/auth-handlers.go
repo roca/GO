0fd5bf8f23967715db3ef692ceb27b60fdf1643e
romel.campbell <romel.campbell@regeneron.com>
2023-08-28 16:15:29 -0400

    Ready to Try out the remember me functionality

--- udemy/GoLaravel/myapp/handlers/auth-handlers.go
+++ udemy/GoLaravel/myapp/handlers/auth-handlers.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"myapp/data"
 	"net/http"
+	"time"
 )
 
 func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
@@ -38,13 +43,73 @@ func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// did the user check remember me?
+	if r.Form.Get("remember") == "remember" {
+		randomString := h.randomString(12)
+		hasher := sha256.New()
+		_, err := hasher.Write([]byte(randomString))
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+		rm := data.RememberToken{}
+		err = rm.InsertToken(user.ID, sha)
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		// set a cookie
+		expire := time.Now().Add(365 * 24 * 60 * 60 * time.Second)
+		cookie := http.Cookie{
+			Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+			Value:    fmt.Sprintf("%d|%s", user.ID, sha),
+			Path:     "/",
+			Expires:  expire,
+			HttpOnly: true,
+			Domain:   h.App.Session.Cookie.Domain,
+			MaxAge:   315350000,
+			Secure:   h.App.Session.Cookie.Secure,
+			SameSite: http.SameSiteStrictMode,
+		}
+		http.SetCookie(w, &cookie)
+		// save hash in session
+		h.App.Session.Put(r.Context(), "remember_token", sha)
+
+	}
+
 	h.sessionPut(r.Context(), "userID", user.ID)
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (h *Handlers) Logout(w http.ResponseWriter, r *http.Request) {
+	// delete the remember token if it exists
+	if h.App.Session.Exists(r.Context(), "remember_token") {
+		rt := data.RememberToken{}
+		_ = rt.DeleteToken(h.App.Session.GetString(r.Context(), "remember_token"))
+	}
+
+	// delete the remember token cookie
+	newCookie := http.Cookie{
+		Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-100 * time.Hour),
+		HttpOnly: true,
+		Domain:   h.App.Session.Cookie.Domain,
+		MaxAge:   -1,
+		Secure:   h.App.Session.Cookie.Secure,
+		SameSite: http.SameSiteStrictMode,
+	}
+	http.SetCookie(w, &newCookie)
+
 	h.sessionRenewToken(r.Context())
 	h.sessionRemove(r.Context(), "userID")
+	h.sessionRemove(r.Context(), "remember_token")
+	h.sessionDestroy(r.Context())
+	h.sessionRenewToken(r.Context())
 	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
 }
