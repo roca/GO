d49f779edd9e59acf73c85aed09f37a98d361cd9
Romel Campbell <RomelCampbell@gmail.com>
2023-09-04 12:26:27 -0400

    test of make auth succesful

---
+++ udemy/GoLaravel/myapp/remove_auth.sh
@@ -0,0 +1,16 @@
+ #!/usr/bin/env bash
+
+rm data/token.go
+rm data/user.go
+rm data/remember_token.go
+rm handlers/auth-handlers.go
+rm mail/password-reset.html.tmpl
+rm mail/password-reset.plain.tmpl
+rm middleware/auth-token.go
+rm middleware/auth.go
+rm middleware/remember.go
+rm migrations/1690823586567819_auth.postgres.down.sql
+rm migrations/1690823586567819_auth.postgres.up.sql
+rm views/forgot.jet
+rm views/login.jet
+rm views/reset-password.jet
