88d752b49b80dd6074afd88e2a8c74a8a17c2187
Romel Campbell <RomelCampbell@gmail.com>
2023-09-04 19:44:28 -0400

    make auth test worked

--- udemy/GoLaravel/myapp/remove_auth.sh
+++ udemy/GoLaravel/myapp/remove_auth.sh
@@ -1,5 +1,7 @@
  #!/usr/bin/env bash
-
+ 
+./celeritas migrate down
+./celeritas migrate down
 rm data/token.go
 rm data/user.go
 rm data/remember_token.go
@@ -9,8 +11,8 @@ rm mail/password-reset.plain.tmpl
 rm middleware/auth-token.go
 rm middleware/auth.go
 rm middleware/remember.go
-rm migrations/1690823586567819_auth.postgres.down.sql
-rm migrations/1690823586567819_auth.postgres.up.sql
+rm migrations/*_auth.postgres.down.sql
+rm migrations/*_auth.postgres.up.sql
 rm views/forgot.jet
 rm views/login.jet
 rm views/reset-password.jet
