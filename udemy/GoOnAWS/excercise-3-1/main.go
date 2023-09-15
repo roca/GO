91dabe6301e429d53c4ec28e89bf625231c53a30
Romel Campbell <RomelCampbell@gmail.com>
2023-09-15 05:12:12 -0400

    Fix some typos

--- udemy/GoOnAWS/excercise-3-1/main.go
+++ udemy/GoOnAWS/excercise-3-1/main.go
@@ -5,18 +5,18 @@ import "fmt"
 func main() {
 	var varInt = 42
 	var varString = "mega"
-	var vrFloat = 3.14
+	var varFloat = 3.14
 	varBool := true
 
 	fmt.Printf("Value Int : %v\n", varInt)
 	fmt.Printf("Type Int : %T\n", varInt)
 
-	fmt.Printf("Value Int : %v\n", varString)
-	fmt.Printf("Type Int : %T\n", varString)
+	fmt.Printf("Value string : %v\n", varString)
+	fmt.Printf("Type string : %T\n", varString)
 
-	fmt.Printf("Value Int : %v\n", vrFloat)
-	fmt.Printf("Type Int : %T\n", vrFloat)
+	fmt.Printf("Value float : %v\n", varFloat)
+	fmt.Printf("Type float : %T\n", varFloat)
 
-	fmt.Printf("Value Int : %v\n", varBool)
-	fmt.Printf("Type Int : %T\n", varBool)
+	fmt.Printf("Value bool : %v\n", varBool)
+	fmt.Printf("Type bool : %T\n", varBool)
 }
