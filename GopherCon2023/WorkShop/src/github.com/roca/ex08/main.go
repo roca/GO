d56919bc098395d26b5858dc0abc119fc65ed65e
romel.campbell <romel.campbell@regeneron.com>
2023-09-26 15:09:27 -0700

    scanPort

--- GopherCon2023/WorkShop/src/github.com/roca/ex08/main.go
+++ GopherCon2023/WorkShop/src/github.com/roca/ex08/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type person struct {
+	name    string
+	address string
+	city    string
+	state   string
+	zip     string
+}
+
+type people []person
+
+func (p people) String() string {
+	lines := []string{}
+	for _, v := range p {
+		lines = append(lines, fmt.Sprint(v.name, " | ", v.address, " ", v.city, " ", v.state, " ", v.zip))
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func main() {
+	peeps := people{
+		{name: "John", address: "123 Main St", city: "Jamestown", state: "NY", zip: "14701"},
+		{name: "Jane", address: "234 Fleet St", city: "Columbia", state: "MD", zip: "21150"},
+		{name: "Terry", address: "345 Charles Ave", city: "Gergetown", state: "DC", zip: "20007"},
+	}
+
+	fmt.Println(peeps)
+}
