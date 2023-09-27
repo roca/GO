894ab8d9179895adb57a57fe416ad4efaac4095b
romel.campbell <romel.campbell@regeneron.com>
2023-09-27 14:43:53 -0700

    OpenTelementry

--- udemy/GoLaravel/myapp/main.go
+++ udemy/GoLaravel/myapp/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
 	"myapp/data"
 	"myapp/handlers"
 	"myapp/middleware"
 
+	"github.com/honeycombio/honeycomb-opentelemetry-go"
+	"github.com/honeycombio/otel-config-go/otelconfig"
 	"github.com/roca/celeritas"
 )
 
@@ -16,6 +19,18 @@ type application struct {
 }
 
 func main() {
+	// enable multi-span attributes
+	bsp := honeycomb.NewBaggageSpanProcessor()
+
+	// use honeycomb distro to setup OpenTelemetry SDK
+	otelShutdown, err := otelconfig.ConfigureOpenTelemetry(
+		otelconfig.WithSpanProcessor(bsp),
+	)
+	if err != nil {
+		log.Fatalf("error setting up OTel SDK - %e", err)
+	}
+	defer otelShutdown()
+
 	c := initApplication()
 	c.App.ListenAndServe()
 }
