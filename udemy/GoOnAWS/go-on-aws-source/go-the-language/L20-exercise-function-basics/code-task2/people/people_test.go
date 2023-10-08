ef5a5e7f05dc38666a295788585c04357ea36556
Romel Campbell <RomelCampbell@gmail.com>
2023-10-08 11:26:56 -0400

    puts name on each table test

--- udemy/GoOnAWS/go-on-aws-source/go-the-language/L20-exercise-function-basics/code-task2/people/people_test.go
+++ udemy/GoOnAWS/go-on-aws-source/go-the-language/L20-exercise-function-basics/code-task2/people/people_test.go
@@ -8,20 +8,23 @@ import (
 func TestMoodString(t *testing.T) {
 	//begin expected
 	tests := []struct {
-		mood         people.Mood
-		expectedStr  string
+		name        string
+		mood        people.Mood
+		expectedStr string
 	}{
-		{people.Neutral, "neutral"},
-		{people.Mood(100), "unknown"}, // Test case for unknown mood
+		{"neutral", people.Neutral, "neutral"},
+		{"unknown", people.Mood(100), "unknown"}, // Test case for unknown mood
 	}
 	//end expected
-	
+
 	//begin test
 	for _, test := range tests {
-		result := test.mood.String()
-		if result != test.expectedStr {
-			t.Errorf("Mood %d: Expected %s, but got %s", test.mood, test.expectedStr, result)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result := test.mood.String()
+			if result != test.expectedStr {
+				t.Errorf("Mood %d: Expected %s, but got %s", test.mood, test.expectedStr, result)
+			}
+		})
 	}
 	//end test
 }
