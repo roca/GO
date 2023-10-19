dfbfcbad599d1260efa0445683ccdfc4629847a3
romel.campbell <romel.campbell@regeneron.com>
2023-10-19 15:42:15 -0400

    WIP

--- udemy/HandsOngRPC/project01/server/server.go
+++ udemy/HandsOngRPC/project01/server/server.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	pb "project01/hello"
+)
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	req := pb.Hello{
+		Name: name,
+	}
+	w.Write([]byte(req.Name))
+}
+
+func main() {
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
