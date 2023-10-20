896feb612637323d8bda745a35ef5b2c75fd0767
romel.campbell <romel.campbell@regeneron.com>
2023-10-20 13:53:50 -0400

    Basic hello

--- udemy/HandsOngRPC/project01/Task.md
+++ udemy/HandsOngRPC/project01/Task.md
@@ -11,7 +11,6 @@
 ```sh
 protoc \
 --go_out=. --go_opt=paths=source_relative \
---go-grpc_out=. --go-grpc_opt=paths=source_relative \
 ./hello.proto
 ```
 
---
+++ udemy/HandsOngRPC/project01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"project01/basic"
+	"time"
+)
+
+type logWriter struct{}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Print(time.Now().Format("15:04:04" + " " + string(bytes)))
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
+	basic.BasicHello()
+}
