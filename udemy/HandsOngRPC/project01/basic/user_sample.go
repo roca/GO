f13f54303d2ecd538f48de89b51f462413d75e80
Romel Campbell <RomelCampbell@gmail.com>
2023-10-23 05:21:48 -0400

    Syntas error in jason

--- udemy/HandsOngRPC/project01/basic/user_sample.go
+++ udemy/HandsOngRPC/project01/basic/user_sample.go
@@ -3,6 +3,8 @@ package basic
 import (
 	"log"
 	hello "project01/hello/protogen"
+
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func BasicUser() {
@@ -15,4 +17,54 @@ func BasicUser() {
 		Gender:   hello.Gender_GENDER_MALE,
 	}
 	log.Println(&u)
+}
+
+func ProtoToJsonUser() {
+	u := hello.User{
+		Id:       99,
+		Username: "wonderwoman",
+		IsActive: true,
+		Password: []byte("wonderwomanpassword"),
+		Emails:   []string{"wonderwoman@movie.com", "wonderwoman@dc.com"},
+		Gender:   hello.Gender_GENDER_FEMALE,
+	}
+
+	jsonBytes, _ := protojson.Marshal(&u)
+	log.Println(string(jsonBytes))
+}
+
+func JsonToProtoUser() {
+	jsonBytes := []byte(`{
+		"id": 97,
+		"username": "batman",
+		"is_active": true,
+		"password": "batmannpassword",
+		"emails": [
+			"batman@movie.com",
+			"batman@dc.com",
+		],
+		"gender": "GENDER_MALE"
+	}`)
+
+	// jsonBytes := []byte(`{
+	// 	"id":97, 
+	// 	"username": "batman", 
+	// 	"is_active": true, 
+	// 	"password": "batmannpassword", 
+	// 	"emails":[
+	// 		"batman@movie.com", 
+	// 		"batman@dc.com"
+	// 	], 
+	// 	"gender": "GENDER_MALE"
+	// 	}`)
+
+	var p hello.User
+
+	err := protojson.Unmarshal(jsonBytes, &p)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(&p)
+
 }
--- udemy/HandsOngRPC/project01/main.go
+++ udemy/HandsOngRPC/project01/main.go
@@ -17,5 +17,7 @@ func main() {
 	log.SetFlags(0)
 	log.SetOutput(new(logWriter))
 	// basic.BasicHello()
-	basic.BasicUser()
+	// basic.BasicUser()
+	//basic.ProtoToJsonUser()
+	basic.JsonToProtoUser()
 }
