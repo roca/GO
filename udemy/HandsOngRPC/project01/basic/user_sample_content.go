f78b42880d0286380a415dc403562208eab4ee5e
romel.campbell <romel.campbell@regeneron.com>
2023-10-30 11:53:55 -0400

    WIP; 19.66

--- udemy/HandsOngRPC/project01/basic/user_sample_content.go
+++ udemy/HandsOngRPC/project01/basic/user_sample_content.go
@@ -3,17 +3,17 @@ package basic
 import (
 	"log"
 	"project01/proto/basic/protogen/basic"
+
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
-func BasicUserContentV1() {
+func BasicWriteUserContentV1() {
 	uc := basic.UserContent{
 		UserContentId: 1,
-		Slug:          "slug-v1",
-		Title:         "title",
-		HtmlContent:   "content",
-		AuthorId:      1,
-		//Category: "Fiction",
-		//SubCategory: "Science Fiction",
+		Slug:          "/this-is-v1",
+		// Title:         "10 Strongest People In The World",
+		HtmlContent:   "<p>Just dummy content for 10 Strongest People In The World</p>",
+		// AuthorId:      99,
 	}
 	WriteProtoToFile(&uc, "basic_user_content_v1.bin")
 }
@@ -23,4 +23,52 @@ func BasicReadUserContentV1() {
 	ReadProtoFromFile("basic_user_content_v1.bin", &uc)
 
 	log.Println(&uc)
+
+	jsonBytes, _ := protojson.Marshal(&uc)
+	log.Println(string(jsonBytes))
+}
+
+func BasicWriteUserContentV2() {
+	uc := basic.UserContent{
+		UserContentId: 2,
+		Slug:          "/this-is-v2",
+		// Title:         "10 Strongest People In The World Version 2",
+		HtmlContent:   "<p>Just dummy content for 10 Strongest People In The World Version 2</p>",
+		// AuthorId:      99,
+		// Category: "NEWS",
+	}
+	WriteProtoToFile(&uc, "basic_user_content_v2.bin")
+}
+
+func BasicReadUserContentV2() {
+	uc := basic.UserContent{}
+	ReadProtoFromFile("basic_user_content_v2.bin", &uc)
+
+	log.Println(&uc)
+
+	jsonBytes, _ := protojson.Marshal(&uc)
+	log.Println(string(jsonBytes))
+}
+
+func BasicWriteUserContentV3() {
+	uc := basic.UserContent{
+		UserContentId: 3,
+		Slug:          "/this-is-v3",
+		// Title:         "10 Strongest People In The World Version 3",
+		HtmlContent:   "<p>Just dummy content for 10 Strongest People In The World Version 3</p>",
+		// AuthorId:      99,
+		// Category: "NEWS",
+		// SubCategory: "PEOPLE",
+	}
+	WriteProtoToFile(&uc, "basic_user_content_v3.bin")
+}
+
+func BasicReadUserContentV3() {
+	uc := basic.UserContent{}
+	ReadProtoFromFile("basic_user_content_v3.bin", &uc)
+
+	log.Println(&uc)
+
+	jsonBytes, _ := protojson.Marshal(&uc)
+	log.Println(string(jsonBytes))
 }
