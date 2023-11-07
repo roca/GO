b0d411a08e9d4b5776dcd304d1781593d1aeb582
romel.campbell <romel.campbell@regeneron.com>
2023-11-07 14:06:06 -0500

    Starting new server app

---
+++ udemy/HandsOngRPC/grpc-go-server/go.mod
@@ -0,0 +1,3 @@
+module grpc-go-server
+
+go 1.21.0
---
+++ udemy/HandsOngRPC/grpc-go-server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	pb "proto/protogen/hello"
+	"time"
+
+	"google.golang.org/protobuf/encoding/protojson"
+
+	grpc "google.golang.org/grpc"
+)
+
+type logWriter struct{}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Printf(time.Now().Format("15:04:04") + " " + string(bytes))
+}
+
+func main() {
+	req := &pb.HelloRequest{
+		Name: "Joe",
+		Age:  32,
+	}
+
+	log.Println(req)
+
+	jsonBytes, _ := protojson.Marshal(req)
+	log.Println(string(jsonBytes))
+
+	cc := grpc.ClientConn{}
+	opts := []grpc.CallOption{}
+
+	client := pb.NewHelloServiceClient(&cc)
+
+	resp, err := client.SayHello(context.Background(), req, opts...)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(&resp)
+}
