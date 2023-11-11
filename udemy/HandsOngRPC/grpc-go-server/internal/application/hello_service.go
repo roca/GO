7fded3df2b0a8f45cfa92aba4364c55490b15cc9
romel.campbell <romel.campbell@regeneron.com>
2023-11-11 14:49:32 -0500

    WIP

--- udemy/HandsOngRPC/grpc-go-server/internal/application/hello_service.go
+++ udemy/HandsOngRPC/grpc-go-server/internal/application/hello_service.go
@@ -1,6 +1,10 @@
 package application
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 type HelloService struct{}
 
@@ -11,7 +15,16 @@ func (a *HelloService) GenerateHello(name string) string {
 func (a *HelloService) GenerateManyHellos(name string, count int) []string {
 	var hellos []string
 	for i := 0; i < count; i++ {
-		hellos = append(hellos, fmt.Sprintf("[%d] %s", i,a.GenerateHello(name)))
+		hellos = append(hellos, fmt.Sprintf("[%d] %s", i, a.GenerateHello(name)))
 	}
 	return hellos
+}
+
+func (a *HelloService) GenerateHelloToEveryone(names []string) string {
+	var hellos []string
+	for _, name := range names {
+		hellos = append(hellos, a.GenerateHello(name))
+	}
+	log.Println(hellos)
+	return strings.Join(hellos, ", ")
 }
--- udemy/HandsOngRPC/grpc-go-server/internal/port/service_port.go
+++ udemy/HandsOngRPC/grpc-go-server/internal/port/service_port.go
@@ -3,4 +3,5 @@ package port
 type HelloServicePort interface {
 	GenerateHello(name string) string
 	GenerateManyHellos(name string, count int) []string
+	GenerateHelloToEveryone(names []string) string
 }
