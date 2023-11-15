bbc69376327347b948315ee98a4a51acc6550236
Romel Campbell <RomelCampbell@gmail.com>
2023-11-15 11:26:13 -0500

    WIP: Sexction 39

---
+++ udemy/HandsOngRPC/grpc-go-server/data/bank_account.go
@@ -0,0 +1,102 @@
+package data
+
+import (
+	"time"
+
+	up "github.com/upper/db/v4"
+)
+
+// BankAccount struct
+type BankAccount struct {
+	ID        int       `db:"id,omitempty"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+// Table returns the table name
+func (t *BankAccount) Table() string {
+	return "bank_accounts"
+}
+
+// GetAll gets all records from the database, using upper
+func (t *BankAccount) GetAll(condition up.Cond) ([]*BankAccount, error) {
+	collection := upper.Collection(t.Table())
+	var all []*BankAccount
+
+	res := collection.Find(condition)
+	err := res.All(&all)
+	if err != nil {
+		return nil, err
+	}
+
+	return all, err
+}
+
+// Get gets one record from the database, by id, using upper
+func (t *BankAccount) Get(id int) (*BankAccount, error) {
+	var one BankAccount
+	collection := upper.Collection(t.Table())
+
+	res := collection.Find(up.Cond{"id": id})
+	err := res.One(&one)
+	if err != nil {
+		return nil, err
+	}
+	return &one, nil
+}
+
+// Update updates a record in the database, using upper
+func (t *BankAccount) Update(m BankAccount) error {
+	m.UpdatedAt = time.Now()
+	collection := upper.Collection(t.Table())
+	res := collection.Find(m.ID)
+	err := res.Update(&m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete deletes a record from the database by id, using upper
+func (t *BankAccount) Delete(id int) error {
+	collection := upper.Collection(t.Table())
+	res := collection.Find(id)
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Insert inserts a model into the database, using upper
+func (t *BankAccount) Insert(m BankAccount) (int, error) {
+	m.CreatedAt = time.Now()
+	m.UpdatedAt = time.Now()
+	collection := upper.Collection(t.Table())
+	res, err := collection.Insert(m)
+	if err != nil {
+		return 0, err
+	}
+
+	id := getInsertedID(res.ID())
+
+	return id, nil
+}
+
+// Builder is an example of using upper's sql builder
+func (t *BankAccount) Builder(id int) ([]*BankAccount, error) {
+	collection := upper.Collection(t.Table())
+
+	var result []*BankAccount
+
+	err := collection.Session().
+		SQL().
+		SelectFrom(t.Table()).
+		Where("id > ?", id).
+		OrderBy("id").
+		All(&result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
--- udemy/HandsOngRPC/grpc-go-server/go.mod
+++ udemy/HandsOngRPC/grpc-go-server/go.mod
@@ -3,15 +3,28 @@ module grpc-go-server
 go 1.21.0
 
 require (
+	github.com/upper/db/v4 v4.7.0
 	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d
 	google.golang.org/grpc v1.59.0
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/segmentio/fasthash v1.0.3 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
