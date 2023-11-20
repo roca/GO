95e4d2915417c34335637f72014ce97fbce4433b
romel.campbell <romel.campbell@regeneron.com>
2023-11-20 11:09:56 -0500

    Set interval func

--- udemy/HandsOngRPC/grpc-go-server/internal/application/bank_service.go
+++ udemy/HandsOngRPC/grpc-go-server/internal/application/bank_service.go
@@ -1,7 +1,9 @@
 package application
 
 import (
+	"fmt"
 	"grpc-go-server/data"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -20,4 +22,35 @@ func (b *BankService) FindCurrentBalance(uuid string) float64 {
 	account, _ := b.Models.BankAccounts.Get(uuid)
 
 	return account.CurrentBalance
+}
+
+// Create a function to inject dummy exchange rates to table bank_exchange_rates. The function should run and inject new exchange rate data every X seconds.
+func (b *BankService) InsertExchangeRatesAtInterval(exit chan bool, interval time.Duration) {
+	//er := &data.BankExchangeRate{}
+	insertFunc := func() {
+		//b.Models.BankExchangeRates.Insert(*er)
+		fmt.Println("# b.Models.BankExchangeRates.Insert(m BankExchangeRate)")
+	}
+	stop := runFuncAtInterval(insertFunc, interval)
+	<-exit
+	fmt.Println("Finished InsertExchangeRatesAtInterval")
+	stop <- true
+}
+
+func runFuncAtInterval(f func(), seconds time.Duration) chan bool {
+	ticker := time.NewTicker(seconds)
+	stop := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				f()
+			case <-stop:
+				ticker.Stop()
+				fmt.Println("Finished runFuncAtInterval")
+				return
+			}
+		}
+	}()
+	return stop
 }
