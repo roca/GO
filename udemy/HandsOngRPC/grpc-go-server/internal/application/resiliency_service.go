36da4ac452774c93c743a6b21ab4fcd5cb120c39
romel.campbell <romel.campbell@regeneron.com>
2023-12-07 11:23:44 -0500

    WIP

--- udemy/HandsOngRPC/grpc-go-server/internal/application/resiliency_service.go
+++ udemy/HandsOngRPC/grpc-go-server/internal/application/resiliency_service.go
@@ -1,5 +1,7 @@
 package application
 
+import "grpc-go-server/internal/port"
+
 type ResiliencyService struct{}
 
 func (r *ResiliencyService) GetResiliency(maxDelay, minDelay int32) (string, error) {
@@ -8,9 +10,9 @@ func (r *ResiliencyService) GetResiliency(maxDelay, minDelay int32) (string, err
 func (r *ResiliencyService) GetResiliencyStream(maxDelay, minDelay int32) (string, error) {
 	return "", nil
 }
-func (r *ResiliencyService) SendResiliencyStream(maxDelay, minDelay int32) (string, error) {
-	return "",nil
+func (r *ResiliencyService) SendResiliencyStream([]*port.ResiliencyRequest) (string, error) {
+	return "", nil
 }
-func (r *ResiliencyService) BidirectionalResiliencyStream() error {
+func (r *ResiliencyService) BidirectionalResiliencyStream(maxDelay, minDelay int32) <-chan *port.ResiliencyResponse {
 	return nil
 }
