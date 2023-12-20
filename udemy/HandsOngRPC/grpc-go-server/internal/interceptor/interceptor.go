4940e693e8be46ee0e82929f28454d86311f08f0
Romel Campbell <RomelCampbell@gmail.com>
2023-12-20 05:24:31 -0500

    WIP: server/client interceptor

--- udemy/HandsOngRPC/grpc-go-server/internal/interceptor/interceptor.go
+++ udemy/HandsOngRPC/grpc-go-server/internal/interceptor/interceptor.go
@@ -7,6 +7,7 @@ import (
 	"proto/protogen/go/resiliency"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 // Unary Interceptors
@@ -28,10 +29,40 @@ func BasicUnaryServerInterceptor() grpc.UnaryServerInterceptor {
 		handler grpc.UnaryHandler) (resp interface{}, err error) {
 
 		// TODO:
-		// 1. Make modifications of the req
-		// 2. Make modifications of the response
+		// Modify request message if the request message is HelloRequest. Append a string to request's Name
+		switch request := req.(type) {
+		case *hello.HelloRequest:
+			request.Name = "[MODIFIED REQUEST BY SERVER INTERCEPTOR - 1]" + request.Name
+		}
+		// Add two new response metadata (any key / value)
+		responseMetadata, ok := metadata.FromOutgoingContext(ctx)
+		if !ok {
+			responseMetadata = metadata.New(nil)
+		}
+		responseMetadata.Append("my-response-metadata-key-1", "my-response-metadata-value-1")
+		responseMetadata.Append("my-response-metadata-key-2", "my-response-metadata-value-2")
+		// Create new context with new response metadata
+		ctx = metadata.NewOutgoingContext(ctx, responseMetadata)
 
-		return nil, nil
+		// Set response header with new response metadata
+		grpc.SetHeader(ctx, responseMetadata)
+
+		// handle request with modified context
+		res, err := handler(ctx, req)
+
+		if err != nil {
+			return res, err
+		}
+
+		// modify response
+		switch response := res.(type) {
+		case *hello.HelloResponse:
+			response.Greet = "[MODIFIED RESPONSE BY SERVER INTERCEPTOR - 2]" + response.Greet
+		case *resiliency.ResiliencyResponse:
+			response.Response = "[MODIFIED RESPONSE BY SERVER INTERCEPTOR - 3]" + response.Response
+		}
+
+		return res, nil
 	}
 }
 
@@ -56,9 +87,22 @@ func BasicStreamServerInterceptor() grpc.StreamServerInterceptor {
 		ss grpc.ServerStream, info *grpc.StreamServerInfo,
 		handler grpc.StreamHandler) error {
 
+		// intercept stream
 		interceptedServerStream := &InterceptedServerStream{
 			ServerStream: ss,
 		}
+
+		// add response metadata
+		responseMetadata, ok := metadata.FromOutgoingContext(interceptedServerStream.Context())
+
+		if !ok {
+			responseMetadata = metadata.New(nil)
+		}
+
+		responseMetadata.Append("my-response-metadata-key-1", "my-response-metadata-value-1")
+		responseMetadata.Append("my-response-metadata-key-2", "my-response-metadata-value-2")
+
+		interceptedServerStream.SetHeader(responseMetadata)
 
 		// handle request
 		return handler(srv, interceptedServerStream)
@@ -72,7 +116,7 @@ func (s *InterceptedServerStream) RecvMsg(msg interface{}) error {
 
 	switch request := msg.(type) {
 	case *hello.HelloRequest:
-		request.Name = "[MODIFIED BY SERVER INTERCEPTOR - 4]" + request.Name
+		request.Name = "[MODIFIED REQUEST BY SERVER INTERCEPTOR - 4]" + request.Name
 	}
 
 	return nil
@@ -81,9 +125,9 @@ func (s *InterceptedServerStream) RecvMsg(msg interface{}) error {
 func (s *InterceptedServerStream) SendMsg(msg interface{}) error {
 	switch response := msg.(type) {
 	case *hello.HelloResponse:
-		response.Greet = "[MODIFIED BY SERVER INTERCEPTOR - 5]" + response.Greet
+		response.Greet = "[MODIFIED RESPONSE BY SERVER INTERCEPTOR - 5]" + response.Greet
 	case *resiliency.ResiliencyResponse:
-		response.Response = "[MODIFIED BY SERVER INTERCEPTOR - 6]" + response.Response
+		response.Response = "[MODIFIED RESPONSE BY SERVER INTERCEPTOR - 6]" + response.Response
 	}
 
 	return s.ServerStream.SendMsg(msg)
