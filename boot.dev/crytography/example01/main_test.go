20a2a8dd7b8377156448d843390d3be317d6d84e
romel.campbell <romel.campbell@regeneron.com>
2024-02-08 10:30:21 -0500

    Done example01 WIP: example02 "SYMMETRIC VS ASYMMETRIC"

--- boot.dev/crytography/example01/main_test.go
+++ boot.dev/crytography/example01/main_test.go
@@ -3,6 +3,8 @@ package main
 import "testing"
 
 func Test_encrypt_decrypt(t *testing.T) {
+	const masterKey = "kjhgfdsaqwertyuioplkjhgfdsaqwert"
+	const iv = "1234567812345678"
 	type args struct {
 		plainText string
 		key       string
@@ -11,16 +13,21 @@ func Test_encrypt_decrypt(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want string
-	}{}
+	}{
+		{"test1", args{"k33pThisPasswordSafe", masterKey, iv}},
+		{"test2", args{"12345", masterKey, iv}},
+		{"test3", args{"thePasswordOnMyLuggage", masterKey, iv}},
+		{"test4", args{"pizza_the_HUt", masterKey, iv}},
+	}
 
 	_ = tests
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := ""
-			if got != tt.want {
-				t.Errorf("encrypt() = %v, want %v", got, tt.want)
+			got := decrypt(encrypt(tt.args.plainText, masterKey, iv), masterKey, iv)
+			want := tt.args.plainText
+			if got != tt.args.plainText {
+				t.Errorf("encrypt() = %v, want %v", got, want)
 			}
 		})
 	}
