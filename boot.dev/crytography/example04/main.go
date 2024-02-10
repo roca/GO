66d876122cc0a2ae3673fc19966a48c957d9cf6c
Romel Campbell <RomelCampbell@gmail.com>
2024-02-10 14:08:43 -0500

    Example04: generateRandomKey

--- boot.dev/crytography/example04/main.go
+++ boot.dev/crytography/example04/main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
+	"crypto/rand"
 )
 
 func generateRandomKey(length int) (string, error) {
-	 bytes := make([]byte, length)
+	bytes := make([]byte, length)
 
-	 _ = bytes
-	 return "", nil
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x",string(bytes)), nil
 }
 
 // don't touch below this line
 
 func main() {
-	rand.Seed(0)
 	for i := 16; i < 33; i++ {
 		key, err := generateRandomKey(i)
 		if err != nil {
