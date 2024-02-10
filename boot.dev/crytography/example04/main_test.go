9b69b0f32c53d0283459a2536260f19628555204
Romel Campbell <RomelCampbell@gmail.com>
2024-02-10 14:22:44 -0500

    example04: adds regex test

--- boot.dev/crytography/example04/main_test.go
+++ boot.dev/crytography/example04/main_test.go
@@ -1,14 +1,24 @@
 package main
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func Test_generateRandomKey(t *testing.T) {
 	i := 32
-	randomString , err := generateRandomKey(i)
+	randomString, err := generateRandomKey(i)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-	if len(randomString) != 2 * i {
+	if len(randomString) != 2*i {
 		t.Errorf("Error: Expected 16, got %v", len(randomString))
+	}
+	match, err := regexp.MatchString("[a-z,A_Z]+|[0-9]+", randomString )
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if !match {
+		t.Errorf("Error: Expected true, got %v", match)
 	}
 }
