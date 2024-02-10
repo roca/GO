0f9f6d9b230f91dc458e59fd81f61a3dd409ac66
Romel Campbell <RomelCampbell@gmail.com>
2024-02-10 14:08:43 -0500

    Example04: generateRandomKey

--- boot.dev/crytography/example04/main_test.go
+++ boot.dev/crytography/example04/main_test.go
@@ -1,1 +1,14 @@
 package main
+
+import "testing"
+
+func Test_generateRandomKey(t *testing.T) {
+	i := 32
+	randomString , err := generateRandomKey(i)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if len(randomString) != 2 * i {
+		t.Errorf("Error: Expected 16, got %v", len(randomString))
+	}
+}
