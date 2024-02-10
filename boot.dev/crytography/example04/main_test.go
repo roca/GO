fe9db6cd23924ab2df4cf1b4a02fabab3401f335
Romel Campbell <RomelCampbell@gmail.com>
2024-02-10 18:06:13 -0500

    Changed regex test in example04

--- boot.dev/crytography/example04/main_test.go
+++ boot.dev/crytography/example04/main_test.go
@@ -14,11 +14,11 @@ func Test_generateRandomKey(t *testing.T) {
 	if len(randomString) != 2*i {
 		t.Errorf("Error: Expected 16, got %v", len(randomString))
 	}
-	match, err := regexp.MatchString("[a-z,A_Z]+|[0-9]+", randomString )
+	match, err := regexp.MatchString("\\W", randomString )
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-	if !match {
+	if match {
 		t.Errorf("Error: Expected true, got %v", match)
 	}
 }
