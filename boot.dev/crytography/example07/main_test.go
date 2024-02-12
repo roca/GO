c7355c3e383e4ef4827c5f583f8b7daa1fe2e84d
Romel Campbell <RomelCampbell@gmail.com>
2024-02-12 05:19:24 -0500

    WIP: example07 decoding

---
+++ boot.dev/crytography/example07/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_getHexBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want []byte
+	}{
+		{"HexnHello", "48:65:6c:6c:6f", []byte("Hello")},
+		{"HexWorld", "57:6f:72:6c:64", []byte("World")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getHexBytes(tt.text)
+			if err != nil {
+				t.Errorf("getHexBytes() error = %v", err)
+			}
+			if !bytes.Equal(got, tt.want) {
+				t.Errorf("getHexBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
