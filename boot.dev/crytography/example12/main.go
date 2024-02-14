f0ca01803038a09b294d687e6ea83962e080b3aa
Romel Campbell <RomelCampbell@gmail.com>
2024-02-14 16:37:29 -0500

    WIP: example 12 'One time pad'

---
+++ boot.dev/crytography/example12/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func crypt(plaintext, key []byte) []byte {
+	var xor []byte
+	for i := range plaintext {
+		xor = append(xor, plaintext[i]^key[i])
+	}
+	return xor
+}
+
+// don't touch below this line
+
+func encrypt(plaintext, key []byte) []byte {
+	return crypt(plaintext, key)
+}
+
+func decrypt(ciphertext, key []byte) []byte {
+	return crypt(ciphertext, key)
+}
+
+func test(plaintext, key []byte) {
+	ciphertext := encrypt(plaintext, key)
+	fmt.Printf("Encrypting '%s' using key '%s'\n", string(plaintext), string(key))
+	fmt.Printf("Encrypted ciphertext bytes: %v\n", ciphertext)
+	decrypted := decrypt(ciphertext, key)
+	fmt.Printf("Decrypted message: %v\n", string(decrypted))
+	fmt.Println("========")
+}
+
+func main() {
+	test([]byte("Shazam"), []byte("Sk7p13"))
+	test([]byte("I'm lovin it"), []byte("mysecurepass"))
+	test([]byte("Don't tell him I'm in love"), []byte("c5f149783abf22a96e9a7bb999"))
+}
---
+++ boot.dev/crytography/example12/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_crypto(t *testing.T) {
+	input := []byte("0110100001100101011011000110110001101111")
+	key := []byte("0111001101101010011001100111010101100100")
+	want := []byte("0001101100001111000010100001100100001011")
+
+	got := crypt(input, key)
+	if len(got) != len(want) {
+		t.Error("Wrong lengths")
+		return
+	}
+	if string(got) != string(want) {
+		t.Errorf("\ndata:\t%v\nkey:\t%v\noutput:\t%v\nwant:\t%v\n", input, string(key), string(got), string(want))
+	}
+}
