bc38b0a55fd88bd70dc6ed3ba6b6fa1a5bb5eee4
Romel Campbell <RomelCampbell@gmail.com>
2024-02-16 05:22:26 -0500

    WIP: example14 'State Ciphers'

--- boot.dev/crytography/godir.sh
+++ boot.dev/crytography/godir.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -f
 
 DIR=$1
+README_TEXT=$2
 
 if [ -z $DIR ]; then
   echo "Usage: godir <dir>"
@@ -13,4 +14,8 @@ mkdir $DIR
 $(cd $DIR && go mod init $DIR && touch main.go && touch main_test.go)
 echo "package main" > $DIR/main.go
 echo "package main" > $DIR/main_test.go
+if [ -z $README_TEXT ]; then
+  README_TEXT="# This is a Go project"
+fi
+echo  "# $DIR: $README_TEXT" > $DIR/README.md
 go work use $DIR
