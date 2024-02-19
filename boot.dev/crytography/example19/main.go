a1fae5bc6f0a8045ad1a5c75db5facee1aed9bad
Romel Campbell <RomelCampbell@gmail.com>
2024-02-19 09:06:59 -0500

    example19: DATA ENCRYPTION STANDARD

--- boot.dev/crytography/example19/main.go
+++ boot.dev/crytography/example19/main.go
@@ -4,16 +4,50 @@ import (
 	"bytes"
 	"crypto/cipher"
 	"crypto/des"
+	"crypto/rand"
 	"errors"
 	"fmt"
+	"io"
 )
 
 func encrypt(key, plaintext []byte) ([]byte, error) {
-	// ?
+	// 1. Create a new cipher block
+
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//2.Pad the plaintext with zeros using padMsg
+	plaintext = padMsg(plaintext, des.BlockSize)
+
+	//3. Generate a random iv and append it to the beginning of the ciphertext. It should be the same length as the block size
+	ciphertext := make([]byte, des.BlockSize+len(plaintext))
+	iv := ciphertext[:des.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	//4. Create a new encrypter
+	mode := cipher.NewCBCEncrypter(block, iv)
+
+	//5. Encrypt the blocks and return the entire ciphertext
+	mode.CryptBlocks(ciphertext[des.BlockSize:], plaintext)
+
+	return ciphertext, nil
 }
 
 func padMsg(plaintext []byte, blockSize int) []byte {
-	// ?
+
+	index := int(len(plaintext)/blockSize) * blockSize
+	lastBlock := plaintext[index:]
+
+	paddedBlock := padWithZeros(lastBlock, blockSize)
+
+	plaintext = append(plaintext[:index], paddedBlock...)
+
+	return plaintext
+
 }
 
 // don't touch below this line
