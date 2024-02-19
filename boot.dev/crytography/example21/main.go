e2af3cca33600f8d9ab7fd89b4a56ca3f20641b9
Romel Campbell <RomelCampbell@gmail.com>
2024-02-19 15:48:27 -0500

    WIP: example21: SUBSTITUTION BOX

---
+++ boot.dev/crytography/example21/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func sBox(b byte) (byte, error) {
+	if b > 0b1111 {
+		return 0, errors.New("invalid input")
+	}
+	box := [4][4]byte{
+		{0b00, 0b10, 0b01, 0b11},
+		{0b10, 0b00, 0b11, 0b01},
+		{0b01, 0b11, 0b00, 0b10},
+		{0b11, 0b01, 0b10, 0b00},
+	}
+
+	row := (b & 0b1100) >> 2
+	col := b & 0b0011
+	return box[row][col], nil
+}
+
+// don't touch below this line
+
+func main() {
+	for i := 0; i <= 16; i++ {
+		b := byte(i)
+		subbed, err := sBox(b)
+		if err != nil {
+			fmt.Printf("Error with input %04b: %v\n", i, err)
+			continue
+		}
+		fmt.Printf("%04b -> %02b\n", i, subbed)
+	}
+}
