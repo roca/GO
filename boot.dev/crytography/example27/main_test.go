2ca23345f86caf2ae780ba2d33a90d92e39da17b
Romel Campbell <RomelCampbell@gmail.com>
2024-02-24 15:09:56 -0500

    wip

--- boot.dev/crytography/example27/main_test.go
+++ boot.dev/crytography/example27/main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"math/big"
+	"testing"
+)
+
+func Test_getTot(t *testing.T) {
+	p := big.NewInt(11)
+	q := big.NewInt(7)
+	expected := "60"
+
+	tot := getTot(p, q)
+	if tot.String() != expected {
+		t.Errorf("Expected %s, got %s", expected, tot.String())
+	}
+}
+
+func Test_getE(t *testing.T) {
+	tot := big.NewInt(11)
+	expected := "7"
+
+	e := getE(tot)
+	if e.String() != expected {
+		t.Errorf("Expected %s, got %s", expected, e.String())
+	}
+}
