4dacac85f37cf299b841b12fc877d198b768c2c1
romel.campbell <romel.campbell@regeneron.com>
2024-02-27 09:40:56 -0500

    WIP: TOY HASH FUNCTION

---
+++ boot.dev/crytography/example33/README.md
@@ -0,0 +1,1 @@
+# example33: TOY HASH FUNCTION
---
+++ boot.dev/crytography/example33/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hash(input []byte) [4]byte {
+	// ?
+}
+
+// don't touch below this line
+
+func test(input string) {
+	fmt.Printf("Hashing '%s'...\n", input)
+	fmt.Printf("Output: %X\n", hash([]byte(input)))
+	fmt.Println("========")
+}
+
+func main() {
+	test("Example message")
+	test("This is a slightly longer example to hash")
+	test("This is a much longer example of some text to hash, maybe it's the opening paragraph of a blog post")
+}
