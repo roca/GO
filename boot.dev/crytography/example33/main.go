e0c266db319eadab70f791d8f697b8751327c1a4
romel.campbell <romel.campbell@regeneron.com>
2024-02-27 11:55:50 -0500

    example33: TOY HASH FUNCTION

--- boot.dev/crytography/example33/main.go
+++ boot.dev/crytography/example33/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 )
 
 func hash(input []byte) [4]byte {
-	// ?
+	final := [4]byte{}
+
+	for i, b := range input {
+		// do something with b
+		rotated := bits.RotateLeft8(uint8(b), 3)
+		shifted := rotated << 2
+		final[i%len(final)] ^= shifted
+	}
+	return final
 }
 
 // don't touch below this line
