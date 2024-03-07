f08a7a2628c9b7434ff243fc31cde95a03f68704
romel.campbell <romel.campbell@regeneron.com>
2024-03-07 13:29:36 -0500

    example08 baggage

--- udemy/PracticalObservability/tracing/example08/baggage_b/baggage_b.go
+++ udemy/PracticalObservability/tracing/example08/baggage_b/baggage_b.go
@@ -7,6 +7,9 @@ import (
 	"tracing/setup"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
@@ -17,6 +20,17 @@ func main() {
 
 	hf := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Header)
+
+		b := baggage.FromContext(r.Context())
+		span := trace.SpanFromContext(r.Context())
+		if b.Member("http.traffic.source").Value() != "" {
+			span.SetAttributes(
+				attribute.String(
+					"traffic.source",
+					b.Member("http.traffic.source").Value(),
+				),
+			)
+		}
 
 		if i := rand.Intn(2); i == 1 {
 			w.WriteHeader(http.StatusInternalServerError)
