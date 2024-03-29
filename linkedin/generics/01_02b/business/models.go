064ca076b6d2aeea24adde718cc7564532c7e073
romel.campbell <romel.campbell@regeneron.com>
2024-03-29 16:08:07 -0400

    01_02b: Uising option type param on the func call

---
+++ linkedin/generics/01_02b/business/models.go
@@ -0,0 +1,33 @@
+package business
+
+import "fmt"
+
+// Solar handles all the different energy offers powered by solar.
+type Solar struct {
+	Name  string
+	Netto float64
+}
+
+// Wind handles all the different energy offers powered by wind.
+type Wind struct {
+	Name  string
+	Netto float64
+}
+
+// Print prints the information for a solar product.
+// The string is enriched with the required kineteco legal information.
+func (s *Solar) Print() string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *s)
+}
+
+// Print prints the information for a wind product.
+// The string is enriched with the required kineteco legal information.
+func (w *Wind) Print() string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *w)
+}
+
+func PrintGeneric[T Wind | Solar](t T) string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, t)
+}
+
+var kinetecoPrint string = "Kineteco Deal:"
