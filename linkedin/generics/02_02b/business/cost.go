409ebfd3ea5d479ad72d53bac26df52b2af50ca3
Romel Campbell <RomelCampbell@gmail.com>
2024-04-01 05:09:45 -0400

    02_02b: Define type sets

---
+++ linkedin/generics/02_02b/business/cost.go
@@ -0,0 +1,9 @@
+package business
+
+func (s Solar) Cost() float64 {
+	return s.Netto * 0.4
+}
+
+func (w Wind) Cost() float64 {
+	return w.Netto * 0.4
+}
---
+++ linkedin/generics/02_02b/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"02_02b/business"
+)
+
+// main is our simple "playground" for the course.
+// Note, that in production code, it is a good practice to keep the main function short.
+func main() {
+	// Create three different energy offers of kineteco
+	solar2k := business.Solar{Name: "Solar 2000", Netto: 4.500}
+	solar3k := business.Solar{Name: "Solar 3000", Netto: 4.000}
+	windwest := business.Wind{Name: "Wind West", Netto: 3.950}
+
+	// Print details for each energy offer with kineteco branding
+	fmt.Printf(solar2k.Print())
+	fmt.Printf(solar3k.Print())
+	fmt.Printf(windwest.Print())
+	fmt.Printf("our first generic function: %s\n", business.PrintGeneric[business.Solar](solar2k))
+	fmt.Printf("our first generic function with wind: %s\n", business.PrintGeneric[business.Wind](windwest))
+
+	// Print functions for 01_04
+	ss := []business.Solar{solar2k, solar3k}
+	business.PrintSlice[business.Solar](ss)
+	business.PrintSlice[business.Wind]([]business.Wind{windwest, windwest})
+}
