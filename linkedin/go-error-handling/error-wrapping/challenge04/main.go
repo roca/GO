eed947ccc01320640317abe791896c3bdd055d61
Romel Campbell <RomelCampbell@gmail.com>
2024-04-08 09:19:32 -0400

    wip

---
+++ linkedin/go-error-handling/error-wrapping/challenge04/main.go
@@ -0,0 +1,45 @@
+// Write your answer here, and then test your code.
+// Your job is to implement the findLargest() method.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
+
+// isEven() returns an error if a number is even
+func isEven(n int) error {
+	if n%2 == 0 {
+		return fmt.Errorf("%d is an even number", n)
+	}
+	return nil
+}
+
+// isOdd returns a string channel containing
+// whether a value is odd
+func isOdd(n int) <-chan string {
+	// Your code goes here.
+	if err := isEven(n); err != nil {
+		return  
+	}
+	
+	return 
+}
+
+func main() {
+	// This is how your code will be called.
+	// Your answer should be the largest value in the numbers array.
+	// You can edit this code to try different testing cases.
+	var result string
+	for i := 0; i < 5; i++ {
+		result = <-isOdd(i)
+		fmt.Println(result)
+		time.Sleep(time.Millisecond)
+	}
+}
