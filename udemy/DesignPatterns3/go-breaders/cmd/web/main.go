a838e9d6cbb16aafdb3b07487eee9b369f772506
Romel Campbell <RomelCampbell@gmail.com>
2024-04-23 03:49:10 -0400

    WIP: Hello world

--- udemy/DesignPatterns3/go-breaders/cmd/web/main.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 const port = ":4000"
 
-func  main () {}
+type application struct{}
+
+func main() {
+	app := application{}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+
+	http.ListenAndServe(port, nil)
+
+}
