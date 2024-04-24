616cbab51183cf505e513532afbf217203a96d0f
Romel Campbell <RomelCampbell@gmail.com>
2024-04-24 03:48:27 -0400

    S2.L10

--- udemy/DesignPatterns3/go-breaders/cmd/web/main.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,12 +11,17 @@ const port = ":4000"
 type application struct{}
 
 func main() {
-	app := application{}
+	// app := application{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, world!")
 	})
 
-	http.ListenAndServe(port, nil)
+	fmt.Println("Starting web application on port", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Panic(err)
+	}
 
 }
