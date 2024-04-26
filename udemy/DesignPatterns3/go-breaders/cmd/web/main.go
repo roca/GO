1aa8f125ffa14ae4e0cc26c129424707f6b879e4
Romel Campbell <RomelCampbell@gmail.com>
2024-04-26 05:08:10 -0400

    S2.L13 setting up of chi router

--- udemy/DesignPatterns3/go-breaders/cmd/web/main.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = ":4000"
@@ -11,17 +12,21 @@ const port = ":4000"
 type application struct{}
 
 func main() {
-	// app := application{}
+	app := application{}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
-	})
+	srv := &http.Server{
+		Addr:              port,
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
 
 	fmt.Println("Starting web application on port", port)
 
-	err := http.ListenAndServe(port, nil)
+	err := srv.ListenAndServe()
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
-
 }
--- udemy/DesignPatterns3/go-breaders/cmd/web/routes.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/routes.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Get("/", app.ShowHome)
 
 	return mux
 }
