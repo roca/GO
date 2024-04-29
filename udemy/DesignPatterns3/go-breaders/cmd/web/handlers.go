0d99403c5f96457e42f08e471c98c3c2b054c101
Romel Campbell <RomelCampbell@gmail.com>
2024-04-29 05:03:17 -0400

    S2.L17 Stub pages

--- udemy/DesignPatterns3/go-breaders/cmd/web/handlers.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/handlers.go
@@ -7,3 +7,7 @@ import (
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
 	app.render(w, "home.page.gohtml", nil)
 }
+
+func (app *application) ShowAbout(w http.ResponseWriter, r *http.Request) {
+	app.render(w, "about.page.gohtml", nil)
+}
