d8b66697795234703c910f90bb710aa4756a5af8
Romel Campbell <RomelCampbell@gmail.com>
2024-04-30 03:42:42 -0400

    WIP: S2.L19

--- udemy/DesignPatterns3/go-breaders/cmd/web/routes.go
+++ udemy/DesignPatterns3/go-breaders/cmd/web/routes.go
@@ -14,6 +14,9 @@ func (app *application) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Timeout(60 * time.Second))
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	mux.Get("/", app.ShowHome)
 	mux.Get("/{page}", app.ShowPage)
 
