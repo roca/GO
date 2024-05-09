52919196c36e1df387c413bd2f6c11b39dea9580
romel.campbell <romel.campbell@regeneron.com>
2024-05-09 11:50:21 -0400

    S3.L30: PetFactoryInterface

---
+++ udemy/DesignPatterns3/go-breaders/pets/abstract-factory.go
@@ -0,0 +1,62 @@
+package pets
+
+import (
+	"errors"
+	"fmt"
+	"go-breaders/models"
+)
+
+type AnimalInterface interface {
+	Show() string
+}
+
+type DogFromFactory struct {
+	Pet *models.Dog
+}
+
+func (dff *DogFromFactory) Show() string {
+	return fmt.Sprintf("This animal is a %s", dff.Pet.Breed.Breed)
+}
+
+type CatFromFactory struct {
+	Pet *models.Cat
+}
+
+func (cff *CatFromFactory) Show() string {
+	return fmt.Sprintf("This animal is a %s", cff.Pet.Breed.Breed)
+}
+
+type PetFactoryInterface interface {
+	newPet() AnimalInterface
+}
+
+type DogAbstractFactory struct{}
+
+func (df *DogAbstractFactory) newPet() AnimalInterface {
+	return &DogFromFactory{
+		Pet: &models.Dog{},
+	}
+}
+
+type CatAbstractFactory struct{}
+
+func (cf *CatAbstractFactory) newPet() AnimalInterface {
+	return &CatFromFactory{
+		Pet: &models.Cat{},
+	}
+}
+
+func NewPetFromAbstractFactory(species string) (AnimalInterface, error) {
+	switch species {
+	case "dog":
+		var dogFactory DogAbstractFactory
+		dog := dogFactory.newPet()
+		return dog, nil
+	case "cat":
+		var catFactory DogAbstractFactory
+		cat := catFactory.newPet()
+		return cat, nil
+	default:
+		return nil, errors.New("invalid species supplied")
+	}
+}
