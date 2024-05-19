d965851161dcea25fa07292df312a3f84c7e1b4c
Romel Campbell <RomelCampbell@gmail.com>
2024-05-19 04:17:27 -0400

    Test for code example

--- leetcode/interviewquestions/main_test.go
+++ leetcode/interviewquestions/main_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	nums := []int{1, 1, 1, 2, 2, 3}
+	expected := 5
+
+	if got := removeDuplicates(nums); got != expected {
+		t.Errorf("removeDuplicates(%v) = %v, want %v", nums, got, expected)
+	}
+}
+
+func TestOccurrenceCount(t *testing.T) {
+	nums := []int{1, 1, 1, 2, 2, 3}
+	expected := map[int]int{1: 3, 2: 2, 3: 1}
+
+	// Map values are deeply equal when all of the following are true: 
+	// they are both nil or both non-nil, they have the same length, 
+	// and either they are the same map object or their corresponding 
+	// keys (matched using Go equality) map to deeply equal values.
+
+	if got := occurrenceCount(nums); !reflect.DeepEqual(got, expected) {
+		t.Errorf("occurrenceCount(%v) = %v, want %v", nums, got, expected)
+	}
+}
