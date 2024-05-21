b0e28f126229973e62c4c616b0f04b3e712045ce
romel.campbell <romel.campbell@regeneron.com>
2024-05-21 13:43:36 -0400

    WIP

--- leetcode/problem06/main.go
+++ leetcode/problem06/main.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	convert("PAYPALISHIRING", 3)
+	convert("PAYPALISHIRING", 4)
 }
 
 func convert(s string, numRows int) string {
 	m := matrix(s, numRows)
 
+	k := 0
 	for j := 0; j < len(s); j++ {
 		if j%2 == 0 {
 			for i := 0; i < numRows; i++ {
-				m[i][j] = rune(s[j])
+				if k < len(s) {
+					m[i][j] = rune(s[k])
+					k++
+				}
 			}
 		} else {
 			for i := numRows - 1; i >= 0; i-- {
-				m[i][j] = rune(s[j])
+				if k < len(s) {
+					m[i][j] = rune(s[k])
+					k++
+				}
 			}
 		}
 	}
