6b5be8910dd7d5051a11c88f10ca0c2a145e83b2
romel.campbell <romel.campbell@regeneron.com>
2024-05-23 13:35:58 -0400

    10. Regular Expression Matching

--- leetcode/problem10/main.go
+++ leetcode/problem10/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
 
+func main() {
+	fmt.Println(isMatch("abc", "a***abc"))
+}
 
 func isMatch(s string, p string) bool {
-    
+	re, _ := regexp.Compile("[*]+")
+	p = string(re.ReplaceAll([]byte(p), []byte("*")))
+
+	match, _ := regexp.MatchString("^"+p+"$", s)
+	return match
 }
--- leetcode/problem10/main_test.go
+++ leetcode/problem10/main_test.go
@@ -1,1 +1,26 @@
 package main
+
+import "testing"
+
+
+func TestIsMatch(t *testing.T) {
+	if isMatch("aa", "a") {
+		t.Error("Expected false, got true")
+	}
+
+	if !isMatch("aa", "a*") {
+		t.Error("Expected true, got false")
+	}
+
+	if !isMatch("aa", ".*") {
+		t.Error("Expected true, got false")
+	}
+
+	if !isMatch("ab", ".*") {
+		t.Error("Expected true, got false")
+	}
+
+	if !isMatch("abc", "a***abc") {
+		t.Error("Expected true, got false")
+	}
+}
