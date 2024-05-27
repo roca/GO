e52acea4a3cf2a4fa13b850b755bf8e60f5ee420
Romel Campbell <RomelCampbell@gmail.com>
2024-05-27 05:03:53 -0400

    S8.L64: Adapter pattern

--- udemy/DesignPatterns3/adapter/main.go
+++ udemy/DesignPatterns3/adapter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
@@ -9,17 +10,91 @@ import (
 )
 
 type ToDo struct {
-	UserID    int    `json:"userId"`
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
+	UserID    int    `json:"userId" xml:"userId"`
+	ID        int    `json:"id" xml:"id"`
+	Title     string `json:"title" xml:"title"`
+	Completed bool   `json:"completed" xml:"completed"`
+}
+
+type DataInterface interface {
+	GetData() (*ToDo, error)
+}
+
+type RemoteService struct {
+	Remote DataInterface
+}
+
+func (rs *RemoteService) CallRemoteService() (*ToDo, error) {
+	return rs.Remote.GetData()
+}
+
+type JSONBackend struct{}
+
+func (jb *JSONBackend) GetData() (*ToDo, error) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var todo ToDo
+	err = json.Unmarshal(body, &todo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
+
+type XMLBackend struct{}
+
+func (xb *XMLBackend) GetData() (*ToDo, error) {
+	xmlFile := `
+<?xml version="1.0" encoding="UTF-8"?>
+<todo>
+	<userId>1</userId>
+	<id>1</id>
+	<title>delectus aut autem</title>
+	<completed>false</completed>
+</todo>`
+
+	var todo ToDo
+	err := xml.Unmarshal([]byte(xmlFile), &todo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
 }
 
 func main() {
 	// No adapter
 	todo := getRemoteData()
-
 	fmt.Println("TODO without adapter:\t", todo.ID, todo.Title)
+
+	// With adapter, using JSONBackend
+	jasonBackend := &JSONBackend{}
+	jasonAdapter := &RemoteService{Remote: jasonBackend}
+
+	todo, err := jasonAdapter.CallRemoteService()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("TODO from JSON adapter:\t", todo.ID, todo.Title)
+
+	// With adapter, using XMLBackend
+	xmlBackend := &XMLBackend{}
+	xmlAdapter := &RemoteService{Remote: xmlBackend}
+
+	todo, err = xmlAdapter.CallRemoteService()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("TODO from XML adapter:\t", todo.ID, todo.Title)
 }
 
 func getRemoteData() *ToDo {
