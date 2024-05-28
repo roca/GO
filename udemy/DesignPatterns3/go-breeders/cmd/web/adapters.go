ea0ec83d97747b05d0e97c6a9ea110d9b2cf45ec
Romel Campbell <RomelCampbell@gmail.com>
2024-05-28 03:49:43 -0400

    S8.L66

---
+++ udemy/DesignPatterns3/go-breeders/cmd/web/adapters.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"go-breaders/models"
+	"io"
+	"net/http"
+)
+
+type CatBreedsInterface interface {
+	GetAllCatBreeds() ([]*models.CatBreed, error)
+}
+
+type RemoteService struct {
+	Remote CatBreedsInterface
+}
+
+func (rs *RemoteService) CallAllBreeds() ([]*models.CatBreed, error) {
+	return rs.Remote.GetAllCatBreeds()
+}
+
+type JSONBackend struct{}
+
+func (jb *JSONBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	resp, err := http.Get("https://localhost:8081/api/cat-breeds/json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var catBreeds []*models.CatBreed
+	err = json.Unmarshal(body, &catBreeds)
+	if err != nil {
+		return nil, err
+	}
+
+	return catBreeds, nil
+}
