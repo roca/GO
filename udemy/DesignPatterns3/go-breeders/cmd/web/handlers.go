0ddcef9c749dfd7b830fe5d02b6621910b461b01
Romel Campbell <RomelCampbell@gmail.com>
2024-05-30 03:54:12 -0400

    WIP: S8.L70

--- udemy/DesignPatterns3/go-breeders/cmd/web/handlers.go
+++ udemy/DesignPatterns3/go-breeders/cmd/web/handlers.go
@@ -108,3 +108,14 @@ func (app *application) CreateCatWithBuilder(w http.ResponseWriter, r *http.Requ
 
 	_ = t.WriteJSON(w, http.StatusOK, cat)
 }
+
+func (app *application) GetAllCatBreeds(w http.ResponseWriter, r *http.Request) {
+	var t toolkit.Tools
+	catBreeds, err := app.catService.CallAllBreeds()
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	_ = t.WriteJSON(w, http.StatusOK, catBreeds)
+}
