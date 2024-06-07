88e7744723237510a61609a84dac9e006442ed42
Romel Campbell <RomelCampbell@gmail.com>
2024-06-07 05:08:22 -0400

    S10.L89

---
+++ udemy/DesignPatterns3/go-breeders/sql/dom.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `dog_of_month` (
+                                `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                                `image` varchar(512) NOT NULL,
+                                `video` varchar(512) NOT NULL,
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
