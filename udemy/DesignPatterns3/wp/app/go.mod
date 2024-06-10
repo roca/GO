75fb312623a40595efaf0ef4abe9b3593c3634a2
romel.campbell <romel.campbell@regeneron.com>
2024-06-10 15:19:51 -0400

    S10.L96. Getting started with the App and Streamer projects

---
+++ udemy/DesignPatterns3/wp/app/go.mod
@@ -0,0 +1,3 @@
+module wp/app
+
+go 1.22.3
---
+++ udemy/DesignPatterns3/wp/app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"wp/streamer"
+)
+
+func main() {
+	// Define number of workers and jobs
+	const numJobs = 4
+	const numWorkers = 2
+
+	// Create channels for work and results
+	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
+	defer close(notifyChan)
+
+	videoQueue := make(chan streamer.VideoProcessingJob, numJobs)
+	defer close(videoQueue)
+
+	// Get a worker pool.
+
+	// Start the worker pool.
+
+	// Create 4 videos to send to the worker pool.
+
+	// Send the videos to the worker pool.
+
+	// Print out the results
+
+}
