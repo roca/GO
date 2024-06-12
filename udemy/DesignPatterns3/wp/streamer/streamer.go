964a09fe7ac911425f36dc9112a31151f56ac182
romel.campbell <romel.campbell@regeneron.com>
2024-06-12 11:28:11 -0400

    S10.L98 Getting started on the worker pool functionality

--- udemy/DesignPatterns3/wp/streamer/streamer.go
+++ udemy/DesignPatterns3/wp/streamer/streamer.go
@@ -7,17 +7,28 @@ type ProcessingMessage struct {
 	OutputFile string
 }
 
-type VideoProcessingJob struct{}
+type VideoProcessingJob struct{
+	Video Video
+}
 
-type Processor struct {}
+type Processor struct{}
 
+type Video struct {
+	ID           int
+	InputFile    string
+	OutputDir    string
+	EncodingType string
+	NotifyChan   chan ProcessingMessage
+	// Options *VideoOptions
+	Encoder Processor
+}
 
 func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
 	workPool := make(chan chan VideoProcessingJob, maxWorkers)
 
 	// TODO: implement processor logic
 	p := Processor{}
-	
+
 	return &VideoDispatcher{
 		jobQueue:   jobQueue,
 		maxWorkers: maxWorkers,
