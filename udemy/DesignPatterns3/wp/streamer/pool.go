5bb378a60743b9584f58b4cbba953b763b33cf86
Romel Campbell <RomelCampbell@gmail.com>
2024-06-13 03:51:06 -0400

    WIP S10.L100

--- udemy/DesignPatterns3/wp/streamer/pool.go
+++ udemy/DesignPatterns3/wp/streamer/pool.go
@@ -4,30 +4,62 @@ type VideoDispatcher struct {
 	WorkerPool chan chan VideoProcessingJob
 	maxWorkers int
 	jobQueue   chan VideoProcessingJob
-	Processor Processor
+	Processor  Processor
 }
 
 // type videoWorker
 // See https://tleyden.github.io/blog/2013/11/23/understanding-chan-chans-in-go/
 type videoWorker struct {
-	id        int
-	jobQueue  chan VideoProcessingJob
+	id         int
+	jobQueue   chan VideoProcessingJob
 	workerPool chan chan VideoProcessingJob
 }
 
-// newVideoWorker 
+// newVideoWorker
 func newVideoWorker(id int, workerPool chan chan VideoProcessingJob) videoWorker {
 	return videoWorker{
-		id: id,
-		jobQueue: make(chan VideoProcessingJob),
+		id:         id,
+		jobQueue:   make(chan VideoProcessingJob),
 		workerPool: workerPool,
 	}
 }
 
 // start()
+func (w videoWorker) start() {
+	go func() {
+		for {
+			// Add jobQueue to the worker pool
+			w.workerPool <- w.jobQueue
+
+			// Wait for a job to come in.
+			job := <-w.jobQueue
+
+			// Process the job
+			w.processVideoJob(job.Video)
+		}
+	}()
+}
 
 // Run()
 
 // dispatch()
+func (vd *VideoDispatcher) dispatch() {
+	for {
+		// Wait for a job to come in.
+		job := <-vd.jobQueue
+
+		go func() {
+			// Wait for a worker to be available.
+			workerJobQueue := <-vd.WorkerPool
+
+			// Send the job to the worker.
+			workerJobQueue <- job
+		}()
+
+	}
+}
 
 // processVideoJob()
+func (w videoWorker) processVideoJob(video Video) {
+	video.encode()
+}
