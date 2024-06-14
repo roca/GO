8227dc9b14403fbe8b54858645a89d9c660a25e4
romel.campbell <romel.campbell@regeneron.com>
2024-06-14 13:06:56 -0400

    SWIP:s10.L109

--- udemy/DesignPatterns3/wp/streamer/encoders.go
+++ udemy/DesignPatterns3/wp/streamer/encoders.go
@@ -3,6 +3,7 @@ package streamer
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 
 	"github.com/xfrr/goffmpeg/transcoder"
 )
@@ -50,9 +51,54 @@ func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
 		ffmpegCmd := exec.Command(
 			"ffmpeg",
 			"-i", v.InputFile,
+
 			"-map", "0:v:0",
 			"-map", "0:a:0",
+
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+
+			"-c:v", "libx264",
+			"-crf", "22",
+			"-c:a", "aac",
+			"-ar", "48000",
+
+			"-filter:v:0", "scale=-2:1080",
+			"-maxrate:v:0", v.Options.MaxRate1080p,
+			"-b:a:0", "128k",
+
+			"-filter:v:1", "scale=-2:720",
+			"-maxrate:v:1", v.Options.MaxRate720p,
+			"-b:a:1", "128k",
+
+			"-filter:v:2", "scale=-2:480",
+			"-maxrate:v:2", v.Options.MaxRate480p,
+			"-b:a:2", "64k",
+
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p",
+
+			"-preset", "slow",
+			"-hls_list_size", "0",
+			"-threads", "0",
+			"-f", "hls",
+			"-hls_playlist_type", "event",
+			"-hls_time", strconv.Itoa(v.Options.SegmentDuration),
+			"hls_flags", "indepent_segments",
+			"-hls_segment_type", "mpegts",
+			"-hls_playlist_type", "vod",
+			"master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
+			"-profile:v", "baseline",
+			"-level", "3.0",
+			"-progress", "-",
+			"-nostats",
+			fmt.Sprintf("%s/%s-%%v.m3u8", v.OutputDir, baseFileName),
 		)
+
+		_, err := ffmpegCmd.CombinedOutput()
+		result <- err
 	}(result)
 
 	// Listen to the result channel
@@ -60,7 +106,7 @@ func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
 	if err != nil {
 		return err
 	}
-	
+
 	// Return the result
 	return nil
 }
