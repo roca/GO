3fb39d81c2b8e1d7a1bec073b70ae687002e3a72
Romel Campbell <RomelCampbell@gmail.com>
2024-06-17 05:15:06 -0400

    WIP: S11.L113

---
+++ udemy/DesignPatterns3/go-breeders/streamer/encoders.go
@@ -0,0 +1,110 @@
+package streamer
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+
+	"github.com/xfrr/goffmpeg/transcoder"
+)
+
+type Encoder interface {
+	EncodeToMP4(v *Video, baseFileName string) error
+	EncodeToHLS(v *Video, baseFileName string) error
+}
+
+type VideoEncoder struct{}
+
+func (ve *VideoEncoder) EncodeToMP4(v *Video, baseFileName string) error {
+	// Create a transcoder.
+	trans := new(transcoder.Transcoder)
+
+	// Build the output path
+	outputPath := fmt.Sprintf("%s/%s.mp4", v.OutputDir, baseFileName)
+
+	// Initialize the transcoder
+	err := trans.Initialize(v.InputFile, outputPath)
+	if err != nil {
+		return err
+	}
+
+	// Set codec
+	trans.MediaFile().SetVideoCodec("libx264")
+
+	// Start transcoder process
+	done := trans.Run(false)
+
+	err = <-done
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
+	// Create a channel to get results
+	result := make(chan error)
+
+	// Spawn a goroutine to do the encode
+	go func(result chan error) {
+		ffmpegCmd := exec.Command(
+			"ffmpeg",
+			"-i", v.InputFile,
+
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+
+			"-c:v", "libx264",
+			"-crf", "22",
+			"-c:a", "aac",
+			"-ar", "48000",
+
+			"-filter:v:0", "scale=-2:1080",
+			"-maxrate:v:0", v.Options.MaxRate1080p,
+			"-b:a:0", "128k",
+
+			"-filter:v:1", "scale=-2:720",
+			"-maxrate:v:1", v.Options.MaxRate720p,
+			"-b:a:1", "128k",
+
+			"-filter:v:2", "scale=-2:480",
+			"-maxrate:v:2", v.Options.MaxRate480p, // Changed to MaxRate480p.
+			"-b:a:2", "64k",
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p", // changed = to dash in 720
+			"-preset", "slow",
+			"-hls_list_size", "0",
+			"-threads", "0",
+			"-f", "hls",
+			"-hls_playlist_type", "event",
+			"-hls_time", strconv.Itoa(v.Options.SegmentDuration),
+			"-hls_flags", "independent_segments", // fixed missing dash and spelling of independent.
+			"-hls_segment_type", "mpegts",
+			"-hls_playlist_type", "vod",
+			"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
+			"-profile:v", "baseline",
+			"-level", "3.0",
+			"-progress", "-",
+			"-nostats",
+			fmt.Sprintf("%s/%s-%%v.m3u8", v.OutputDir, baseFileName),
+		)
+
+		_, err := ffmpegCmd.CombinedOutput()
+		result <- err
+	}(result)
+
+	// Listen to the result channel
+	err := <-result
+	if err != nil {
+		return err
+	}
+
+	// Return the result
+	return nil
+}
---
+++ udemy/DesignPatterns3/go-breeders/streamer/streamer.go
@@ -0,0 +1,162 @@
+package streamer
+
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/roca/go-toolkit/v2"
+)
+
+type ProcessingMessage struct {
+	ID         int
+	Success    bool
+	Message    string
+	OutputFile string
+}
+
+type VideoProcessingJob struct {
+	Video Video
+}
+
+type Processor struct {
+	Engine Encoder
+}
+
+type Video struct {
+	ID           int
+	InputFile    string
+	OutputDir    string
+	EncodingType string
+	NotifyChan   chan ProcessingMessage
+	Options      *VideoOptions
+	Encoder      Processor
+}
+
+type VideoOptions struct {
+	RenameOutput    bool
+	SegmentDuration int
+	MaxRate1080p    string
+	MaxRate720p     string
+	MaxRate480p     string
+}
+
+func (vd *VideoDispatcher) NewVideo(id int, input string, output string, encType string, notifyChan chan ProcessingMessage, ops *VideoOptions) Video {
+	if ops == nil {
+		ops = &VideoOptions{}
+	}
+
+	fmt.Println("NewVideo(): new video created:", id, input)
+
+	return Video{
+		ID:           id,
+		InputFile:    input,
+		OutputDir:    output,
+		EncodingType: encType,
+		NotifyChan:   notifyChan,
+		Encoder:      vd.Processor,
+		Options:      ops,
+	}
+}
+
+func (v *Video) encode() {
+	var fileName string
+	switch v.EncodingType {
+	case "mp4":
+		// encode the video
+		fmt.Println("v.encode(): About to encode to mp4", v.ID)
+		name, err := v.encodeToMP4()
+		if err != nil {
+			v.sendToNotifyChan(false, "", fmt.Sprintf("encode failed for %d: %s", v.ID, err.Error()))
+			return
+		}
+		fileName = fmt.Sprintf("%s.mp4", name)
+	case "hls":
+		// encode the video
+		fmt.Println("v.encode(): About to encode to hls", v.ID)
+		name, err := v.encodeToHLS()
+		if err != nil {
+			v.sendToNotifyChan(false, "", fmt.Sprintf("encode failed for %d: %s", v.ID, err.Error()))
+			return
+		}
+		fileName = fmt.Sprintf("%s.m3u8", name)
+	default:
+		fmt.Println("v.encode(): error trying to encode unknown type", v.ID, v.EncodingType)
+		v.sendToNotifyChan(false, "", fmt.Sprintf("unknown encoding type %s", v.EncodingType))
+		return
+	}
+
+	fmt.Println("v.encode(): sending sucee message for video is", v.ID, "to notifyChan")
+	v.sendToNotifyChan(true, fileName, fmt.Sprintf("video id %d processed and saved as %s", v.ID, fileName))
+}
+
+func (v *Video) encodeToMP4() (string, error) {
+	baseFileName := ""
+	fmt.Println("v.encodeToMP4(): about to try to encode video id", v.ID)
+	if !v.Options.RenameOutput {
+		// Get the base file name
+		b := path.Base(v.InputFile)
+		baseFileName = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		var t toolkit.Tools
+		baseFileName = t.RandomString(10)
+	}
+
+	err := v.Encoder.Engine.EncodeToMP4(v, baseFileName)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("v.encodeToMP4(): successfully encoded video id", v.ID)
+
+	return baseFileName, nil
+}
+
+func (v *Video) encodeToHLS() (string, error) {
+	baseFileName := ""
+	fmt.Println("v.encodeToHLS(): about to try to encode video id", v.ID)
+	if !v.Options.RenameOutput {
+		// Get the base file name
+		b := path.Base(v.InputFile)
+		baseFileName = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		var t toolkit.Tools
+		baseFileName = t.RandomString(10)
+	}
+
+	err := v.Encoder.Engine.EncodeToHLS(v, baseFileName)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("v.encodeToHLS(): successfully encoded video id", v.ID)
+
+	return baseFileName, nil
+}
+
+func (v *Video) sendToNotifyChan(success bool, fileName, message string) {
+	fmt.Println("v.sendToNotifyChan(): sending message to notifyChan for video id", v.ID)
+	v.NotifyChan <- ProcessingMessage{
+		ID:         v.ID,
+		Success:    success,
+		Message:    message,
+		OutputFile: fileName,
+	}
+}
+
+func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
+	fmt.Println("New(): creating new worker pool with maxWorkers", maxWorkers)
+	workPool := make(chan chan VideoProcessingJob, maxWorkers)
+
+	// TODO: implement processor logic
+	var e VideoEncoder
+	p := Processor{
+		Engine: &e,
+	}
+
+	return &VideoDispatcher{
+		jobQueue:   jobQueue,
+		maxWorkers: maxWorkers,
+		WorkerPool: workPool,
+		Processor:  p,
+	}
+}
