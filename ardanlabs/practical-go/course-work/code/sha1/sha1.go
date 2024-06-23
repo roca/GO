0cb19b6ffd49e563ba778f29ce4c929fcd7f7ed8
Romel Campbell <RomelCampbell@gmail.com>
2024-06-23 04:27:14 -0400

    WIP Day 01 - Part04: video  19:00

--- ardanlabs/practical-go/course-work/code/sha1/sha1.go
+++ ardanlabs/practical-go/course-work/code/sha1/sha1.go
@@ -1,6 +1,44 @@
 package main
 
-//$ cat http.log.gz|gunzip|sha1sum 
+import (
+	"compress/gzip"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	sig,err := sha1Sum("http.log.gz")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(sig)
+}
+
+// $ cat http.log.gz|gunzip|sha1sum
 func sha1Sum(fileName string) (string, error) {
-	return "", nil
+	// idom: acquire a resource, check for error, defer release
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close() // idom: defer the release of the resource
+
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return "", err
+	}
+	// io.Copy(os.Stdout, r, 100)
+
+	w := sha1.New()
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
+
+	sig := w.Sum(nil)
+	return fmt.Sprintf("%x", sig), nil
+
 }
