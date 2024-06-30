80eb0eb249a853f21d57849e99aef376ee55b1ac
Romel Campbell <RomelCampbell@gmail.com>
2024-06-30 04:33:20 -0400

    WIP Day 02 - Part04: Exercises

--- ardanlabs/practical-go/course-work/code/day2/freq/freq.go
+++ ardanlabs/practical-go/course-work/code/day2/freq/freq.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
+	"strings"
 )
 
 // Who's on first -> [Who s on first]
@@ -30,40 +32,61 @@ func main() {
 	}
 	defer file.Close()
 
-	wordCount, err := wordFrequency(file)
+	freqs, err := wordFrequency(file)
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
-	for word, count := range wordCount {
+	for word, count := range freqs {
 		fmt.Printf("%s: %d\n", word, count)
 	}
+
+	mostFreqWord, mostFreqCount, err := maxWord(freqs)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	fmt.Printf("Most common word: %s (%d)\n", mostFreqWord, mostFreqCount)
 	// count the frequency of each word
 	// print the most common word
 }
 
+func sortByFrequenceCount(freqs map[string]int) []string {
+	words := make([]string, 0, len(freqs))
+	for word := range freqs {
+		words = append(words, word)
+	}
+	sort.Slice(words, func(i, j int) bool {
+		return freqs[words[i]] > freqs[words[j]]
+	})
+	return words
+}
+
+func maxWord(freqs map[string]int) (string, int, error) {
+	if len(freqs) == 0 {
+		return "", 0, fmt.Errorf("empty map")
+	}
+
+	maxN, maxW := 0, ""
+	for word, count := range freqs {
+		if count > maxN {
+			maxN, maxW = count, word
+		}
+	}
+	return maxW, maxN, nil
+}
+
 func wordFrequency(r io.Reader) (map[string]int, error) {
-	// read from r one line at a time
 	s := bufio.NewScanner(r)
-	wordCount := make(map[string]int)
-	lnum := 0
+	freqs := make(map[string]int)
 	for s.Scan() {
 		words := wordRe.FindAllString(s.Text(), -1) // current line
-		if len(words) != 0 {
-			lnum++
-			fmt.Println(words)
-			for _, word := range words {
-				wordCount[word]++
-			}
-			break
+		for _, w := range words {
+			freqs[strings.ToLower(w)]++
 		}
 	}
 	if err := s.Err(); err != nil {
 		return nil, err
 	}
-	fmt.Println("num lines:", lnum)
-	// split the text into words
-	// count the frequency of each word
-	// return the frequency map
 
-	return wordCount, nil
+	return freqs, nil
 }
