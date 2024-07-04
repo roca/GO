ed5dc4e9b8d69ccc7107a284dd3d21f44c586b3b
Romel Campbell <RomelCampbell@gmail.com>
2024-07-04 03:49:50 -0400

    WIP Day 03 - Part03: Video 13:00

--- ardanlabs/practical-go/course-work/code/payment/payment.go
+++ ardanlabs/practical-go/course-work/code/payment/payment.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+type Payment struct {
+	From   string
+	To     string
+	Amount float64 // USD
+}
+
+func (p *Payment) Process() {
+	fmt.Printf("%s - > $%.2f -> %s\n", p.From, p.Amount, p.To)
+}
