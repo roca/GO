da9c73d8ef1035d4acbb6242638c3e07aa695b20
romel.campbell <romel.campbell@regeneron.com>
2024-07-08 20:28:37 -0500

    wip Day 04 - Part03: Video 00:00

--- ardanlabs/practical-go/course-work/code/day4/nlp/nlp.go
+++ ardanlabs/practical-go/course-work/code/day4/nlp/nlp.go
@@ -3,6 +3,8 @@ package nlp
 import (
 	"regexp"
 	"strings"
+
+	"nlp/stemmer"
 )
 
 var (
@@ -14,7 +16,11 @@ func Tokenize(text string) []string {
 	var tokens []string
 	for _, w := range words {
 		token := strings.ToLower(w)
-		tokens = append(tokens, token)
+		token = stemmer.Stem(token)
+		if len(token) != 0 {
+			tokens = append(tokens, token)
+		}
+
 	}
 	return tokens
 }
