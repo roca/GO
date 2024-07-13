d8cce6884ce8b30294123d3ededafa0f6a74cbf6
Romel Campbell <RomelCampbell@gmail.com>
2024-07-13 04:18:10 -0400

    WIP Exercise: Got ContentLength and Etag from url

--- ardanlabs/practical-go/course-work/code/final_example/main.go
+++ ardanlabs/practical-go/course-work/code/final_example/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Head("https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2018-05.parquet")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	// // Get HEADERS
+	// for k, v := range resp.Header {
+	// 	log.Printf("%v: %v", k, v)
+	// }
+	contentLength := resp.Header.Get("Content-Length")
+	eTag := resp.Header.Get("ETag")
+
+	fmt.Printf("Content-Length: %v\n", contentLength)
+	fmt.Printf("ETag: %v\n", eTag)
+}
