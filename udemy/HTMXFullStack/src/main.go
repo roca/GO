444425fbc219fddff2fd46aacdec46e7ebc8f5ce
Romel Campbell <RomelCampbell@gmail.com>
2024-12-08 04:57:42 -0500

    Passing String and Map data to templates

--- udemy/HTMXFullStack/src/main.go
+++ udemy/HTMXFullStack/src/main.go
@@ -10,7 +10,24 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseGlob("./templates/*.html"))
 
-		err := tmpl.ExecuteTemplate(w, "greetingFragment", nil)
+		data := struct {
+			Name        string
+			Title       string
+			Description string
+			Socials     map[string]string
+		}{
+			Name:        "Alice",
+			Title:       "Visitor",
+			Description: "This paragraph is the new body of our page",
+			Socials: map[string]string{
+				"Facebook":  "example.com",
+				"Twitter/X": "@example.com",
+				"Instagram": "example-pics",
+				"LikedIn":   "example-inc",
+			},
+		}
+
+		err := tmpl.ExecuteTemplate(w, "home.html", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
