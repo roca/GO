7fcb8986fee34bcbddcba3c56ce71a61b9d8f8ec
romel.campbell <romel.campbell@regeneron.com>
2024-12-10 11:19:58 -0500

    Working standard router

--- udemy/HTMXFullStack/go-htmx-app/main.go
+++ udemy/HTMXFullStack/go-htmx-app/main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 
 	gRouter := mux.NewRouter()
+	gRouter.HandleFunc("/", HomeHandler)
 
 	http.ListenAndServe(":3000", gRouter)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the Home Page"))
 }
