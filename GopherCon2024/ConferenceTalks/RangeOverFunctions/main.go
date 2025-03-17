7f45755b2f5d0c50a08648af099cb78eb373f5ad
romel.campbell <romel.campbell@regeneron.com>
2025-03-17 15:21:14 -0400

    map using generics

---
+++ GopherCon2024/ConferenceTalks/RangeOverFunctions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "example/set"
+
+func main() {
+	// Create a new set
+	s := set.New[int]()
+	// Add elements
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+	// Create another set
+	t := set.New[int]()
+	// Add elements
+	t.Add(3)
+	t.Add(4)
+	t.Add(5)
+	// Union
+	u := set.Union(s, t)
+	// Print all elements
+	set.PrintAllElemetsPush(u)
+}
