f6c0aacfd4b7116dc6f7082809db215961c22011
Romel Campbell <RomelCampbell@gmail.com>
2025-03-23 04:50:20 -0400

    mustLoadImage func to get images as ebiten Image type

--- udemy/Ebitengine/go-asteroids/assets/assets.go
+++ udemy/Ebitengine/go-asteroids/assets/assets.go
@@ -1,1 +1,29 @@
 package assets
+
+import (
+	"embed"
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed *
+var assets embed.FS
+
+var PlayerSprite = mustLoadImage("images/player.png")
+
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
