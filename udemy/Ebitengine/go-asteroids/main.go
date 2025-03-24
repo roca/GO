c42bf0ff4b7570f5a97a8f6bd7f40b8c4e1d37c1
Romel Campbell <RomelCampbell@gmail.com>
2025-03-24 05:13:12 -0400

    Rotation left/right working

--- udemy/Ebitengine/go-asteroids/main.go
+++ udemy/Ebitengine/go-asteroids/main.go
@@ -2,14 +2,17 @@ package main
 
 import "github.com/hajimehoshi/ebiten/v2"
 
-type Game struct{}
+type Game struct{
+	player *Player
+}
 
 func (g *Game) Update() error {
+	g.player.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	g.player.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (ScreenWidth, ScreenHeight int) {
@@ -18,6 +21,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (ScreenWidth, ScreenHeigh
 
 func main() {
 	g := &Game{}
+	g.player = NewPlayer(g)
 
 	err := ebiten.RunGame(g)
 	if err != nil {
