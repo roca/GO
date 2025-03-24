43c91057ae8026bc33c8fc2656bfc90bf99d47a0
Romel Campbell <RomelCampbell@gmail.com>
2025-03-24 05:13:12 -0400

    Rotation left/right working

---
+++ udemy/Ebitengine/go-asteroids/player.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"go-asteroids/assets"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const rotationPerSecond = math.Pi
+
+type Player struct {
+	sprite   *ebiten.Image
+	rotation float64
+}
+
+func NewPlayer(game *Game) *Player {
+	sprite := assets.PlayerSprite
+
+	p := &Player{
+		sprite: sprite,
+	}
+
+	return p
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(p.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
+	screen.DrawImage(p.sprite, op)
+}
+
+func (p *Player) Update() {
+	speed := rotationPerSecond / float64(ebiten.TPS())
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		// fmt.Println("Left pressed")
+		p.rotation -= speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		// fmt.Println("Right pressed")
+		p.rotation += speed
+	}
+}
