e0a712053bcd8ba52db4c3d31fd3dc6a9596c10a
Romel Campbell <RomelCampbell@gmail.com>
2025-03-27 03:46:17 -0400

    WIP: making a title scene

--- udemy/Ebitengine/go-asteroids/assets/assets.go
+++ udemy/Ebitengine/go-asteroids/assets/assets.go
@@ -6,12 +6,37 @@ import (
 	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
-)
+	"golang.org/x/image/font"
 
 //go:embed *
 var assets embed.FS
 
 var PlayerSprite = mustLoadImage("images/player.png")
+var TiTleFont = titleFont("fonts/title.ttf")
+
+func titleFont(name string) font.Face {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	tt, err := opentype.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+
+	face, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    48,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return face
+}
 
 func mustLoadImage(name string) *ebiten.Image {
 	f, err := assets.Open(name)
