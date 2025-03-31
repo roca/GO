2f55987a771df52d3bf9ceab927f55637c1d02d7
romel.campbell <romel.campbell@regeneron.com>
2025-03-31 15:40:30 -0400

    WIP: 4. Adding Meteors to our Game 26. Getting started with meteor.go

--- udemy/Ebitengine/go-asteroids/asteroids/meteor.go
+++ udemy/Ebitengine/go-asteroids/asteroids/meteor.go
@@ -1,6 +1,12 @@
 package asteroids
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"go-asteroids/assets"
+	"math"
+	"math/rand"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 const (
 	rotationSpeedMin                    = -0.02
@@ -20,18 +26,54 @@ type Meteor struct {
 
 func NewMeteor(baseVelocity float64, game *GameScene, index int) *Meteor {
 	// Target the center of the screen
+	target := Vector{
+		X: ScreenWidth / 2,
+		Y: ScreenHeight / 2,
+	}
 
 	// Pick a random angle
+	angle := rand.Float64() * 2 * math.Pi
 
 	// The distance from the center that meteor should spawn at. Half the width, and some arbitrary distance
+	r := ScreenWidth/2.0 + 500
 
 	// Create the position vector, using the angle and simple math
+	pos := Vector{
+		X: target.X + r*math.Cos(angle),
+		Y: target.Y + r*math.Sin(angle),
+	}
 
 	// Keep the meteor moving toward the center of the screen
+	// F=Give it a random velocity
+	velocity := baseVelocity + rand.Float64()*1.5
+
+	// Create the direction vector and normalize it.
+	direction := Vector{
+		X: target.X - pos.X,
+		Y: target.Y - pos.Y,
+	}
+
+	normalizedDirection := direction.Normalize()
+
+	// Create the movement vector
+	movement := Vector{
+		X: normalizedDirection.X * velocity,
+		Y: normalizedDirection.Y * velocity,
+	}
 
 	// Assign a sprite to the meteor
 
-	// Create the meteor object and return it
+	sprite := assets.MeteorSprites[rand.Intn(len(assets.MeteorSprites))]
 
-	return nil
+	// Create the meteor object and return it
+	m := &Meteor{
+		game:          game,
+		position:      pos,
+		movement:      movement,
+		rotationSpeed: rotationSpeedMin + rand.Float64()*(rotationSpeedMax-rotationSpeedMin),
+		sprite:        sprite,
+		angle:         angle,
+	}
+
+	return m
 }
