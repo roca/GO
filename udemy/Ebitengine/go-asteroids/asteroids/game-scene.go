b8e38a3b79f2be7be3e235e1b662e058a9e96393
romel.campbell <romel.campbell@regeneron.com>
2025-04-10 15:21:59 -0400

    7. Adding Sound 43. What we'll build in this section

--- udemy/Ebitengine/go-asteroids/asteroids/game-scene.go
+++ udemy/Ebitengine/go-asteroids/asteroids/game-scene.go
@@ -37,6 +37,7 @@ type GameScene struct {
 	playerIsDead         bool            // Is the player dead?
 	audioContext         *audio.Context  // The audio context for the game
 	thrustPlayer         *audio.Player   // The audio player for the thrust sound
+	exhaust              *Exhaust        // The exhaust for the player
 }
 
 func NewGameScene() *GameScene {
@@ -70,6 +71,8 @@ func NewGameScene() *GameScene {
 func (g *GameScene) Update(state *State) error {
 	g.player.Update()
 
+	g.updateExhaust()
+
 	g.isPlayerDying()
 
 	g.isPlayerDead(state)
@@ -98,6 +101,11 @@ func (g *GameScene) Update(state *State) error {
 func (g *GameScene) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 
+	// Draw the exhaust
+	if g.exhaust != nil {
+		g.exhaust.Draw(screen)
+	}
+
 	// Draw the meteors
 	for _, m := range g.meteors {
 		m.Draw(screen)
@@ -111,6 +119,12 @@ func (g *GameScene) Draw(screen *ebiten.Image) {
 
 func (g *GameScene) Layout(outsideWidth, outsideHeight int) (ScreenWidth, ScreenHeight int) {
 	return outsideWidth, outsideHeight
+}
+
+func (g *GameScene) updateExhaust() {
+	if g.exhaust != nil {
+		g.exhaust.Update()
+	}
 }
 
 func (g *GameScene) isMeteorHitByPlayerLaser() {
@@ -166,7 +180,8 @@ func (g *GameScene) isPlayerDead(state *State) {
 	if g.player.isDead {
 		g.player.livesRemaining--
 		if g.player.livesRemaining == 0 {
-			state.SceneManager.GoToScene(NewGameScene())
+			g.Reset()
+			state.SceneManager.GoToScene(g)
 		}
 	}
 }
@@ -218,4 +233,20 @@ func (g *GameScene) cleanupMeteorsAndAliens() {
 		}
 		g.cleanupTimer.Reset()
 	}
+}
+
+func (g *GameScene) Reset() {
+	g.player = NewPlayer(g)
+	g.meteors = make(map[int]*Meteor)
+	g.meteorCount = 0
+	g.lasers = make(map[int]*Laser)
+	g.laserCount = 0
+	g.score = 0
+	g.meteorSpawnTimer.Reset()
+	g.baseVelocity = baseMeteorVelocity
+	g.velocityTimer.Reset()
+	g.playerIsDead = false
+	g.exhaust = nil
+	g.space.RemoveAll()
+	g.space.Add(g.player.playerObj)
 }
