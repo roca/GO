54d5aea537dcf2af44a9a8b3b0ab9ec5548c4900
Romel Campbell <RomelCampbell@gmail.com>
2025-04-11 05:13:25 -0400

    Can reverse also

--- udemy/Ebitengine/go-asteroids/asteroids/player.go
+++ udemy/Ebitengine/go-asteroids/asteroids/player.go
@@ -114,6 +114,10 @@ func (p *Player) Update() {
 
 	p.isDoneAccelerating()
 
+	p.reverse()
+	
+	p.isDoneReversing()
+
 	p.updateExhaustSprite()
 
 	p.playerObj.SetPosition(p.position.X, p.position.Y)
@@ -206,8 +210,46 @@ func (p *Player) isDoneAccelerating() {
 	}
 }
 
+func (p *Player) reverse() {
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		p.keepOnScreen()
+
+		dx := math.Sin(p.rotation) * -3
+		dy := math.Cos(p.rotation) * 3
+
+		bounds := p.sprite.Bounds()
+		halfW := float64(bounds.Dx()) / 2
+		halfH := float64(bounds.Dy()) / 2
+
+		spawnPos := Vector{
+			p.position.X + halfW + math.Sin(p.rotation)*-exhaustSpawnOffset,
+			p.position.Y + halfH + math.Cos(p.rotation)*exhaustSpawnOffset,
+		}
+
+		p.game.exhaust = NewExhaust(spawnPos, p.rotation+180.0*math.Pi/180.0)
+
+		p.position.X += dx
+		p.position.Y += dy
+
+		p.playerObj.SetPosition(p.position.X, p.position.Y)
+
+		if !p.game.thrustPlayer.IsPlaying() {
+			_ = p.game.thrustPlayer.Rewind()
+			p.game.thrustPlayer.Play()
+		}
+	}
+}
+
+func (p *Player) isDoneReversing() {
+	if inpututil.IsKeyJustReleased(ebiten.KeyDown) {
+		if p.game.thrustPlayer.IsPlaying() {
+			p.game.thrustPlayer.Pause()
+		}
+	}
+}
+
 func (p *Player) updateExhaustSprite() {
-	if !ebiten.IsKeyPressed(ebiten.KeyUp) && p.game.exhaust != nil {
+	if !ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyDown) && p.game.exhaust != nil {
 		p.game.exhaust = nil
 	}
 }
