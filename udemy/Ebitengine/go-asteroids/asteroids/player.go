359f3dbb30aa35171d2b76e0a717d00ce9500021
Romel Campbell <RomelCampbell@gmail.com>
2025-04-18 05:15:37 -0400

    S8.56. Adding Lives Remaining indicators II

--- udemy/Ebitengine/go-asteroids/asteroids/player.go
+++ udemy/Ebitengine/go-asteroids/asteroids/player.go
@@ -20,6 +20,7 @@ const (
 	laserSpawnOffset     = 50.0
 	maxShotsPerBurst     = 3
 	dyingAnimationAmount = 50 * time.Millisecond
+	numberOfLives      = 3
 )
 
 var curVelocity float64
@@ -40,6 +41,7 @@ type Player struct {
 	dyingTimer     *Timer
 	dyingCounter   int
 	livesRemaining int
+	lifeIndicators  []*LifeIndicator
 }
 
 func NewPlayer(game *GameScene) *Player {
@@ -58,6 +60,14 @@ func NewPlayer(game *GameScene) *Player {
 	// Create the player object for collision detection
 	playerObj := resolv.NewCircle(pos.X, pos.Y, float64(sprite.Bounds().Dx())/2)
 
+	var lifeIndicators []*LifeIndicator
+	var xPosition = 20.0
+	for i := 0; i < numberOfLives; i++ {
+		li := NewLifeIndicator(Vector{X: xPosition, Y: 20})
+		lifeIndicators = append(lifeIndicators, li)
+		xPosition += 50.0
+	}
+
 	p := &Player{
 		sprite:         sprite,
 		game:           game,
@@ -70,7 +80,8 @@ func NewPlayer(game *GameScene) *Player {
 		isDead:         false,
 		dyingTimer:     NewTimer(dyingAnimationAmount),
 		dyingCounter:   0,
-		livesRemaining: 1,
+		livesRemaining: numberOfLives,
+		lifeIndicators: lifeIndicators,
 	}
 
 	p.playerObj.SetPosition(pos.X, pos.Y)
