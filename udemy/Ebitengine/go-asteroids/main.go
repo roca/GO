78b86c0ab939622f862da97100fd0c72b4e1c64e
Romel Campbell <RomelCampbell@gmail.com>
2025-04-30 05:23:52 -0400

    S12.75. Building an executable with a custom icon for a Mac

--- udemy/Ebitengine/go-asteroids/main.go
+++ udemy/Ebitengine/go-asteroids/main.go
@@ -10,6 +10,12 @@ func main() {
 	ebiten.SetWindowTitle("Go Asteroids")
 	ebiten.SetWindowSize(asteroids.ScreenWidth, asteroids.ScreenHeight)
 
+	// Hiden cursor
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+
+	// Set to full screen
+	ebiten.SetFullscreen(true)
+
 	err := ebiten.RunGame(&asteroids.Game{})
 	if err != nil {
 		panic(err)
