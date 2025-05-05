ae3483363b27622129e81680c18c9ee6c06ec280
Romel Campbell <RomelCampbell@gmail.com>
2025-05-05 05:04:59 -0400

    WIP: tranaction type 2

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/transaction.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/transaction.go
@@ -1,5 +1,10 @@
 package database
 
+import (
+	"errors"
+	"math/big"
+)
+
 // Tx is the transactional information between two parties.
 type Tx struct {
 	ChainID uint16    `json:"chain_id"` // Ethereum: The chain id that is listed in the genesis file.
@@ -9,4 +14,37 @@ type Tx struct {
 	Value   uint64    `json:"value"`    // Ethereum: Monetary value received from this transaction.
 	Tip     uint64    `json:"tip"`      // Ethereum: Tip offered by the sender as an incentive to mine this transaction.
 	Data    []byte    `json:"data"`     // Ethereum: Extra data related to the transaction.
+}
+
+// NewTx constructs a new transaction.
+func NewTx(chainID uint16, nonce uint64, fromID AccountID, toID AccountID, value uint64, tip uint64, data []byte) (Tx, error) {
+	if !fromID.IsAccountID() {
+		return Tx{}, errors.New("from account is not properly formatted")
+	}
+	if !toID.IsAccountID() {
+		return Tx{}, errors.New("to account is not properly formatted")
+	}
+
+	tx := Tx{
+		ChainID: chainID,
+		Nonce:   nonce,
+		FromID:  fromID,
+		ToID:    toID,
+		Value:   value,
+		Tip:     tip,
+		Data:    data,
+	}
+
+	return tx, nil
+}
+
+// =============================================================================
+
+// SignedTx is a signed version of the transaction. This is how clients like
+// a wallet provide transactions for inclusion into the blockchain.
+type SignedTx struct {
+	Tx
+	V *big.Int `json:"v"` // Ethereum: Recovery identifier, either 29 or 30 with ardanID.
+	R *big.Int `json:"r"` // Ethereum: First coordinate of the ECDSA signature.
+	S *big.Int `json:"s"` // Ethereum: Second coordinate of the ECDSA signature.
 }
