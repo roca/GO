dc7d043af0d77473a3006ca59501eda3ac772e70
Romel Campbell <RomelCampbell@gmail.com>
2025-05-09 05:16:58 -0400

    WIP: Day02 2.1 13:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/account.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/account.go
@@ -2,6 +2,23 @@ package database
 
 import "errors"
 
+// Account represents information stored in the database for an individual account.
+type Account struct {
+	AccountID AccountID
+	Nonce     uint64
+	Balance   uint64
+}
+
+// newAccount constructs a new account value for use.
+func newAccount(accountID AccountID, balance uint64) Account {
+	return Account{
+		AccountID: accountID,
+		Balance:   balance,
+	}
+}
+
+// =============================================================================
+
 // AccountID represents an account id that is used to sign transactions and is
 // associated with transactions on the blockchain. This will be the last 20
 // bytes of the public key.
