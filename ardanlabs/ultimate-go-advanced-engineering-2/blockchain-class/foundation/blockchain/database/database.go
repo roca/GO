6a905d732993d46d4875b7b562b3195bad65774c
romel.campbell <romel.campbell@regeneron.com>
2025-05-09 11:22:27 -0400

    WIP: Accounting support Day02 2.1 29:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/database.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/database.go
@@ -1,3 +1,71 @@
 // Package database handles all the lower level support for maintaining the
 // blockchain in storage and maintaining an in-memory databse of account information.
 package database
+
+import (
+	"blockchain/foundation/blockchain/genesis"
+	"errors"
+	"sync"
+)
+
+// =============================================================================
+
+// Database manages data related to accounts who have transacted on the blockchain.
+type Database struct {
+	mu       sync.RWMutex
+	genesis  genesis.Genesis
+	accounts map[AccountID]Account
+}
+
+// New constructs a new database and applies account genesis information and
+// reads/writes the blockchain database on disk if a dbPath is provided.
+func New(genesis genesis.Genesis, evHandler func(v string, args ...any)) (*Database, error) {
+	db := Database{
+		genesis:  genesis,
+		accounts: make(map[AccountID]Account),
+	}
+
+	// Update the database with account balance information from genesis.
+	for accountStr, balance := range genesis.Balances {
+		accountID, err := ToAccountID(accountStr)
+		if err != nil {
+			return nil, err
+		}
+		db.accounts[accountID] = newAccount(accountID, balance)
+	}
+
+	return &db, nil
+}
+
+// Remove deletes an account from the database.
+func (db *Database) Remove(accountID AccountID) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	delete(db.accounts, accountID)
+}
+
+// Query retrieves an account from the database.
+func (db *Database) Query(accountID AccountID) (Account, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	acount, exists := db.accounts[accountID]
+	if !exists {
+		return Account{}, errors.New("account does not exist")
+	}
+
+	return acount, nil
+}
+
+// Copy makes a copy of the current accounts in the database.
+func (db *Database) Copy() map[AccountID]Account {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	accounts := make(map[AccountID]Account)
+	for accountID, account := range db.accounts {
+		accounts[accountID] = account
+	}
+	return accounts
+}
