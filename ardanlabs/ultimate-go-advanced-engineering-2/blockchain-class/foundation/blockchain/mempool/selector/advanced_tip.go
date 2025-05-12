d6903b536108c9e55e6c3a2cdc05156fe1569c3b
romel.campbell <romel.campbell@regeneron.com>
2025-05-12 13:49:49 -0400

    WIP: Mem pool Day 2.2 25:00

---
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/mempool/selector/advanced_tip.go
@@ -0,0 +1,103 @@
+package selector
+
+import (
+	"sort"
+
+	"blockchain/foundation/blockchain/database"
+)
+
+// advancedTipSelect returns transactions with the best tip while respecting the nonce
+// for each account/transaction. This strategy takes into account high-value transactions
+// that happens to be stuck on a low-nonce transaction with a low tip price.
+var advancedTipSelect = func(m map[database.AccountID][]database.BlockTx, howMany int) []database.BlockTx {
+	final := []database.BlockTx{}
+
+	// Sort the transactions per account by nonce.
+	for key := range m {
+		if len(m[key]) > 1 {
+			sort.Sort(byNonce(m[key]))
+		}
+	}
+
+	at := newAdvancedTips(m, howMany)
+	for from, num := range at.findBest() {
+		for i := 0; i < num; i++ {
+			final = append(final, m[from][i])
+		}
+	}
+
+	return final
+}
+
+// =============================================================================
+
+type advancedTips struct {
+	howMany   int
+	bestTip   uint64
+	bestPos   map[database.AccountID]int
+	groupTips map[database.AccountID][]uint64
+	groups    []database.AccountID
+}
+
+func newAdvancedTips(m map[database.AccountID][]database.BlockTx, howMany int) *advancedTips {
+	groupTips := map[database.AccountID][]uint64{}
+	groups := []database.AccountID{}
+
+	for from := range m {
+		groupTips[from] = []uint64{0}
+		groups = append(groups, from)
+	}
+
+	for from, group := range m {
+		for i, tx := range group {
+			if i > howMany {
+				break
+			}
+			groupTips[from] = append(groupTips[from], tx.Tip+groupTips[from][i])
+		}
+	}
+
+	return &advancedTips{
+		howMany:   howMany,
+		groupTips: groupTips,
+		groups:    groups,
+	}
+}
+
+func (at *advancedTips) findBest() map[database.AccountID]int {
+	at.findBestTransactions(0, 0, at.howMany, at.bestPos, 0)
+	return at.bestPos
+}
+
+func (at *advancedTips) findBestTransactions(groupID, pos int, left int, currPos map[database.AccountID]int, prevTip uint64) {
+	if prevTip > at.bestTip {
+		at.bestTip = prevTip
+		at.bestPos = currPos
+	}
+
+	if groupID >= len(at.groups) {
+		return
+	}
+	from := at.groups[groupID]
+
+	for pos, tip := range at.groupTips[from] {
+		if left-pos < 0 {
+			break
+		}
+
+		newCurrPos := copyMap(currPos)
+		newCurrPos[from] = pos
+		at.findBestTransactions(groupID+1, pos, left-pos, newCurrPos, prevTip+tip)
+	}
+}
+
+// =============================================================================
+
+func copyMap(m map[database.AccountID]int) map[database.AccountID]int {
+	newCurrPos := map[database.AccountID]int{}
+	for from, pos := range m {
+		newCurrPos[from] = pos
+	}
+
+	return newCurrPos
+}
---
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/mempool/selector/tip.go
@@ -0,0 +1,93 @@
+package selector
+
+import (
+	"blockchain/foundation/blockchain/database"
+	"sort"
+)
+
+// CORE NOTE: On Ethereum a transaction will stay in the mempool and not be selected
+// unless the transaction holds the next expected nonce. Transactions can get stuck
+// in the mempool because of this. This is very complicated for us to implement for
+// now. So we will check the nonce for each transaction when the block is mined.
+// If the nonce is not expected, it will fail but the user continues to pay fees.
+
+// tipSelect returns transactions with the best tip while respecting the nonce
+// for each account/transaction.
+var tipSelect = func(m map[database.AccountID][]database.BlockTx, howMany int) []database.BlockTx {
+
+	/*
+		Bill: {Nonce: 2, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 250},
+			  {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 150},
+		Pavl: {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 200},
+			  {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 75},
+		Edua: {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 75},
+			  {Nonce: 1, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 100},
+	*/
+
+	// Sort the transactions per account by nonce.
+	for key := range m {
+		if len(m[key]) > 1 {
+			sort.Sort(byNonce(m[key]))
+		}
+	}
+
+	/*
+		Bill: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 150},
+		      {Nonce: 2, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 250},
+		Pavl: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 75},
+		      {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 200},
+		Edua: {Nonce: 1, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 100},
+		      {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 75},
+	*/
+
+	// Pick the first transaction in the slice for each account. Each iteration
+	// represents a new row of selections. Keep doing that until all the
+	// transactions have been selected.
+	var rows [][]database.BlockTx
+	for {
+		var row []database.BlockTx
+		for key := range m {
+			if len(m[key]) > 0 {
+				row = append(row, m[key][0])
+				m[key] = m[key][1:]
+			}
+		}
+		if row == nil {
+			break
+		}
+		rows = append(rows, row)
+	}
+
+	/*
+		0: Bill: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 150},
+		0: Pavl: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 75},
+		0: Edua: {Nonce: 1, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 100},
+		1: Bill: {Nonce: 2, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 250},
+		1: Pavl: {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 200},
+		1: Edua: {Nonce: 2, To: "0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0", Tip: 75},
+	*/
+
+	// Sort each row by tip unless we will take all transactions from that row
+	// anyway. Then try to select the number of requested transactions. Keep
+	// pulling transactions from each row until the amount of fulfilled or
+	// there are no more transactions.
+	final := []database.BlockTx{}
+	for _, row := range rows {
+		need := howMany - len(final)
+		if len(row) > need {
+			sort.Sort(byTip(row))
+			final = append(final, row[:need]...)
+			break
+		}
+		final = append(final, row...)
+	}
+
+	/*
+		0: Bill: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 150},
+		1: Pavl: {Nonce: 1, To: "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76", Tip: 75},
+		2: Edua: {Nonce: 1, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 100},
+		3: Bill: {Nonce: 2, To: "0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9", Tip: 250},
+	*/
+
+	return final
+}
