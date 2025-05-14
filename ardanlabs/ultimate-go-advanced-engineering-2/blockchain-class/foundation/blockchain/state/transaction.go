a35a1a5ebebb01e4f310b4bb7f3966dfbdaec4a4
romel.campbell <romel.campbell@regeneron.com>
2025-05-14 15:09:13 -0400

    Mem pool Day 2.3, Support for adding transaction to the mempool 21:00

---
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/state/transaction.go
@@ -0,0 +1,25 @@
+package state
+
+import "blockchain/foundation/blockchain/database"
+
+// UpsertWalletTransaction accepts a transaction from a wallet for inclusion.
+func (s *State) UpsertWalletTransaction(signedTx database.SignedTx) error {
+	// CORE NOTE: It's up to the wallet to make sure the account has a proper
+	// balance and this transaction has a proper nonce. Fees will be taken if
+	// this transaction is mined into a block it doesn't have enough money to
+	// pay or the nonce isn't the next expected nonce for the account.
+
+	// Check the signed transaction has a proper signature, the from matches the
+	// signature, and the from and to fields are properly formatted.
+	if err := signedTx.Validate(s.genesis.ChainID); err != nil {
+		return err
+	}
+
+	const oneUnitOfGas = 1
+	tx := database.NewBlockTx(signedTx, s.genesis.GasPrice, oneUnitOfGas)
+	if err := s.mempool.Upsert(tx); err != nil {
+		return err
+	}
+
+	return nil
+}
