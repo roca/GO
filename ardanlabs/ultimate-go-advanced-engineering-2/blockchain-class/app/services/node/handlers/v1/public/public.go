f8074a80f36efa9644386439ad1e29db477a9c6b
romel.campbell <romel.campbell@regeneron.com>
2025-05-14 14:24:06 -0400

    Mem pool Day 2.3, Support for accounts information 5:29

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/public.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/public.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"net/http"
 
+	"blockchain/foundation/blockchain/database"
 	"blockchain/foundation/blockchain/state"
 	"blockchain/foundation/web"
 
@@ -21,4 +22,28 @@ type Handlers struct {
 func (h Handlers) Genesis(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	gen := h.State.Genesis()
 	return web.Respond(ctx, w, gen, http.StatusOK)
+}
+
+// Accounts returns the current balances for all users.
+func (h Handlers) Accounts(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	accountStr := web.Param(r, "account")
+
+	var accounts map[database.AccountID]database.Account
+	switch accountStr {
+	case "":
+		accounts = h.State.Accounts()
+
+	default:
+		accountID, err := database.ToAccountID(accountStr)
+		if err != nil {
+			return err
+		}
+		account, err := h.State.QueryAccount(accountID)
+		if err != nil {
+			return err
+		}
+		accounts = map[database.AccountID]database.Account{accountID: account}
+	}
+
+	return web.Respond(ctx, w, accounts, http.StatusOK)
 }
