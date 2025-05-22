fc480a25e5f5a2c0db3469ff0c30d1b5c4f65266
romel.campbell <romel.campbell@regeneron.com>
2025-05-22 14:16:45 -0400

    working mining operation

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/public.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/public.go
@@ -22,6 +22,18 @@ type Handlers struct {
 	NS    *nameservice.NameService
 }
 
+// Cancel stops the mining operation.
+func (h Handlers) Cancel(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	h.State.Worker.SignalCancelMining()
+	resp := struct {
+		Status string `json:"status"`
+	}{
+		Status: "canceling mining",
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
+}
+
 // SubmitWalletTransaction adds new transactions to the mempool.
 func (h Handlers) SubmitWalletTransaction(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	v, err := web.GetValues(ctx)
