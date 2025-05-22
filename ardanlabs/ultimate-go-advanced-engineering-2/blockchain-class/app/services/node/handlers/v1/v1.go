f6865ae8af678445af0393ea350d04a58dc4677e
romel.campbell <romel.campbell@regeneron.com>
2025-05-22 14:53:14 -0400

    working mining operation 3

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/v1.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/v1.go
@@ -32,7 +32,7 @@ func PublicRoutes(app *web.App, cfg Config) {
 		NS:    cfg.NS,
 	}
 
-	app.Handle(http.MethodGet, version, "/camcel", pbl.Cancel)
+	app.Handle(http.MethodGet, version, "/cancel", pbl.Cancel)
 
 	app.Handle(http.MethodGet, version, "/genesis/list", pbl.Genesis)
 	app.Handle(http.MethodGet, version, "/accounts/list", pbl.Accounts)
