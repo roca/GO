cce633dee29c04d98b436659ee58b238eb0c7f9b
Romel Campbell <RomelCampbell@gmail.com>
2025-05-26 05:10:07 -0400

    WIP: accounting working 3,3 35:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/models.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/public/models.go
@@ -2,6 +2,19 @@ package public
 
 import "blockchain/foundation/blockchain/database"
 
+type act struct {
+	Account database.AccountID `json:"account"`
+	Name    string             `json:"name"`
+	Balance uint64             `json:"balance"`
+	Nonce   uint64             `json:"nonce"`
+}
+
+type actInfo struct {
+	LastestBlock string `json:"lastest_block"`
+	Uncommitted  int    `json:"uncommitted"`
+	Accounts     []act  `json:"accounts"`
+}
+
 type tx struct {
 	FromAccount database.AccountID `json:"from"`
 	FromName    string             `json:"from_name"`
--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/state/state.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/state/state.go
@@ -98,6 +98,11 @@ func (s *State) Shutdown() error {
 	return nil
 }
 
+// LatestBlock returns a copy the current latest block.
+func (s *State) LatestBlock() database.Block {
+	return s.db.LatestBlock()
+}
+
 // MempoolLength returns the current length of the mempool.
 func (s *State) MempoolLength() int {
 	return s.mempool.Count()
