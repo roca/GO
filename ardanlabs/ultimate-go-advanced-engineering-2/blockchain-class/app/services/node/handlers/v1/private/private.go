d00e120e4b3776bf48a9ee9a0fb16c6ffd827a2f
romel.campbell <romel.campbell@regeneron.com>
2025-06-02 14:55:54 -0400

    WIP: 4.3 25:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/private/private.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/private/private.go
@@ -36,6 +36,42 @@ func (h Handlers) Sample(ctx context.Context, w http.ResponseWriter, r *http.Req
 	return web.Respond(ctx, w, resp, http.StatusOK)
 }
 
+// ProposeBlock takes a block received from a peer, validates it and
+// if that passes, adds the block to the local blockchain.
+func (h Handlers) ProposeBlock(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	// Decode the JSON in the post call into a file system block.
+	var blockData database.BlockData
+	if err := web.Decode(r, &blockData); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	// Convert the block data into a block. This action will create a merkle
+	// tree for the set of transactions required for blockchain operations.
+	block, err := database.ToBlock(blockData)
+	if err != nil {
+		return fmt.Errorf("unable to decode block: %w", err)
+	}
+
+	// Ask the state package to validate the proposed block. If the block
+	// passes validation, it will be added to the blockchain database.
+	if err := h.State.ProcessProposedBlock(block); err != nil {
+		// if errors.Is(err, database.ErrChainForked) {
+		// 	h.State.Reorganize()
+		// }
+
+		return errs.NewTrusted(errors.New("block not accepted"), http.StatusNotAcceptable)
+	}
+
+	resp := struct {
+		Status string `json:"status"`
+	}{
+		Status: "accepted",
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
+}
+
 // SubmitPeer is called by a node so they can be added to the known peer list.
 func (h Handlers) SubmitPeer(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	v, err := web.GetValues(ctx)
