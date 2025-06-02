f9a2af69102d2f0588f18a2cd06659d22c4fef5c
Romel Campbell <RomelCampbell@gmail.com>
2025-06-02 05:15:18 -0400

    WIP: 4.2 20:00 'Get Peers handlers'

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/v1.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/v1.go
@@ -49,5 +49,8 @@ func PrivateRoutes(app *web.App, cfg Config) {
 		NS: cfg.NS,
 	}
 
+	app.Handle(http.MethodPost, version, "/node/peers", prv.SubmitPeer)
 	app.Handle(http.MethodGet, version, "/node/status", prv.Status)
+	app.Handle(http.MethodGet, version, "/node/tx/list", prv.Mempool)
+	app.Handle(http.MethodGet, version, "/node/block/list/:from/:to", prv.BlocksByNumber)
 }
