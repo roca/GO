9e682da88dec7a2748348af28c36b268d0c575b3
Romel Campbell <RomelCampbell@gmail.com>
2025-06-02 05:15:18 -0400

    WIP: 4.2 20:00 'Get Peers handlers'

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/private/private.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/app/services/node/handlers/v1/private/private.go
@@ -3,13 +3,18 @@ package private
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"blockchain/foundation/blockchain/database"
 	"blockchain/foundation/blockchain/peer"
 	"blockchain/foundation/blockchain/state"
 	"blockchain/foundation/nameservice"
 	"blockchain/foundation/web"
 
+	"github.com/ardanlabs/blockchain/business/web/errs"
 	"go.uber.org/zap"
 )
 
@@ -31,6 +36,25 @@ func (h Handlers) Sample(ctx context.Context, w http.ResponseWriter, r *http.Req
 	return web.Respond(ctx, w, resp, http.StatusOK)
 }
 
+// SubmitPeer is called by a node so they can be added to the known peer list.
+func (h Handlers) SubmitPeer(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	var peer peer.Peer
+	if err := web.Decode(r, &peer); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	if !h.State.AddKnownPeer(peer) {
+		h.Log.Infow("adding peer", "traceid", v.TraceID, "host", peer.Host)
+	}
+
+	return web.Respond(ctx, w, nil, http.StatusOK)
+}
+
 // Status returns the current status of the node.
 func (h Handlers) Status(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	latestBlock := h.State.LatestBlock()
@@ -42,4 +66,48 @@ func (h Handlers) Status(ctx context.Context, w http.ResponseWriter, r *http.Req
 	}
 
 	return web.Respond(ctx, w, status, http.StatusOK)
+}
+
+// BlocksByNumber returns all the blocks based on the specified to/from values.
+func (h Handlers) BlocksByNumber(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	fromStr := web.Param(r, "from")
+	if fromStr == "latest" || fromStr == "" {
+		fromStr = fmt.Sprintf("%d", state.QueryLastest)
+	}
+
+	toStr := web.Param(r, "to")
+	if toStr == "latest" || toStr == "" {
+		toStr = fmt.Sprintf("%d", state.QueryLastest)
+	}
+
+	from, err := strconv.ParseUint(fromStr, 10, 64)
+	if err != nil {
+		return errs.NewTrusted(err, http.StatusBadRequest)
+	}
+	to, err := strconv.ParseUint(toStr, 10, 64)
+	if err != nil {
+		return errs.NewTrusted(err, http.StatusBadRequest)
+	}
+
+	if from > to {
+		return errs.NewTrusted(errors.New("from greater than to"), http.StatusBadRequest)
+	}
+
+	blocks := h.State.QueryBlocksByNumber(from, to)
+	if len(blocks) == 0 {
+		return web.Respond(ctx, w, nil, http.StatusNoContent)
+	}
+
+	blockData := make([]database.BlockData, len(blocks))
+	for i, block := range blocks {
+		blockData[i] = database.NewBlockData(block)
+	}
+
+	return web.Respond(ctx, w, blockData, http.StatusOK)
+}
+
+// Mempool returns the set of uncommitted transactions.
+func (h Handlers) Mempool(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	txs := h.State.Mempool()
+	return web.Respond(ctx, w, txs, http.StatusOK)
 }
--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/database.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/database/database.go
@@ -237,6 +237,17 @@ func (db *Database) ApplyTransaction(block Block, tx BlockTx) error {
 	return nil
 }
 
+// GetBlock searches the blockchain on disk to locate and return the
+// contents of the specified block by number.
+func (db *Database) GetBlock(num uint64) (Block, error) {
+	blockData, err := db.storage.GetBlock(num)
+	if err != nil {
+		return Block{}, err
+	}
+
+	return ToBlock(blockData)
+}
+
 // =============================================================================
 
 // DatabaseIterator provides support for iterating over the blocks in the
