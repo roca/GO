521c98ae486702067c19a1d9d32b0ab43d5fa4e8
romel.campbell <romel.campbell@regeneron.com>
2025-06-03 13:59:21 -0400

    prp sync 4.4 19:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/worker/worker.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/worker/worker.go
@@ -6,12 +6,20 @@ import (
 	"blockchain/foundation/blockchain/database"
 	"blockchain/foundation/blockchain/state"
 	"sync"
+	"time"
 )
+
+// peerUpdateInterval represents the interval of finding new peer nodes
+// and updating the blockchain on disk with missing blocks.
+const peerUpdateInterval = time.Second * 10
+
+// =============================================================================
 
 // Worker manages the POW workflows for the blockchain.
 type Worker struct {
 	state        *state.State
 	wg           sync.WaitGroup
+	ticker       time.Ticker
 	shut         chan struct{}
 	startMining  chan bool
 	cancelMining chan bool
@@ -24,6 +32,7 @@ type Worker struct {
 func Run(st *state.State, evHandler state.EventHandler) {
 	w := Worker{
 		state:        st,
+		ticker:       *time.NewTicker(peerUpdateInterval),
 		shut:         make(chan struct{}),
 		startMining:  make(chan bool, 1),
 		cancelMining: make(chan bool, 1),
@@ -39,6 +48,7 @@ func Run(st *state.State, evHandler state.EventHandler) {
 
 	// Load the set of operations we need to run.
 	operations := []func(){
+		w.peerOperations,
 		w.shareTxOperations,
 		w.powOperations,
 	}
