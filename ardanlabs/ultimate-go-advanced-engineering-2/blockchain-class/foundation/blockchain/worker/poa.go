2d6e90fae661e739688215d24df976f8717266b0
romel.campbell <romel.campbell@regeneron.com>
2025-06-05 13:38:24 -0400

    WIP: POA 5.1 43:00

--- ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/worker/poa.go
+++ ardanlabs/ultimate-go-advanced-engineering-2/blockchain-class/foundation/blockchain/worker/poa.go
@@ -1,8 +1,12 @@
 package worker
 
 import (
+	"blockchain/foundation/blockchain/state"
+	"context"
+	"errors"
 	"hash/fnv"
 	"sort"
+	"sync"
 	"time"
 )
 
@@ -51,6 +55,82 @@ func (w *Worker) runPoaOperation() {
 	// Run the selection algorithm.
 	peer := w.selection()
 	w.evHandler("worker: runPoaOperation: SELECTED: %s", peer)
+
+	// If we are not selected, return and wait for the new block.
+	if peer != w.state.Host() {
+		return
+	}
+
+	// Make sure there are transactions in the mempool.
+	length := w.state.MempoolLength()
+	if length == 0 {
+		w.evHandler("worker: runMiningOperation: MINING: no transactions to mine: Txs[%d]", length)
+		return
+	}
+
+	// Drain the cancel mining channel before starting.
+	select {
+	case <-w.cancelMining:
+		w.evHandler("worker: runMiningOperation: MINING: drained cancel channel")
+	default:
+	}
+
+	// Create a context so mining can be cancelled.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Can't return from this function until these G's are complete.
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+		// This G exists to cancel the mining operation.
+	go func() {
+		defer func() {
+			cancel()
+			wg.Done()
+		}()
+
+		select {
+		case <-w.cancelMining:
+			w.evHandler("worker: runMiningOperation: MINING: CANCEL: requested")
+		case <-ctx.Done():
+		}
+	}()
+
+	// This G is performing the mining.
+	go func() {
+		defer func() {
+			cancel()
+			wg.Done()
+		}()
+
+		t := time.Now()
+		block, err := w.state.MineNewBlock(ctx)
+		duration := time.Since(t)
+
+		w.evHandler("worker: runMiningOperation: MINING: mining duration[%v]", duration)
+
+		if err != nil {
+			switch {
+			case errors.Is(err, state.ErrNoTransactions):
+				w.evHandler("worker: runMiningOperation: MINING: WARNING: no transactions in mempool")
+			case ctx.Err() != nil:
+				w.evHandler("worker: runMiningOperation: MINING: CANCEL: complete")
+			default:
+				w.evHandler("worker: runMiningOperation: MINING: ERROR: %s", err)
+			}
+			return
+		}
+
+		// The block is mined. Propose the new block to the network.
+		// Log the error, but that's it.
+		if err := w.state.NetSendBlockToPeers(block); err != nil {
+			w.evHandler("worker: runMiningOperation: MINING: proposeBlockToPeers: WARNING %s", err)
+		}
+	}()
+
+	// Wait for both G's to terminate.
+	wg.Wait()
 }
 
 // selection selects a peer to be the next one to mine a block.
