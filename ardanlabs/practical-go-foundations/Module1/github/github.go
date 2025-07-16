1d7216dc6108c1ccae52d411a4c65175fb7ee546
Romel Campbell <romel.campbell@km4lh7hxv1.regeneron.regn.com>
2025-07-16 14:30:02 -0400

    Left off at M2.3

--- ardanlabs/practical-go-foundations/Module1/github/github.go
+++ ardanlabs/practical-go-foundations/Module1/github/github.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,6 +13,16 @@ import (
 // curl -i -H 'User-Agent: go' https://api.github.com/users/ardanlabs
 
 func main() {
+	Name, NumRepos, err := UserInfo("ardanlabs")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Name:", Name, ", NumRepos:", NumRepos)
+}
+
+func demo() {
 	resp, err := http.Get("https://api.github.com/users/ardanlabs")
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -39,21 +50,49 @@ func main() {
 
 	fmt.Println(reply.Name, reply.NumRepos)
 
-	/* JSON <-> Go
-
-	Types
-	string <-> string
-	true/false <-> bool
-	number <-> float[64,32....], int[8,64......], uint[8....]
-	array <-> []T, []any
-	object <-> map[string]any, struct
-
-	encoding/json API
-	JSON -> []byte -> Go: Unmarshal
-	Go -> []byte -> JSON: Marshal
-
-	JSON -> io.Reader -> Go: Decoder
-	Go -> io.Writer -> JSON Encoder
-	*/
-
 }
+
+// UserInfo return name and number of public repos from GitHub API.
+func UserInfo(login string) (string, int, error) {
+	url := "https://api.github.com/users/" + login
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", 0, fmt.Errorf("%q - bad status: %s", url, resp.Request.Response.Status)
+	}
+
+	return parseResponse(resp.Body)
+}
+
+func parseResponse(r io.Reader) (string, int, error) {
+	var reply struct {
+		Name     string
+		NumRepos int `json:"public_repos"`
+	}
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(&reply); err != nil {
+		return "", 0, err
+	}
+
+	return reply.Name, reply.NumRepos, nil
+}
+
+/* JSON <-> Go
+
+Types
+string <-> string
+true/false <-> bool
+number <-> float[64,32....], int[8,64......], uint[8....]
+array <-> []T, []any
+object <-> map[string]any, struct
+
+encoding/json API
+JSON -> []byte -> Go: Unmarshal
+Go -> []byte -> JSON: Marshal
+
+JSON -> io.Reader -> Go: Decoder
+Go -> io.Writer -> JSON Encoder
+*/
