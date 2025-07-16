e7c1a0f97291ea5548ff79d41735b4add5a0e6ab
Romel Campbell <romel.campbell@km4lh7hxv1.regeneron.regn.com>
2025-07-16 13:56:37 -0400

    github.go

--- ardanlabs/practical-go-foundations/Module1/github/github.go
+++ ardanlabs/practical-go-foundations/Module1/github/github.go
@@ -1,10 +1,60 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 // Given a github user login, return name and number of public repos
 // curl -i https://api.github.com/users/ardanlabs
+// curl -i -H 'User-Agent: go' https://api.github.com/users/ardanlabs
 
 func main() {
 	resp, err := http.Get("https://api.github.com/users/ardanlabs")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("ERROR: bad status - %s\n", resp.Status)
+		os.Exit(1)
+	}
+
+	ctype := resp.Header.Get("Content-Type")
+	fmt.Println("content-type:", ctype)
+
+	// io.Copy(os.Stdout, resp.Body)
+	type reply struct {
+		Name         string
+		Public_Repos int
+	}
+	var rply reply
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&rply); err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(rply)
+
+	/* JSON <-> Go
+
+	Types
+	string <-> string
+	true/false <-> bool
+	number <-> float[64,32....], int[8,64......], uint[8....]
+	array <-> []T, []any
+	object <-> map[string]any, struct
+
+	encoding/json API
+	JSON -> []byte -> Go: Unmarshal
+	Go -> []byte -> JSON: Marshal
+
+	JSON -> io.Reader -> Go: Decoder
+	Go -> io.Writer -> JSON Encoder
+	*/
+
 }
