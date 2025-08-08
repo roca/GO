490fa7e9f44159b5419d96ce63a4e682aa74c0a6
Romel Campbell <RomelCampbell@gmail.com>
2025-08-08 05:12:21 -0400

    WIP: M6.1 @11:00

---
+++ ardanlabs/practical-go-foundations/Module-5/nlp/example_test.go
@@ -0,0 +1,15 @@
+package nlp_test
+
+import (
+	"fmt"
+
+	"github.com/roca/GO/tree/staging/ardanlabs/practical-go-foundations/Module-5/nlp"
+)
+
+func ExampleTokenize() {
+	tokens := nlp.Tokenize("Who's on first?")
+	fmt.Println(tokens)
+
+	// Output:
+	// [who s on first]
+}
