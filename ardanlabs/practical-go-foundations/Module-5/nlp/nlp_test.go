07ea88d9b212c9bfb6a3bbde52112b52d1295f55
Romel Campbell <romel.campbell@gmail.com>
2025-08-11 11:30:31 -0400

    WIP: M6.5 @00:00

--- ardanlabs/practical-go-foundations/Module-5/nlp/nlp_test.go
+++ ardanlabs/practical-go-foundations/Module-5/nlp/nlp_test.go
@@ -2,21 +2,22 @@ package nlp
 
 import (
 	"os"
-	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestTokenize(t *testing.T) {
-	_ = require.New
 	text := "Who's on first?"
 	tokens := Tokenize(text)
 
 	expected := []string{"who", "s", "on", "first"}
-	if !slices.Equal(expected, tokens) {
-		t.Fatalf("expected %#v, got %#v", expected, tokens)
-	}
+	/*
+		if !slices.Equal(expected, tokens) {
+			t.Fatalf("expected %#v, got %#v", expected, tokens)
+		}
+	*/
+	require.Equal(t, expected, tokens)
 }
 
 func TestTokenizeTable(t *testing.T) {
@@ -32,10 +33,12 @@ func TestTokenizeTable(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.text, func(t *testing.T) {
 			tokens := Tokenize(tc.text)
-
-			if !slices.Equal(tc.expected, tokens) {
-				t.Fatalf("expected %#v, got %#v", tc.expected, tokens)
-			}
+			/*
+				if !slices.Equal(tc.expected, tokens) {
+					t.Fatalf("expected %#v, got %#v", tc.expected, tokens)
+				}
+			*/
+			require.Equalf(t, tc.expected, tokens,"expected %#v, got %#v",tc.expected, tokens)
 		})
 	}
 }
