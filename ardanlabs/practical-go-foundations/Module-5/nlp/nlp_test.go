719410866dab1f1b0cc99dc8608a46705adb618d
Romel Campbell <RomelCampbell@gmail.com>
2025-08-11 05:10:10 -0400

    WIP: 6.2 @09:00

---
+++ ardanlabs/practical-go-foundations/Module-5/nlp/nlp_test.go
@@ -0,0 +1,37 @@
+package nlp
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestTokenize(t *testing.T) {
+	text := "Who's on first?"
+	tokens := Tokenize(text)
+
+	expected := []string{"who", "s", "on", "first"}
+	if !slices.Equal(expected, tokens) {
+		t.Fatalf("expected %#v, got %#v", expected, tokens)
+	}
+}
+
+func TestTokenizeTable(t *testing.T) {
+	var cases = []struct {
+		text   string
+		expected []string
+	}{
+		{"Who's on first?", []string{"who", "s", "on", "first"}},
+		{"What's on second?", []string{"what", "s", "on", "second"}},
+		{"", nil},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.text, func(t *testing.T) {
+			tokens := Tokenize(tc.text)
+			
+			if !slices.Equal(tc.expected, tokens) {
+				t.Fatalf("expected %#v, got %#v", tc.expected, tokens)
+			}
+		})
+	}
+}
