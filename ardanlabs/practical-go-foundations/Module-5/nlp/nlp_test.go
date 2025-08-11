75923ac2d7d93148f6a07888d5240ce0c0cd45cd
Romel Campbell <romel.campbell@gmail.com>
2025-08-11 10:11:15 -0400

    changes for v0.1.0

--- ardanlabs/practical-go-foundations/Module-5/nlp/nlp_test.go
+++ ardanlabs/practical-go-foundations/Module-5/nlp/nlp_test.go
@@ -1,6 +1,7 @@
 package nlp
 
 import (
+	"os"
 	"slices"
 	"testing"
 )
@@ -17,7 +18,7 @@ func TestTokenize(t *testing.T) {
 
 func TestTokenizeTable(t *testing.T) {
 	var cases = []struct {
-		text   string
+		text     string
 		expected []string
 	}{
 		{"Who's on first?", []string{"who", "s", "on", "first"}},
@@ -28,10 +29,18 @@ func TestTokenizeTable(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.text, func(t *testing.T) {
 			tokens := Tokenize(tc.text)
-			
+
 			if !slices.Equal(tc.expected, tokens) {
 				t.Fatalf("expected %#v, got %#v", tc.expected, tokens)
 			}
 		})
+	}
+}
+
+var inCI = os.Getenv("CI") != ""
+
+func TestInCI(t *testing.T) {
+	if !inCI {
+		t.Skip("no in CI")
 	}
 }
