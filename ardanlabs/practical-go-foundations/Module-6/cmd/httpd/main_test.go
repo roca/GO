725d3b77280a2f1ed41b9bd23c1a34d1d33f0126
Romel Campbell <RomelCampbell@gmail.com>
2025-08-18 05:14:58 -0400

    WIP: M6.12 @00:00

--- ardanlabs/practical-go-foundations/Module-6/cmd/httpd/main_test.go
+++ ardanlabs/practical-go-foundations/Module-6/cmd/httpd/main_test.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_healthHandler(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/health", nil)
 
-	healthHandler(w, r)
+	api := API{log: slog.Default()}
+	api.healthHandler(w, r)
 
 	resp := w.Result()
-
-	fmt.Println(resp)
-
+	require.Equal(t, http.StatusOK, resp.StatusCode)
 }
---
+++ ardanlabs/practical-go-foundations/Module-6/go.mod
@@ -0,0 +1,3 @@
+module Module-6
+
+go 1.24.5
