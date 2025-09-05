a47dadd11da399eb1f14ef59d05af67f0d3efbce
Romel Campbell <romel.campbell@gmail.com>
2025-09-05 11:13:09 -0400

    WIP: example 1

---
+++ anthropic/example1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/anthropics/anthropic-sdk-go"
+)
+
+func main() {
+	// Install dependencies
+
+	// Load env varaibles
+	// SDK looks for 'ANTHROPIC_API_KEY' env by default
+
+	// Create an API Client
+	client := anthropic.NewClient()
+
+	// Make a Request
+}
