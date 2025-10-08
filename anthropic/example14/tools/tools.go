2c4d7ff1c1a6c743ca5a54a477db9b11f5e2fd7c
Romel Campbell <romel.campbell@gmail.com>
2025-10-08 18:13:10 -0400

    WIP

---
+++ anthropic/example14/tools/tools.go
@@ -0,0 +1,30 @@
+package tools
+
+import (
+	"encoding/json"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/invopop/jsonschema"
+)
+
+type ToolDefinition struct {
+	Name        string                         `json:"name"`
+	Description string                         `json:"description"`
+	InputSchema anthropic.ToolInputSchemaParam `json:"input_schema"`
+	Function    func(input json.RawMessage) (string, error)
+}
+
+func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
+	var reflector = jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+
+	var v T
+
+	schema := reflector.Reflect(v)
+
+	return anthropic.ToolInputSchemaParam{
+		Properties: schema.Properties,
+	}
+}
