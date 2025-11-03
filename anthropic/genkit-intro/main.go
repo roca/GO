3fd7a549cada0a259d25f7e53eb135595245297a
Romel Campbell <romel.campbell@gmail.com>
2025-11-03 10:48:21 -0500

    Adds cli project code

--- anthropic/genkit-intro/main.go
+++ anthropic/genkit-intro/main.go
@@ -6,16 +6,35 @@ import (
 
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
-	"github.com/firebase/genkit/go/plugins/googlegenai"
 )
 
 func main() {
+	// gemini()
+	anthropic()
+}
+
+// func gemini() {
+// 	ctx := context.Background()
+// 	g := genkit.Init(ctx, genkit.WithPlugins(&googlegenai.GoogleAI{}))
+//
+// 	resp, err := genkit.Generate(ctx, g,
+// 		ai.WithPrompt("Why is Genkit awesome?"),
+// 		ai.WithModelName("googleai/gemini-2.5-flash"),
+// 	)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	log.Println(resp.Text())
+// }
+
+func anthropic() {
 	ctx := context.Background()
-	g := genkit.Init(ctx, genkit.WithPlugins(&googlegenai.GoogleAI{}))
+	g := genkit.Init(ctx, genkit.WithPlugins(&anthropic.Anthropic{}))
 
 	resp, err := genkit.Generate(ctx, g,
 		ai.WithPrompt("Why is Genkit awesome?"),
-		ai.WithModelName("googleai/gemini-2.5-flash"),
+		ai.WithModelName("anthropic/claude-3-7-sonnet-20250219"),
 	)
 	if err != nil {
 		log.Fatal(err)
