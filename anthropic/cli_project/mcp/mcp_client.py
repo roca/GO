45f216955d56cd6f6c88684ff9d1956a4fa06280
Romel Campbell <romel.campbell@gmail.com>
2025-11-06 15:34:51 -0500

    Defined Resources

--- anthropic/cli_project/mcp/mcp_client.py
+++ anthropic/cli_project/mcp/mcp_client.py
@@ -5,6 +5,9 @@ from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
 
+import json
+from pydantic import AnyUrl
+
 
 class MCPClient:
     def __init__(
@@ -41,27 +44,54 @@ class MCPClient:
             )
         return self._session
 
+    # async def list_tools(self) -> list[types.Tool]:
+    #     # TODO: Return a list of tools defined by the MCP server
+    #     return []
+
+    # async def call_tool(
+    #     self, tool_name: str, tool_input: dict
+    # ) -> types.CallToolResult | None:
+    #     # TODO: Call a particular tool and return the result
+    #     return None
+
+    # async def list_prompts(self) -> list[types.Prompt]:
+    #     # TODO: Return a list of prompts defined by the MCP server
+    #     return []
+
+    # async def get_prompt(self, prompt_name, args: dict[str, str]):
+    #     # TODO: Get a particular prompt defined by the MCP server
+    #     return []
+
+    # async def read_resource(self, uri: str) -> Any:
+    #     # TODO: Read a resource, parse the contents and return it
+    #     return []
+
     async def list_tools(self) -> list[types.Tool]:
-        # TODO: Return a list of tools defined by the MCP server
-        return []
+        result = await self.session().list_tools()
+        return result.tools
 
     async def call_tool(
-        self, tool_name: str, tool_input: dict
+        self, tool_name: str, tool_input
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
-        # TODO: Return a list of prompts defined by the MCP server
-        return []
+        result = await self.session().list_prompts()
+        return result.prompts
 
     async def get_prompt(self, prompt_name, args: dict[str, str]):
-        # TODO: Get a particular prompt defined by the MCP server
-        return []
+        result = await self.session().get_prompt(prompt_name, args)
+        return result.messages
 
     async def read_resource(self, uri: str) -> Any:
-        # TODO: Read a resource, parse the contents and return it
-        return []
+        result = await self.session().read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mimeType == "application/json":
+                return json.loads(resource.text)
+
+            return resource.text
 
     async def cleanup(self):
         await self._exit_stack.aclose()
