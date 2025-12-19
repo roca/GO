0a317c24f52e16354e1431547df1c7c83242d4ec
Romel Campbell <romel.campbell@gmail.com>
2025-12-19 05:14:36 -0500

    Example creating pompts

---
+++ udemy/BuildAgentsandAIWorkflows/langchain-go/example02/string-to-template.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tmc/langchaingo/prompts"
+)
+
+func stringPromptTemplates() {
+	simpleTemplate := prompts.NewPromptTemplate(
+		"Write a {{.content_type}} about {{.subject}}",
+		[]string{"content_type", "subject"},
+	)
+
+	templatInput := map[string]any{
+		"content_type": "poem",
+		"subject":      "cats",
+	}
+
+	simplePrompt, _ := simpleTemplate.Format(templatInput)
+
+	fmt.Println(simplePrompt)
+
+}
