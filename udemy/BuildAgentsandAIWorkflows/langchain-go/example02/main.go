547d8f8e16add4358f63540c509eb2dcb1e48e5c
Romel Campbell <romel.campbell@gmail.com>
2025-12-22 11:29:50 -0500

    Created a system prompt

--- udemy/BuildAgentsandAIWorkflows/langchain-go/example02/main.go
+++ udemy/BuildAgentsandAIWorkflows/langchain-go/example02/main.go
@@ -1,11 +1,31 @@
 package main
 
+import (
+	"log"
+
+	"github.com/tmc/langchaingo/llms/anthropic"
+)
+
+var llm *anthropic.LLM
+
+func init() {
+	var err error
+
+	llm, err = anthropic.New(
+		anthropic.WithModel("claude-haiku-4-5-20251001"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create Anthropic LLM: %v", err)
+	}
+}
+
 func main() {
 	// stringPromptTemplates()
 	// standardTemplateDefinition()
 	// jinjaPromptTemplates()
 	// multilinePromptTemplates()
 	// partialVariablePromptTemplates()
-	promptWithModels()
+	// promptWithModels()
+	chatPromptTemplate()
 
 }
