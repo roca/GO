0282206794f69765691a250cd9f05cdaea135719
Romel Campbell <romel.campbell@gmail.com>
2025-12-29 04:41:44 -0500

    Using local Ollama instance

--- udemy/BuildAgentsandAIWorkflows/langchain-go/example02/using-local-models.go
+++ udemy/BuildAgentsandAIWorkflows/langchain-go/example02/using-local-models.go
@@ -1,5 +1,29 @@
 package main
 
-func usingLocalModels() {
+import (
+	"context"
+	"log"
 
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+func usingLocalModels() {
+	ctx := context.Background()
+
+	localModel, err := ollama.New(
+		ollama.WithServerURL("https://open-webui-ollama.eksdev.aws.regeneron.com"),
+		ollama.WithModel("llama3.2"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prompt := "What is the capital of United States ?"
+
+	response, err := localModel.Call(ctx, prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Response from local model: %s", response)
 }
