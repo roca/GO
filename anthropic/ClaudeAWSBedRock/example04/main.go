e0f4e67c06ac73a135d21a116b2adf5c66591739
Romel Campbell <romel.campbell@gmail.com>
2026-01-09 05:08:12 -0500

    WIP

--- anthropic/ClaudeAWSBedRock/example04/main.go
+++ anthropic/ClaudeAWSBedRock/example04/main.go
@@ -2,42 +2,64 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/anthropics/anthropic-sdk-go"
 	"github.com/anthropics/anthropic-sdk-go/bedrock"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-func main() {
+var client anthropic.Client
+
+func init() {
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
 		config.WithSharedConfigProfile("default"),
 		config.WithRegion("us-east-1"),
 	)
-	client := anthropic.NewClient(
+	if err != nil {
+		log.Fatalf("Error eablishing config: %v", err)
+	}
+
+	client = anthropic.NewClient(
 		// bedrock.WithLoadDefaultConfig(context.Background()),
 		bedrock.WithConfig(cfg),
 	)
+}
+
+func main() {
 
 	content := "Write me a function that checks a string for duplicate characters in Python."
 
 	println("[user]: " + content)
 
+	messages := []anthropic.MessageParam{
+		anthropic.NewUserMessage(anthropic.NewTextBlock(content)),
+	}
+
+	message, err := chat(messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	println("[assistant]: " + message.Content[0].Text + message.StopSequence)
+}
+
+func chat(messages []anthropic.MessageParam) (*anthropic.Message, error) {
+
 	message, err := client.Messages.New(context.TODO(), anthropic.MessageNewParams{
-		MaxTokens: 1024,
-		Messages: []anthropic.MessageParam{
-			anthropic.NewUserMessage(anthropic.NewTextBlock(content)),
-		},
+		MaxTokens:     1024,
+		Messages:      messages,
 		Model:         "global.anthropic.claude-haiku-4-5-20251001-v1:0",
 		StopSequences: []string{"```\n"},
 		System: []anthropic.TextBlockParam{
-			anthropic.TextBlockParam{Text: "You are a Python engineer who writes very concise code.", Type: "text"},
+			{Text: "You are a Python engineer who writes very concise code.", Type: "text"},
 		},
 	})
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	println("[assistant]: " + message.Content[0].Text + message.StopSequence)
+	return message, nil
 }
