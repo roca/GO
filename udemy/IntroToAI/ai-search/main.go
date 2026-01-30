292aaabcb03f3d5b0f90000c81270953df9e388d
Romel Campbell <romel.campbell@gmail.com>
2026-01-30 17:16:46 -0500

    Can load maze files

--- udemy/IntroToAI/ai-search/main.go
+++ udemy/IntroToAI/ai-search/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
 const (
 	DFS = iota
 	BFS
@@ -16,4 +25,109 @@ type Point struct {
 type Wall struct {
 	State Point
 	wall  bool
+}
+
+type Maze struct {
+	Height int
+	Width  int
+	Start  Point
+	Goal   Point
+	Walls  [][]Wall
+}
+
+func main() {
+	var m Maze
+	var maze, searchType string
+
+	flag.StringVar(&maze, "file", "maze.txt", "maze file")
+	flag.StringVar(&searchType, "search", "dfs", "search type: dfs, bfs, gbfs, astar, dijkstra")
+	flag.Parse()
+
+	err := m.Load(maze)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Maze Height/Width: %d/%d\n", m.Height, m.Width)
+}
+
+func (g *Maze) Load(fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return fmt.Errorf("error opening file %s: %v", fileName, err)
+	}
+	defer f.Close()
+
+	var fileContents []string
+
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("error reading file %s: %v", fileName, err)
+		}
+		fileContents = append(fileContents, line)
+	}
+
+	foundStart, foundEnd := false, false
+	for _, line := range fileContents {
+		if strings.Contains(line, "A") {
+			foundStart = true
+		}
+		if strings.Contains(line, "B") {
+			foundEnd = true
+		}
+	}
+
+	if !foundStart {
+		return fmt.Errorf("start point 'A' not found in the maze")
+	}
+	if !foundEnd {
+		return fmt.Errorf("end point 'B' not found in the maze")
+	}
+
+	g.Height = len(fileContents)
+	g.Width = len(fileContents[0])
+
+	var rows [][]Wall
+
+	for i, row := range fileContents {
+		var cols []Wall
+		for j, col := range row {
+			curLetter := fmt.Sprintf("%c", col)
+			var wall Wall
+			switch curLetter {
+			case "A":
+				g.Start = Point{Row: i, Col: j}
+				wall.State.Row = i
+				wall.State.Col = j
+				wall.wall = false
+			case "B":
+				g.Goal = Point{Row: i, Col: j}
+				wall.State.Row = i
+				wall.State.Col = j
+				wall.wall = false
+			case " ":
+				wall.State.Row = i
+				wall.State.Col = j
+				wall.wall = false
+			case "#":
+				wall.State.Row = i
+				wall.State.Col = j
+				wall.wall = true
+			default:
+				continue
+			}
+			cols = append(cols, wall)
+		}
+		rows = append(rows, cols)
+	}
+
+	g.Walls = rows
+
+	return nil
 }
