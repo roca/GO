cf3b90e497659de6364f3bdd969e9315a935bce3
Romel Campbell <romel.campbell@gmail.com>
2026-02-01 15:51:38 -0500

    WIP: Impements DFS solution

--- udemy/IntroToAI/ai-search/dfs.go
+++ udemy/IntroToAI/ai-search/dfs.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"slices"
+)
 
 // DepthFirstSearch struct implements the DFS algorithm
 type DepthFirstSearch struct {
@@ -42,4 +47,109 @@ func (dfs *DepthFirstSearch) Remove() (*Node, error) {
 		return node, nil
 	}
 	return nil, fmt.Errorf("Frontier is empty")
+}
+
+func (dfs *DepthFirstSearch) Solve() {
+	fmt.Println("Solving maze using Depth-First Search...")
+	dfs.Game.NumExplored = 0
+
+	start := Node{
+		State:  dfs.Game.Start,
+		Parent: nil,
+		Action: "",
+	}
+	dfs.Add(&start)
+	dfs.Game.CurrentNode = &start
+
+	for {
+		if dfs.Empty() {
+			return
+		}
+
+		currentNode, err := dfs.Remove()
+		if err != nil {
+			log.Println("Error removing node from frontier:", err)
+			return
+		}
+
+		if dfs.Game.Debug {
+			fmt.Println("Removed:", currentNode.State)
+			fmt.Println("---------------")
+			fmt.Println()
+		}
+
+		dfs.Game.CurrentNode = currentNode
+		dfs.Game.NumExplored += 1
+
+		// Have we found the solution?
+		if dfs.Game.Goal == currentNode.State {
+			var actions []string
+			var cells []Point
+
+			for {
+				if currentNode.Parent != nil {
+					actions = append(actions, currentNode.Action)
+					cells = append(cells, currentNode.State)
+					currentNode = currentNode.Parent
+				} else {
+					break
+				}
+			}
+
+			// Reverse the actions and cells
+			slices.Reverse(actions)
+			slices.Reverse(cells)
+
+			dfs.Game.Solution = Solution{
+				Actions: actions,
+				Cells:   cells,
+			}
+
+			dfs.Game.Explored = append(dfs.Game.Explored, currentNode.State)
+		}
+
+		dfs.Game.Explored = append(dfs.Game.Explored, currentNode.State)
+
+		for _, neighbor := range dfs.Neighbors(currentNode) {
+			if !dfs.ContainsState(neighbor) {
+				if !inExplored(neighbor.State, dfs.Game.Explored) {
+					dfs.Add(&Node{
+						State:  neighbor.State,
+						Parent: currentNode,
+						Action: neighbor.Action,
+					})
+				}
+			}
+		}
+	}
+}
+
+func (dfs *DepthFirstSearch) Neighbors(node *Node) []*Node {
+	row := node.State.Row
+	col := node.State.Col
+
+	candidates := []*Node{
+		{State: Point{Row: row - 1, Col: col}, Parent: node, Action: "up"},
+		{State: Point{Row: row + 1, Col: col}, Parent: node, Action: "down"},
+		{State: Point{Row: row, Col: col - 1}, Parent: node, Action: "left"},
+		{State: Point{Row: row, Col: col + 1}, Parent: node, Action: "right"},
+	}
+
+	var neighbors []*Node
+	for _, candidate := range candidates {
+		if 0 <= candidate.State.Row && candidate.State.Row < dfs.Game.Height {
+			if 0 <= candidate.State.Col && candidate.State.Col < dfs.Game.Width {
+				if !dfs.Game.Walls[candidate.State.Row][candidate.State.Col].wall {
+					neighbors = append(neighbors, candidate)
+				}
+			}
+		}
+	}
+
+	for i := range neighbors {
+		j := rand.Intn(i + 1)
+		neighbors[i], neighbors[j] = neighbors[j], neighbors[i]
+	}
+
+	return neighbors
 }
