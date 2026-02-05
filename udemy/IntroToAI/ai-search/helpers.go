d42390fd552dfca81419fcc405efa680703888f3
Romel Campbell <romel.campbell@gmail.com>
2026-02-05 13:33:32 -0500

    Adds animoation

--- udemy/IntroToAI/ai-search/helpers.go
+++ udemy/IntroToAI/ai-search/helpers.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 func inExplored(needle Point, haystack []Point) bool {
 	for _, point := range haystack {
 		if point.Row == needle.Row && point.Col == needle.Col {
@@ -7,4 +9,16 @@ func inExplored(needle Point, haystack []Point) bool {
 		}
 	}
 	return false
+}
+
+func emptyTmp() {
+	directory := "./tmp/"
+	dir, _ := os.Open(directory)
+	filesToDelete, _ := dir.Readdir(0)
+
+	for index := range filesToDelete {
+		f := filesToDelete[index]
+		fullpath := directory + f.Name()
+		_ = os.Remove(fullpath)
+	}
 }
