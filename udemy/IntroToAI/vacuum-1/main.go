c89354e26c1f035ac60386f74c66c9e9d8970425
Romel Campbell <romel.campbell@gmail.com>
2026-02-10 11:15:30 -0500

    WIP: vacuum-1

--- udemy/IntroToAI/vacuum-1/main.go
+++ udemy/IntroToAI/vacuum-1/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "flag"
+
+func main() {
+	var configFile, algorithm string
+	var animate bool
+	flag.StringVar(&configFile, "file", "empty.json", "Path to the configuration file")
+	flag.StringVar(&algorithm, "algorithm", "random", "Cleaning Algorithm, Cleaning algorithm to use (random, ...")
+	flag.BoolVar(&animate, "animate", true, "Animate while cleaning")
+	flag.Parse()
+
+}
